71777ed8681ddb20eb2a95d2fcdaece7791785b4
Torsten Schmidt <schmto@roetzer-engineering.com>
2015-06-30 09:29:19 +0200

    start generic config

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Way\Generators\GeneratorsServiceProvider'
 
 	),
 
