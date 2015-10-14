863d4511e5b851045e7a32dd2dd9d52947b36e1c
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2015-10-14 13:48:51 +0200

    Added param to BaseModel->getPossibleEnumValues
    Now you can set the $with_empty_option flag. If true you get an empty
    value in first position – this can be used in generating forms to
    indicate a not explizit set field.

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -10,14 +10,21 @@ class BaseModel extends \Eloquent
 	 *	This returns an array with all possible enum values.
 	 *	Use this instead of hardcoding it e.g. in your view (where it has to be
 	 *		changed with changing/extending enum definition in database)
-	 *	This is following an idea found on:
-	 *		http://stackoverflow.com/questions/26991502/get-enum-options-in-laravels-eloquent
+	 *	You can also get an array with a first empty option – use this in create forms to
+	 *		show that this value is still not set
 	 *	call this method via YourModel::getPossibleEnumValues('yourEnumCol')
 	 *
-	 *	@param column name of your database defined as enum
+	 *	This method is following an idea found on:
+	 *		http://stackoverflow.com/questions/26991502/get-enum-options-in-laravels-eloquent
+	 *
 	 *	@author Patrick Reichel
+	 *
+	 *	@param name column name of your database defined as enum
+	 *	@param with_empty_option should an empty option be added?
+	 *
+	 *	@return array with available enum options
 	 */
-	public static function getPossibleEnumValues($name)
+	public static function getPossibleEnumValues($name, $with_empty_option=false)
 	{
 		// create an instance of the model to be able to get the table name
 		$instance = new static;
@@ -25,12 +32,20 @@ class BaseModel extends \Eloquent
 		// get metadata for the given column and extract enum options
 		$type = DB::select( DB::raw('SHOW COLUMNS FROM '.$instance->getTable().' WHERE Field = "'.$name.'"') )[0]->Type;
 		preg_match('/^enum\((.*)\)$/', $type, $matches);
-		$enum = array();
-		foreach(explode(',', $matches[1]) as $value){
-			$v = trim( $value, "'" );
-			$enum[] = $v;
+
+		$enum_values = array();
+
+		// add an empty option if wanted
+		if ($with_empty_option) {
+			$enum_values[0] = '';
 		}
 
-		return $enum;
+		// add options extracted from database
+		foreach(explode(',', $matches[1]) as $value){
+			$v = trim( $value, "'" );
+			$enum_values[$v] = $v;
+		}
+
+		return $enum_values;
 	}
 }
