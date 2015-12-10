9961f1afa319081ea7042dc1bd9248990131329d
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2015-12-10 10:28:29 +0100

    L5: move ProvVoipEnvia to L5 modules directory

--- Modules/ProvVoipEnvia/Http/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Authentification is necessary before accessing a route
-Route::group(array('before' => 'auth'), function() {
-
-	Route::get('/provvoipenvia/index', array('as' => 'ProvVoipEnvia.index', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@index'));
-	Route::get('/provvoipenvia/request/{job}', array('as' => 'ProvVoipEnvia.request', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@request'));
-
-});
