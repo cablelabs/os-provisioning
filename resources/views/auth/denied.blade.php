7d4cb81fa08ea0cd7a8665fe478db8af305f808d
Torsten Schmidt <schmto@roetzer-engineering.com>
2015-12-11 12:05:25 +0100

    Bootstrap: add Login / Error Theme

--- resources/views/auth/denied.blade.php
+++ resources/views/auth/denied.blade.php
@@ -1,31 +1,38 @@
 <!doctype html>
 <html>
-<head>
-<title>ACCESS DENIED!</title>
-</head>
-<body style="padding: 50px">
 
-<h1>Access denied</h1>
+	<head>
+		<meta charset="utf-8">
+		<title>NMS</title>
+		@include ('bootstrap.header')
+	</head>
 
-@if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
-    </div>
-@endif
+	@include ('bootstrap.header')	
 
-<br><br>
-<button onclick="goBack()">Go Back</button>
+	<body class="pace-top">
 
-<br><br>
-<form action="{{Request::root()}}">
-    <input type="submit" value="Login">
-</form>
+        <div class="error">
+            <div class="error-code m-b-10">Access Denied <i class="fa fa-warning"></i></div>
+            <div class="error-content">
+                <div class="error-message">
+	                @if (session('status'))
+					    {{ session('status') }}
+					@endif
+                </div>
+                <div class="error-desc m-b-20">
+                    The page you're looking for doesn't exist. <br />
+                    Perhaps, there pages will help find what you're looking for.
+                </div>
+                <div>
+                    <a href="{{Request::root()}}" class="btn btn-success">Go Back to Home Page</a>
+                </div>
+            </div>
+        </div>
+        <!-- end error -->
+        
+    </body>
 
-<script>
-	function goBack() {
-		window.history.back();
-	}
-</script>
-
-</body>
+	@include ('bootstrap.footer')	
 </html>
+
+
