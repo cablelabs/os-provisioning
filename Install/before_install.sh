ce134c3d6c20c1438495fccebc4f31af31606015
Torsten Schmidt <schmto@roetzer-engineering.com>
2016-02-01 13:55:50 +0100

    install: test adaptions

--- Install/before_install.sh
+++ Install/before_install.sh
@@ -1,17 +1,8 @@
 cd /tmp
 
-# add IUS repo
-wget https://centos7.iuscommunity.org/ius-release.rpm
-rpm -Uvh ius-release.rpm
- 
-# update php version with yum replace plugin
-yum install yum-plugin-replace
-yum replace php --replace-with php56u
-
-
 #
 # SSL
 # Self Signed Certificat
 #
-mkdir /etc/httpd/ssl
-openssl req -new -x509 -days 365 -nodes -out /etc/httpd/ssl/httpd.pem -keyout /etc/httpd/ssl/httpd.key
+#mkdir /etc/httpd/ssl
+#openssl req -new -x509 -days 365 -nodes -out /etc/httpd/ssl/httpd.pem -keyout /etc/httpd/ssl/httpd.key
