6a97de87580c4699db7ecda6af6cb9b6e998fca8
Torsten Schmidt <schmto@roetzer-engineering.com>
2016-02-19 11:00:56 +0100

    Modem: migration update x,y as double
    NOTE: a normal float has only 2 precisions
    NOTE: double does not work at the time in Laravel 5.1.29. Ask google.
      - float(.., 8)->default(0) forces SQL to use a double field
      - without ->default(0) we will also get a double of size (8,2). Funny

---
+++ modules/ProvBase/Database/Migrations/2016_02_19_093022_update_Modem_xy_field_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateModemXyFieldTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('modem', function(Blueprint $table)
+        {
+            // NOTE: a normal float has only 2 precisions
+            // NOTE: double does not work at the time in Laravel 5.1.29. Ask google.
+            //      - float(.., 8)->default(0) forces SQL to use a double field
+            //      - without ->default(0) we will also get a double of size (8,2). Funny
+            $table->float('x', 8)->default(0)->change();
+            $table->float('y', 8)->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('modem', function(Blueprint $table)
+        {
+            $table->float('x')->change();
+            $table->float('y')->change();
+        });
+    }
+
+}
