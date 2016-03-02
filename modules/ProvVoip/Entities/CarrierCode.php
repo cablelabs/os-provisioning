3323c0378a8710964835d9043e6ade53b7df61d8
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-03-02 10:07:41 +0100

    Fixed some sorting problems.
    - Carriers are now sorted first by name, secondary by carrier code
    - As a result I had to care for the sorting of the favorites ⇒ they shall
      appear in given order

--- modules/ProvVoip/Entities/CarrierCode.php
+++ modules/ProvVoip/Entities/CarrierCode.php
@@ -27,21 +27,32 @@ class CarrierCode extends \BaseModel {
 
 
 	/**
-	 * return a list [id => carrier (carriercode)] of all carriers
+	 * Return a list [id => carrier (carriercode)] of all carriers.
+	 * This list is prepared for the use in a form's <select>
+	 *	- first comes the default value (=no carrier)
+	 *	- second are the favorites
+	 *	- then the rest
+	 *
+	 *	@author Patrick Reichel
+	 *
+	 *	@return array containing all carriers
 	 */
-	public function carrier_list()
+	public function carrier_list_for_form_select()
 	{
 
 		# carrier codes of the carriers to be on top of the list
+		# the given sorting will be the sorting of the <select>
 		$favorite_carriers = array(
-			'D150', # Telekom
-			'D057', # EnviaTel
+			'D001', # Telekom
+			'D201', # 1&1
 		);
 
 		$normal_carrier_list = array();
 		$favorite_carrier_list = array();
+		$no_carrier_list = array();
 
-		foreach ($this::all()->sortBy('company') as $carrier)
+		// get all the carrier code data and put it into arrays
+		foreach (CarrierCode::orderBy('company')->orderBy('carrier_code')->get() as $carrier)
 		{
 			$id = $carrier->id;
 			$carrier_code = $carrier->carrier_code;
@@ -52,22 +63,41 @@ class CarrierCode extends \BaseModel {
 				$entry .= ' ('.$carrier_code.')';
 			}
 
-			# add to favorite or normal carriers
-			if (array_search($carrier_code, $favorite_carriers) !== False) {
-				$favorite_carrier_list[$id] = $entry;
+			// helper to check if current carrier is favorite and – if yes – in which position it shall occur
+			$fav_pos = array_search($carrier_code, $favorite_carriers);
+
+			// add to the corresponding array
+			if (!boolval($carrier_code)) {
+				$no_carrier_list[$id] = $entry;
+			}
+			elseif ($fav_pos !== False) {
+				$favorite_carrier_list[$fav_pos] = array($id, $entry);
 			}
 			else {
 				$normal_carrier_list[$id] = $entry;
 			}
 		}
 
+
 		// build result array (with favorites on top)
 		// attention! don't change the keys – they refer directly to ids in table carriercode!
-		// therefore we cannot use array_merge
-		$result = array();
-		foreach ($favorite_carrier_list as $id => $entry) {
+		// therefore we cannot use array_merge, but have to do it by hand!
+
+		// first element is the null element ⇒ this will be selected by default in a new form
+		// so we initialize the result array with the null element
+		$result = $no_carrier_list;
+
+		// sort favorites by key (=position in favorite_carriers) to get correct order
+		ksort($favorite_carrier_list);
+
+		// then add the favorites sorted like in favorite_carriers
+		foreach ($favorite_carrier_list as $fav) {
+			$id = $fav[0];
+			$entry = $fav[1];
 			$result[$id] = $entry;
 		}
+
+		// finally add the rest of the carriers
 		foreach ($normal_carrier_list as $id => $entry) {
 			$result[$id] = $entry;
 		}
