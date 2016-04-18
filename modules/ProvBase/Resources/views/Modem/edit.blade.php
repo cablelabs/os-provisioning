0cdd664c7bbf8f7afb6b0c13be253a4cdbb19ef1
Torsten Schmidt <schmto@roetzer-engineering.com>
2016-04-18 10:37:02 +0200

    LAR-79: only show MTA analysis if MTA exists

--- modules/ProvBase/Resources/views/Modem/edit.blade.php
+++ modules/ProvBase/Resources/views/Modem/edit.blade.php
@@ -1,8 +1,11 @@
-<?php 
-$panel_right = [['name' => 'Edit', 'route' => 'Modem.edit', 'link' => [$view_var->id]], 
+<?php
+$panel_right = [['name' => 'Edit', 'route' => 'Modem.edit', 'link' => [$view_var->id]],
 				['name' => 'Analyses', 'route' => 'Provmon.index', 'link' => [$view_var->id]],
-				['name' => 'CPE-Analysis', 'route' => 'Provmon.cpe', 'link' => [$view_var->id]],
-				['name' => 'MTA-Analysis', 'route' => 'Provmon.mta', 'link' => [$view_var->id]]];
+				['name' => 'CPE-Analysis', 'route' => 'Provmon.cpe', 'link' => [$view_var->id]]];
+
+// MTA: only show MTA analysis if Modem has MTAs
+if (isset($view_var->mtas[0]))
+	array_push($panel_right, ['name' => 'MTA-Analysis', 'route' => 'Provmon.mta', 'link' => [$view_var->id]]);
 ?>
 
 @extends ('Generic.edit')
