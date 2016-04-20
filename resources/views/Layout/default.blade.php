2fb41bce69a84b8201797dd1f24bbc0c697907a7
Torsten Schmidt <schmto@roetzer-engineering.com>
2016-04-20 09:01:24 +0200

    LAR-79: prepare breadcrumb

--- resources/views/Layout/default.blade.php
+++ resources/views/Layout/default.blade.php
@@ -7,19 +7,33 @@
 		@include ('bootstrap.header')
 	</head>
 
-	<body <?php if(isset($body_onload)) echo "onload=$body_onload()";?> > 
+	<body <?php if(isset($body_onload)) echo "onload=$body_onload()";?> >
 
 		<div id="page-container" class="fade page-sidebar-fixed page-header-fixed in">
 
 			@include ('Layout.header')
 
 			@include ('bootstrap.sidebar')
-			
+
 			<div id="content" class="content">
+
+				<ol class="breadcrumb pull-right">
+					@if(isset($panel_right))
+						@foreach ($panel_right as $menu)
+							<?php
+				              $route = $menu['route'];
+				              $name  = $menu['name'];
+				              $link  = $menu['link'];
+				            ?>
+				            <li> {{ HTML::linkRoute($route, $name, $link) }}</li>
+						@endforeach
+					@endif
+				</ol>
+
 				@yield ('content')
 			</div>
-			
-			@include ('bootstrap.footer')	
+
+			@include ('bootstrap.footer')
 		</div>
 
 	</body>
