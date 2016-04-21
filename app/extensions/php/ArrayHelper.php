8af378a201e92892bd327a0c36b0a393a0ba89af
Torsten Schmidt <schmto@roetzer-engineering.com>
2016-04-21 08:21:35 +0200

    LAR-79: ArrayHelper: add array_depth()

--- app/extensions/php/ArrayHelper.php
+++ app/extensions/php/ArrayHelper.php
@@ -17,51 +17,77 @@ class ArrayHelper {
 	 *
 	 * @author: Torsten Schmidt
 	 */
-    public static function objArraySearch($array, $index, $value)
-    {
-        foreach($array as $arrayInf) {
-            if($arrayInf->{$index} == $value) {
-                return $arrayInf;
-            }
-        }
-        return null;
-    }
+	public static function objArraySearch($array, $index, $value)
+	{
+		foreach($array as $arrayInf) {
+			if($arrayInf->{$index} == $value) {
+				return $arrayInf;
+			}
+		}
+		return null;
+	}
 
 
-    /*
-     * Device all entrys of an Array by $div
-     *
-     * @param $array: The Array to split
-     * @param $div: device by $div
-     * @return: The devided array
-     *
-     * @author: Torsten Schmidt
-     */
-    public static function ArrayDiv($array, $div=10)
-    {
-        $ret = [];
+	/*
+	 * Device all entrys of an Array by $div
+	 *
+	 * @param $array: The Array to split
+	 * @param $div: device by $div
+	 * @return: The devided array
+	 *
+	 * @author: Torsten Schmidt
+	 */
+	public static function ArrayDiv($array, $div=10)
+	{
+		$ret = [];
 
-        foreach ($array as $a)
-        {
-            array_push($ret, $a/$div);
-        }
+		foreach ($array as $a)
+		{
+			array_push($ret, $a/$div);
+		}
 
-        return $ret;
-    }
+		return $ret;
+	}
 
 
-    /*
-     * return the rotated array ($a)
-     * Example: [1,2,3,4] -> [4,1,2,3]
-     *
-     * @param $a: The Array to rotate
-     * @return: The rotated/shifted array
-     *
-     * @author: Torsten Schmidt
-     */
-    public static function array_rotate($a)
-    {
-        return array_merge(array(array_pop($a)), $a);
-    }
+	/*
+	 * return the rotated array ($a)
+	 * Example: [1,2,3,4] -> [4,1,2,3]
+	 *
+	 * @param $a: The Array to rotate
+	 * @return: The rotated/shifted array
+	 *
+	 * @author: Torsten Schmidt
+	 */
+	public static function array_rotate($a)
+	{
+		return array_merge(array(array_pop($a)), $a);
+	}
+
+
+	/*
+	 * return nested array depth
+	 *
+	 * @param $a: The Array to check
+	 * @return: the nested array depth as int
+	 *
+	 * @author: Torsten Schmidt
+	 */
+	public static function array_depth(array $array)
+	{
+		$max_depth = 1;
+
+		foreach ($array as $value) {
+			if (is_array($value)) {
+				$depth = ArrayHelper::array_depth($value) + 1;
+
+				if ($depth > $max_depth) {
+					$max_depth = $depth;
+				}
+			}
+		}
+
+		return $max_depth;
+	}
 
 }
