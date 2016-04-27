bd617ae21f2d442815fe2f77a05715c4484806a8
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2016-04-27 15:24:46 +0200

    LAR53-Migrations: fixed update migration bug for contract table

---
+++ modules/ProvBase/Database/Migrations/2016_03_15_000200_update_Contract_for_billing_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateContractForBillingTable{
+
+    /**
+	 * Run the migrations.
+	 * For using the Envia API we need some changes in storing the contracts data.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contract', function(Blueprint $table)
+        {
+			$table->integer('costcenter_id');
+			$table->integer('salesman_id');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contract', function(Blueprint $table)
+		{
+			$table->dropColumn([
+				'costcenter_id',
+				'salesman_id',
+				]);
+        });
+    }
+
+}
