1a390199cfced8a57484ca063630d35a0ec6eb1f
Torsten Schmidt <schmto@roetzer-engineering.com>
2016-05-13 12:20:58 +0200

    CCC: start BaseAuth middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,6 +16,8 @@ class Kernel extends HttpKernel {
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
 		'App\Http\Middleware\VerifyCsrfToken',
+
+		\App\Http\Middleware\BaseAuthMiddleware::class,
 	];
 
 	/**
