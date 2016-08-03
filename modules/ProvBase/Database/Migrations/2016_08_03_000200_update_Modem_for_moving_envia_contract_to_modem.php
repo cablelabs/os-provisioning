db520efb6f9c9f4b0e6b0fbd676b67d52a72eea3
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-08-03 10:02:38 +0200

    Moved envia contract stuff to modem table.

---
+++ modules/ProvBase/Database/Migrations/2016_08_03_000200_update_Modem_for_moving_envia_contract_to_modem.php
@@ -0,0 +1,93 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Envia contracts match our Modems (=telephone connection) we move external contract stuff to modem…
+ *
+ * @author Patrick Reichel
+ */
+class UpdateModemForMovingEnviaContractToModem extends BaseMigration {
+
+	// name of the table to create
+	protected $tablename = "modem";
+
+
+    /**
+	 * Run the migrations.
+	 * For using the Envia API we need some changes in storing the contracts data.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->tablename, function(Blueprint $table)
+        {
+			$table->string('contract_external_id')->nullable()->default(NULL)->after('contract_id');
+			$table->date('contract_ext_creation_date')->nullable()->default(NULL)->after('contract_external_id');
+			$table->date('contract_ext_termination_date')->nullable()->default(NULL)->after('contract_ext_creation_date');
+        });
+
+		// give all cols to be indexed (old and new ones => the index will be dropped and then created from scratch)
+		$this->set_fim_fields([
+			'name',
+			'hostname',
+			'contract_external_id',
+			'firstname',
+			'lastname',
+			'street',
+			'house_number',
+			'zip',
+			'city',
+			'district',
+			'birthday',
+			'company',
+			'department',
+			'mac',
+			'serial_num',
+			'inventar_num',
+			'description',
+		]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tablename, function(Blueprint $table)
+		{
+			$table->dropColumn([
+				'contract_external_id',
+				'contract_ext_creation_date',
+				'contract_ext_termination_date',
+			]);
+        });
+
+		// give all cols to be indexed (old and new ones => the index will be dropped and then created from scratch)
+		$this->set_fim_fields([
+			'name',
+			'hostname',
+			'firstname',
+			'lastname',
+			'street',
+			'house_number',
+			'zip',
+			'city',
+			'district',
+			'birthday',
+			'company',
+			'department',
+			'mac',
+			'serial_num',
+			'inventar_num',
+			'description',
+		]);
+    }
+
+}
+
