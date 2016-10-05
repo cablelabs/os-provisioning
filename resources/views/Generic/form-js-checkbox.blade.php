6c23b13e1a28826b9a7386c39de004b5d011d8c9
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-10-05 15:51:40 +0200

    Added show/hide of form fields depending on checkbox state.
    Now man can show/hide form fields when the correlating checkbox is
    checked/unchecked. Called onLoad and onChange.

--- resources/views/Generic/form-js-checkbox.blade.php
+++ resources/views/Generic/form-js-checkbox.blade.php
@@ -1,25 +1,19 @@
 {{--
 
-This blade is used for jquery (java script) realtime based showing/hiding of fields depending on checkbox state.
+	This blade is used for jquery (java script) realtime based showing/hiding of fields depending on checkbox state.
 
-NOTE: will be used from form-js blade and must be called inside javascript context
+	NOTE: will be used from form-js blade and must be called inside javascript context
 
-@param id the id of the triggering checkbox
-@author Patrick Reichel
+	@param id the id of the triggering checkbox
+	@author Patrick Reichel
 
 --}}
 
-function par__toggle_class_visibility_depending_on_checkbox(id) {
-
-	show_class = 'show_on_' + id;
-	hide_class = 'hide_on_' + id;
-
-	if ($('#' + id).prop('checked')) {
-		$('.' + show_class).show();
-		$('.' + hide_class).hide();
-	}
-	else {
-		$('.' + show_class).hide();
-		$('.' + hide_class).show();
-	};
+if ($('#{{$field['name']}}').prop('checked')) {
+	$('.show_on_{{$field['name']}}').show();
+	$('.hide_on_{{$field['name']}}').hide();
+}
+else {
+	$('.show_on_{{$field['name']}}').hide();
+	$('.hide_on_{{$field['name']}}').show();
 };
