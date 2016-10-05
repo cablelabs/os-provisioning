a81326f43f88f85243f791d3eaff0091082fb129
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-10-05 17:18:28 +0200

    Moved toggling of visibility (by checkbox) to function.
    This function is only included once and then called for each checkbox.

--- resources/views/Generic/form-js-checkbox.blade.php
+++ resources/views/Generic/form-js-checkbox.blade.php
@@ -4,15 +4,22 @@ This blade is used for jquery (java script) realtime based showing/hiding of fie
 
 NOTE: will be used from form-js blade and must be called inside javascript context
 
-@param $field: the form field that has a valid select entry with type array
+@param id the id of the triggering checkbox
+@author Patrick Reichel
 
 --}}
 
-if ($('#{{$field['name']}}').prop('checked')) {
-	$('.show_on_{{$field['name']}}').show();
-	$('.hide_on_{{$field['name']}}').hide();
-}
-else {
-	$('.show_on_{{$field['name']}}').hide();
-	$('.hide_on_{{$field['name']}}').show();
+function par__toggle_class_visibility_depending_on_checkbox(id) {
+
+	show_class = 'show_on_' + id;
+	hide_class = 'hide_on_' + id;
+
+	if ($('#' + id).prop('checked')) {
+		$('.' + show_class).show();
+		$('.' + hide_class).hide();
+	}
+	else {
+		$('.' + show_class).hide();
+		$('.' + hide_class).show();
+	};
 };
