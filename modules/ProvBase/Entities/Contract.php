b2e82fb257073989c3fe376ea131625c9996d0ba
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2016-10-07 15:31:47 +0200

    Bugfix-Contract: validate contract numbers to be unique

--- modules/ProvBase/Entities/Contract.php
+++ modules/ProvBase/Entities/Contract.php
@@ -24,6 +24,8 @@ class Contract extends \BaseModel {
 		return array(
 			'number' => 'integer|unique:contract,number,'.$id.',id,deleted_at,NULL',
 			'number2' => 'string|unique:contract,number2,'.$id.',id,deleted_at,NULL',
+			'number3' => 'string|unique:contract,number3,'.$id.',id,deleted_at,NULL',
+			'number4' => 'string|unique:contract,number4,'.$id.',id,deleted_at,NULL',
 			'firstname' => 'required',
 			'lastname' => 'required',
 			'street' => 'required',
