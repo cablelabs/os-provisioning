c894a49808cbe2c0b0cec735fa9ec371298b2633
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-10-26 15:05:55 +0200

    misc_get_orders_csv now updates Contract, Modem, PhonenumberManagement.
    If an order within the CSV is not existent in our database we now not
    only add it but also update the related stuff.
    This is e.g. needed to show the correct Envia actions.

---
+++ app/Exceptions/NotImplementedException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Exceptions;
+
+/**
+ * Exception to indicate missing (but needed) values.
+ *
+ * @author Patrick Reichel
+ */
+class NotImplementedException extends \LogicException {
+}
+
