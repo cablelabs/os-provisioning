c6fe0fc0ef64499a8ad7a9d3a932cbae19aec9fa
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-11-02 09:04:00 +0100

    Fixed typos: Hfccustomer ⇒ HfcCustomer in several files.
    Reason: some of my stuff stopped working with class not found and
    similar stuff. I decided to fix this systemwide…
    
    Now we identical spelling in all files. Exception: Class name of one
    seeder – for this I would also have to change the filename (I guess).
    Maybe this should be changed by Ole himself.

--- modules/ProvBase/Entities/Contract.php
+++ modules/ProvBase/Entities/Contract.php
@@ -1084,7 +1084,7 @@ class ContractObserver
 
 	public function updating($contract)
 	{
-		// autocalculate contract number if empty
+		// autocalculate contract number if not given
 		$contract->number = $contract->number ? $contract->number : $contract->id - $this->num;
 
 		if (!\PPModule::is_active('billingbase'))
