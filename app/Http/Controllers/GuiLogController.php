804a67201cd8ecc56f2ef223654e84b47dec5f77
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2016-11-10 15:54:12 +0100

    GuiLog: Add Link to Changed Model in Edit View

--- app/Http/Controllers/GuiLogController.php
+++ app/Http/Controllers/GuiLogController.php
@@ -14,14 +14,29 @@ class GuiLogController extends BaseController {
      */
 	public function view_form_fields($model = null)
 	{
-		// label has to be the same like column in sql table
-		return array(
+		$a = array(
 			array('form_type' => 'text', 'name' => 'username', 'description' => 'Username'),
 			array('form_type' => 'text', 'name' => 'method', 'description' => 'Method'),
 			array('form_type' => 'text', 'name' => 'model', 'description' => 'Model'),
 			array('form_type' => 'text', 'name' => 'model_id', 'description' => 'ID'),
 			array('form_type' => 'textarea', 'name' => 'text', 'description' => 'Changed Attributes'),
 			);
+
+		// add link of changed Model in edit view
+		if ($model)
+		{
+			array_push($a, array('form_type' => 'text', 'name' => 'link', 'description' => 'Link', 'html' => 
+				'<div class="col-md-12" style="background-color:white">
+					<div class="form-group"><label style="margin-top: 10px;" class="col-md-4 control-label">Link</label>
+						<div class="col-md-7">
+							<a class="btn btn-default btn-block" href="'.route($model->model.'.edit', ['id' => $model->model_id]).'"> '.$model->model.' '.$model->model_id.'</a>
+						</div>
+					</div>
+				</div>'
+				));
+		}
+
+		return $a;
 	}
 
 
