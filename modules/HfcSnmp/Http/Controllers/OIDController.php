d8977b87b1fbcbcaf2474e14cffc19d4b1a7b05f
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2017-01-17 11:56:32 +0100

    LAR34-OID: Hide specific fields dependent of snmptype in edit/create view (select2 jquery)

--- modules/HfcSnmp/Http/Controllers/OIDController.php
+++ modules/HfcSnmp/Http/Controllers/OIDController.php
@@ -15,6 +15,11 @@ class OIDController extends \BaseController {
 		if (!$model)
 			$model = new OID;
 
+		$snmp_types = $types = OID::getPossibleEnumValues('type', true);
+
+		// unset null element because otherwise hiding of fields doesnt work with jquery select2
+		unset($types[0]);
+
 		// label has to be the same like column in sql table
 		return array(
 			array('form_type' => 'select', 'name' => 'mibfile_id', 'description' => 'MIB-File', 'value' => $model->html_list(MibFile::all(), 'name')),
@@ -26,10 +31,10 @@ class OIDController extends \BaseController {
 			array('form_type' => 'select', 'name' => 'html_type', 'description' => 'HTML Type', 'value' => OID::getPossibleEnumValues('html_type')),
 
 			// array('form_type' => 'checkbox', 'name' => 'oid_table', 'description' => 'SNMP Table Element'),
-			array('form_type' => 'select', 'name' => 'type', 'description' => 'SNMP Type', 'value' => OID::getPossibleEnumValues('type', true)),
-			array('form_type' => 'text', 'name' => 'unit_divisor', 'description' => 'Unit Divisor'),
-			array('form_type' => 'text', 'name' => 'startvalue', 'description' => 'Start Value'),
-			array('form_type' => 'text', 'name' => 'endvalue', 'description' => 'End Value'),
+			array('form_type' => 'select', 'name' => 'type', 'description' => 'SNMP Type', 'value' => $snmp_types, 'select' => $types),
+			array('form_type' => 'text', 'name' => 'unit_divisor', 'description' => 'Unit Divisor', 'select' => 'i u'),
+			array('form_type' => 'text', 'name' => 'startvalue', 'description' => 'Start Value', 'select' => 'i u'),
+			array('form_type' => 'text', 'name' => 'endvalue', 'description' => 'End Value', 'select' => 'i u'),
 
 			array('form_type' => 'textarea', 'name' => 'description', 'description' => 'Description'),
 		);
