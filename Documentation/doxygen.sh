4c0ea66fbac5c94d770e3235afbdd51e58113332
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-01-17 20:07:04 +0100

    doxygen.sh now informs that warnings can be found in logfile.

--- Documentation/doxygen.sh
+++ Documentation/doxygen.sh
@@ -4,4 +4,8 @@
 # see https://christianhujer.github.io/Git-Version-in-Doxygen
 export PROJECT_NUMBER="$(git rev-parse HEAD ; git diff-index --quiet HEAD || echo '(with uncommitted changes)')"
 
-exec doxygen /var/www/lara/Documentation/doxyfile
+doxygen /var/www/lara/Documentation/doxyfile
+
+echo
+echo "Warnings from last run are stored in doxywarn.log"
+echo
