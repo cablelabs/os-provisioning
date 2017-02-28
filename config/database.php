1166bcbc58b59d16f5af225a2a431a8e4f3ee856
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-02-28 19:02:11 +0100

    Re-added config dir.
    We have to check this again – but I had problems with deleted config dir
    when trying to checkout dev.

---
+++ config/database.php
@@ -0,0 +1,166 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| PDO Fetch Style
+	|--------------------------------------------------------------------------
+	|
+	| By default, database results will be returned as instances of the PHP
+	| stdClass object; however, you may desire to retrieve records in an
+	| array format for simplicity. Here you can tweak the fetch style.
+	|
+	*/
+
+	'fetch' => PDO::FETCH_CLASS,
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Database Connection Name
+	|--------------------------------------------------------------------------
+	|
+	| Here you may specify which of the database connections below you wish
+	| to use as your default connection for all database work. Of course
+	| you may use many connections at once using the Database library.
+	|
+	*/
+
+	'default' => 'mysql',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Database Connections
+	|--------------------------------------------------------------------------
+	|
+	| Here are each of the database connections setup for your application.
+	| Of course, examples of configuring each database platform that is
+	| supported by Laravel is shown below to make development simple.
+	|
+	|
+	| All database work in Laravel is done through the PHP PDO facilities
+	| so make sure you have the driver for your particular database of
+	| choice installed on your machine before you begin development.
+	|
+	*/
+
+	'connections' => [
+
+		'sqlite' => [
+			'driver'   => 'sqlite',
+			'database' => storage_path().'/database.sqlite',
+			'prefix'   => '',
+		],
+
+		// TODO: don't use root sql user to access lara DB
+		//       we should use a separate user for this.
+		'mysql' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
+
+		// TODO: adapt ENV settings to access cacti DB with
+		//       the installed Cacti user and not with root
+		'mysql-cacti' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => 'cacti',
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
+
+		'pgsql-km3' => [
+			'driver'   => 'pgsql',
+			'host'     => env('DB_IMPORT_HOST', 'localhost'),
+			'database' => env('DB_IMPORT_DATABASE', 'db_nms'),
+			'username' => env('DB_IMPORT_USERNAME', 'schmto'),
+			'password' => env('DB_IMPORT_PASSWORD', ''),
+			'charset'  => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'   => '',
+			'schema'   => 'public',
+		],
+
+		'mysql-ccc' => [
+			'driver'    => 'mysql',
+			'host'      => env('CCC_DB_HOST', env('DB_HOST', 'localhost')),
+			'database'  => env('CCC_DB_DATABASE', env('DB_DATABASE', 'forge')),
+			'username'  => env('CCC_DB_USERNAME', env('DB_USERNAME', '')),
+			'password'  => env('CCC_DB_PASSWORD', env('DB_PASSWORD', '')),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
+
+		'sqlsrv' => [
+			'driver'   => 'sqlsrv',
+			'host'     => env('DB_HOST', 'localhost'),
+			'database' => env('DB_DATABASE', 'forge'),
+			'username' => env('DB_USERNAME', 'forge'),
+			'password' => env('DB_PASSWORD', ''),
+			'prefix'   => '',
+		],
+
+		'mysql-voipmonitor' => [
+			'driver'    => 'mysql',
+			'host'      => env('VOIPMONITOR_DB_HOST', env('DB_HOST', 'localhost')),
+			'database'  => env('VOIPMONITOR_DB_DATABASE', env('DB_DATABASE','voipmonitor')),
+			'username'  => env('VOIPMONITOR_DB_USERNAME', env('DB_USERNAME', 'root')),
+			'password'  => env('VOIPMONITOR_DB_PASSWORD', env('DB_PASSWORD', '')),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
+
+	],
+
+	/*
+	|--------------------------------------------------------------------------
+	| Migration Repository Table
+	|--------------------------------------------------------------------------
+	|
+	| This table keeps track of all the migrations that have already run for
+	| your application. Using this information, we can determine which of
+	| the migrations on disk haven't actually been run in the database.
+	|
+	*/
+
+	'migrations' => 'migrations',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Redis Databases
+	|--------------------------------------------------------------------------
+	|
+	| Redis is an open source, fast, and advanced key-value store that also
+	| provides a richer set of commands than a typical key-value systems
+	| such as APC or Memcached. Laravel makes it easy to dig right in.
+	|
+	*/
+
+	'redis' => [
+
+		'cluster' => false,
+
+		'default' => [
+			'host'     => '127.0.0.1',
+			'port'     => 6379,
+			'database' => 0,
+		],
+
+	],
+
+];
