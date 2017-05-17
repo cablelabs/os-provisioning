822a2a3e8ae57e5516db372e1383647adbb57e1c
Christian Schramm <christian.schramm@roetzer-engineering.com>
2017-05-18 00:30:26 +0200

    add Scroll to Top Button

--- resources/views/Layout/default.blade.php
+++ resources/views/Layout/default.blade.php
@@ -29,6 +29,9 @@
 				@yield ('content')
 			</div>
 
+			<!-- begin scroll to top btn -->
+			  <a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top fade" data-click="scroll-top"><i class="fa fa-angle-up"></i></a>
+			 <!-- end scroll to top btn -->
 			@include ('bootstrap.footer')
 			@yield ('java')
 		</div>
