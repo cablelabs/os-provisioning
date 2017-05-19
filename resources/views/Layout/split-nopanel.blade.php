a9e82987dfe005e6d8c0a01be23ba2541360790a
Torsten Schmidt <schmto@roetzer-engineering.com>
2017-05-19 19:25:02 +0200

    GUI: edit - don't use panel blade for left content (looks and feels quit more cool to me)

---
+++ resources/views/Layout/split-nopanel.blade.php
@@ -0,0 +1,16 @@
+@extends ('Layout.default')
+
+@section ('content')
+
+<div class="row col-md-12">
+	<div class="col-md-{{isset($edit_left_md_size) ? $edit_left_md_size : 5}}">
+		<div class="col-md-12 well"><h1 class="page-header">{{$view_header}}</h1>
+			@yield('content_left')
+		</div>
+	</div>
+	@yield('content_right')
+	@yield('content_right_extra')
+
+</div>
+
+@stop
