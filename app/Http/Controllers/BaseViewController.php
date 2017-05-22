dca8e435566a4103c6b3cd12114878b8ee88720d
Ole Ernst <ole.ernst@roetzer-engineering.com>
2017-05-22 10:09:45 +0200

    fix wrong color (yellow instead of green) for SNR values

--- app/Http/Controllers/BaseViewController.php
+++ app/Http/Controllers/BaseViewController.php
@@ -721,8 +721,14 @@ finish:
 	{
 		if ($val < $limit[0] || (isset($limit[3]) && $val > $limit[3]))
 			return 2;
-		if ($val >= $limit[1] && (isset($limit[2]) && $val <= $limit[2]))
-			return 0;
+
+		if ($val >= $limit[1]) {
+			if (!isset($limit[2]))
+				return 0;
+			if ($val <= $limit[2])
+				return 0;
+		}
+
 		return 1;
 	}
 
