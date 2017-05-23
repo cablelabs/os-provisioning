2c698c1a8f5cc94b2de5d31ca1f24943bf9ba009
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2017-05-23 12:02:02 +0200

    hotfix: dashboard - add contract show + disable ticket link

--- modules/ProvBase/Resources/views/dashboard.blade.php
+++ modules/ProvBase/Resources/views/dashboard.blade.php
@@ -2,16 +2,21 @@
 
 @section ('quickstart')
     <!-- TODO: move to a seperate file -->
+    <!-- TODO: use translate lang -->
     <ul class="registered-users-list clearfix">
+        <li>
+            {{ HTML::decode (HTML::linkRoute('Contract.index',
+                '<h1><div class="text-center"><i class="img-center fa fa-address-book-o"></i></div></h1>
+                 <h4 class="username text-ellipsis text-center">Show Contracts<small>Easy</small></h4>')) }}
+        </li>
         <li>
             {{ HTML::decode (HTML::linkRoute('Contract.create',
                 '<h1><div class="text-center"><i class="img-center fa fa-address-book-o"></i></div></h1>
                  <h4 class="username text-ellipsis text-center">Add Contract<small>Easy</small></h4>')) }}
         </li>
         <li>
-            {{ HTML::decode (HTML::linkRoute('Contract.create',
-                '<h1><div class="text-center"><i class="img-center fa fa-ticket"></i></div></h1>
-                 <h4 class="username text-ellipsis text-center">Add Ticket<small>Easy</small></h4>')) }}
+                <h1><div class="text-center"><i class="img-center fa fa-ticket"></i></div></h1>
+                 <h4 class="username text-ellipsis text-center">Add Ticket<small>Easy</small></h4>
         </li>
     </ul>
 @stop
