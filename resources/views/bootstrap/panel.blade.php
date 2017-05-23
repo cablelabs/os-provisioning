282ae7bc244372436c893fc7f804cadddb0af9a0
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2017-05-23 11:48:24 +0200

    hotfix: panel blade - enable y auto scroll

--- resources/views/bootstrap/panel.blade.php
+++ resources/views/bootstrap/panel.blade.php
@@ -3,7 +3,7 @@
 		<div class="panel panel-inverse" data-sortable-id="table-basic-{{$md}}">
 			@include ('bootstrap.panel-header', ['view_header' => $view_header])
 
-			<div class="panel-body fader">
+			<div class="panel-body fader" style="overflow-y:auto; height:100%">
 				@yield($content)
 			</div>
 		</div>
