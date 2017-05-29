f75a5bb5a31080b4c2bd90d1fe94e184c40cef8f
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2017-05-29 22:57:30 +0200

    Dashboard: Add height option for panel blade and remove so the 100% height style from quickstart section

--- resources/views/bootstrap/panel.blade.php
+++ resources/views/bootstrap/panel.blade.php
@@ -1,11 +1,18 @@
-	<!-- begin col-dyn -->
-	<div class="col-md-{{$md}} ui-sortable">
-		<div class="panel panel-inverse" data-sortable-id="table-basic-{{$md}}">
-			@include ('bootstrap.panel-header', ['view_header' => $view_header])
+<?php
+	$style = 'height:100%';
+	if (isset($height))
+	{
+		$style = ($height == 'auto') ? '' : "height:$height%";
+	}
+?>
 
-			<div class="panel-body fader" style="overflow-y:auto; height:100%">
-				@yield($content)
-			</div>
+<!-- begin col-dyn -->
+<div class="col-md-{{$md}} ui-sortable">
+	<div class="panel panel-inverse" data-sortable-id="table-basic-{{$md}}">
+		@include ('bootstrap.panel-header', ['view_header' => $view_header])
+
+		<div class="panel-body fader" style="overflow-y:auto; {{ $style }}">
+			@yield($content)
 		</div>
 	</div>
-	
+</div>
