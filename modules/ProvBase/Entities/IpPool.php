4847e934af40e9faa75d1b7cc445bf4da112abfd
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2017-06-27 10:35:43 +0200

    Intendation Commit for IpPool.php

--- modules/ProvBase/Entities/IpPool.php
+++ modules/ProvBase/Entities/IpPool.php
@@ -7,91 +7,91 @@ use Modules\ProvBase\Entities\Cmts;
 
 class IpPool extends \BaseModel {
 
-    // The associated SQL table for this Model
-    public $table = 'ippool';
+	// The associated SQL table for this Model
+	public $table = 'ippool';
 
-    // Add your validation rules here
-    public static function rules($id = null)
-    {
-        return array(
-            'net' => 'required|ip',
-            'netmask' => 'required|ip|netmask',     // netmask must not be in first place!
-            'ip_pool_start' => 'required|ip|ip_in_range:net,netmask|ip_larger:net',   // own validation - see in classes: ExtendedValidator and IpPoolController
-            'ip_pool_end' => 'required|ip|ip_in_range:net,netmask|ip_larger:ip_pool_start',
-            'router_ip' => 'required|ip|ip_in_range:net,netmask|ip_larger:ip_pool_start',
-            'broadcast_ip' => 'ip|ip_in_range:net,netmask|ip_larger:ip_pool_start',
-            'dns1_ip' => 'ip',
-            'dns2_ip' => 'ip',
-            'dns3_ip' => 'ip'
-        );
-    }
-
-
-    // Name of View
-    public static function view_headline()
-    {
-        return 'IP-Pools';
-    }
-
-    // View Icon
-    public static function view_icon()
-    {
-      return '<i class="fa fa-tags"></i>';
-    }
-
-    // link title in index view
-    public function view_index_label()
-    {
-        $bsclass = 'success';
-
-        if ($this->type == 'CPEPub' || $this->type == 'CPEPriv')
-            $bsclass = 'warning';
-        if ($this->type == 'MTA')
-            $bsclass = 'danger';
-
-        return ['index' => [$this->id, $this->cmts->hostname, $this->type, $this->net, $this->netmask, $this->router_ip, $this->description],
-                'index_header' => ['ID', 'CMTS', 'Type of Pool', 'IP network', 'IP netmask', 'IP router', 'Description'],
-                'bsclass' => $bsclass,
-                'header' => $this->type.': '.$this->net.' / '.$this->netmask];
-    }
+	// Add your validation rules here
+	public static function rules($id = null)
+	{
+		return array(
+			'net' => 'required|ip',
+			'netmask' => 'required|ip|netmask',     // netmask must not be in first place!
+			'ip_pool_start' => 'required|ip|ip_in_range:net,netmask|ip_larger:net',   // own validation - see in classes: ExtendedValidator and IpPoolController
+			'ip_pool_end' => 'required|ip|ip_in_range:net,netmask|ip_larger:ip_pool_start',
+			'router_ip' => 'required|ip|ip_in_range:net,netmask|ip_larger:ip_pool_start',
+			'broadcast_ip' => 'ip|ip_in_range:net,netmask|ip_larger:ip_pool_start',
+			'dns1_ip' => 'ip',
+			'dns2_ip' => 'ip',
+			'dns3_ip' => 'ip'
+		);
+	}
 
 
+	// Name of View
+	public static function view_headline()
+	{
+		return 'IP-Pools';
+	}
 
-    /**
-     * Returns all cmts hostnames for ip pools as an array
-     */
-    public function cmts_hostnames ()
-    {
-        return DB::table('cmts')->select('id', 'hostname')->get();
-    }
+	// View Icon
+	public static function view_icon()
+	{
+	  return '<i class="fa fa-tags"></i>';
+	}
+
+	// link title in index view
+	public function view_index_label()
+	{
+		$bsclass = 'success';
+
+		if ($this->type == 'CPEPub' || $this->type == 'CPEPriv')
+			$bsclass = 'warning';
+		if ($this->type == 'MTA')
+			$bsclass = 'danger';
+
+		return ['index' => [$this->id, $this->cmts->hostname, $this->type, $this->net, $this->netmask, $this->router_ip, $this->description],
+				'index_header' => ['ID', 'CMTS', 'Type of Pool', 'IP network', 'IP netmask', 'IP router', 'Description'],
+				'bsclass' => $bsclass,
+				'header' => $this->type.': '.$this->net.' / '.$this->netmask];
+	}
 
 
-    /**
-     * Relationships:
-     */
 
-    public function cmts()
-    {
-        return $this->belongsTo('Modules\ProvBase\Entities\Cmts', 'cmts_id');
-    }
+	/**
+	 * Returns all cmts hostnames for ip pools as an array
+	 */
+	public function cmts_hostnames ()
+	{
+		return DB::table('cmts')->select('id', 'hostname')->get();
+	}
 
-    // belongs to a cmts - see BaseModel for explanation
-    public function view_belongs_to ()
-    {
-        return $this->cmts;
-    }
 
-    /**
-     * BOOT:
-     * - init cmts observer
-     */
-    public static function boot()
-    {
-        parent::boot();
+	/**
+	 * Relationships:
+	 */
 
-        IpPool::observe(new IpPoolObserver);
-        IpPool::observe(new \App\SystemdObserver);
-    }
+	public function cmts()
+	{
+		return $this->belongsTo('Modules\ProvBase\Entities\Cmts', 'cmts_id');
+	}
+
+	// belongs to a cmts - see BaseModel for explanation
+	public function view_belongs_to ()
+	{
+		return $this->cmts;
+	}
+
+	/**
+	 * BOOT:
+	 * - init cmts observer
+	 */
+	public static function boot()
+	{
+		parent::boot();
+
+		IpPool::observe(new IpPoolObserver);
+		IpPool::observe(new \App\SystemdObserver);
+	}
 
 
 }
@@ -107,24 +107,24 @@ class IpPool extends \BaseModel {
  */
 class IpPoolObserver
 {
-    public function created($pool)
-    {
-        // fetch cmts object that is related to the created ippool and make dhcp conf
-        // if (isset($pool->cmts))
-            $pool->cmts->make_dhcp_conf();
-    }
+	public function created($pool)
+	{
+		// fetch cmts object that is related to the created ippool and make dhcp conf
+		// if (isset($pool->cmts))
+			$pool->cmts->make_dhcp_conf();
+	}
 
-    public function updated($pool)
-    {
-       $pool->cmts->make_dhcp_conf();
+	public function updated($pool)
+	{
+	   $pool->cmts->make_dhcp_conf();
 
-        // make dhcp conf of old cmts if relation got changed
-        if ($pool["original"]["cmts_id"] != $pool["attributes"]["cmts_id"])
-            $cmts_old = Cmts::find($pool["original"]["cmts_id"])->make_dhcp_conf();
-    }
+		// make dhcp conf of old cmts if relation got changed
+		if ($pool["original"]["cmts_id"] != $pool["attributes"]["cmts_id"])
+			$cmts_old = Cmts::find($pool["original"]["cmts_id"])->make_dhcp_conf();
+	}
 
-    public function deleted($pool)
-    {
-        $pool->cmts->make_dhcp_conf();
-    }
+	public function deleted($pool)
+	{
+		$pool->cmts->make_dhcp_conf();
+	}
 }
