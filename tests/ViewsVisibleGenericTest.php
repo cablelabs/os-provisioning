2dafc6896ce42f562cfaca69f735cba914a646f6
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-07-19 16:50:18 +0200

    Deleted accidently still existing migration file.

--- tests/ViewsVisibleGenericTest.php
+++ tests/ViewsVisibleGenericTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ViewsVisibleTest extends TestCase
+class ViewsVisibleGenericTest extends TestCase
 {
     use WithoutMiddleware;
 
