55ba9a1b0de5983b39a09d7e5de2366fccc7bf59
Torsten Schmidt <schmto@roetzer-engineering.com>
2017-09-21 19:55:04 +0200

    ccc: use .env to config admin/ccc port

--- app/Http/Middleware/RedirectStartPage.php
+++ app/Http/Middleware/RedirectStartPage.php
@@ -7,7 +7,16 @@ use Closure;
 class RedirectStartPage
 {
     /**
-     * Handle an incoming request.
+     * Start Page: Page which show link to admin or ccc panel
+     * This site is only required if Admin and CCC runs on same port
+     *
+     * USE: HTTPS_ADMIN_PORT or
+     *      HTTPS_CCC_PORT
+     *
+     *      in .env file to skip start page and go directly to admin or ccc
+     *
+     * NOTE: if different ports are used, take care that apache conf.d files
+     *       are used in correct manner! See Documentation / Confluence..
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
@@ -15,11 +24,23 @@ class RedirectStartPage
      */
     public function handle($request, Closure $next)
     {
-        if (str_contains($request->url(), ':8080/'))
+        // default settings
+        // NOTE: required for backward compatibility
+        //       admin and ccc run's on same port
+        $admin = env('HTTPS_ADMIN_PORT', '443');
+        $ccc   = env('HTTPS_CCC_PORT', '443');
+
+        // if same port, show start page
+        if ($admin == $ccc)
+            return $next($request);
+
+        if ($_SERVER['SERVER_PORT'] == $admin)
             return redirect('admin');
-        else
+
+        if ($_SERVER['SERVER_PORT'] == $ccc)
             return redirect('customer');
 
+        // start page
         return $next($request);
     }
 }
