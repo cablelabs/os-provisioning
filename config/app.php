39801d2c6c81c9595a7c64d6d2aa705f9105409d
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-10-25 16:52:35 +0200

    Modified for use of separate ports for admin and ccc.

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
 	/* 'url' => 'http://localhost', */
 
 	// changed by Patrick Reichel => use this in /modules/ProvVoipEnvia/Console/EnviaOrderUpdaterCommand.php
-	'url' => 'https://localhost/nmsprime',
+	'url' => env('APP_BASEURL', 'https://localhost'),
 
 	/*
 	|--------------------------------------------------------------------------
