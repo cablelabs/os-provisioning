e222c94f1306f01cf3ed4f022fc779075c411d9e
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-11-04 14:35:35 +0100

    Edited after_install.sh scripts to make .env readable for apache.

--- Install/after_install.sh
+++ Install/after_install.sh
@@ -61,3 +61,8 @@ php artisan migrate
 # Note: needs to run last. storage/logs is only available after artisan optimize
 mkdir $dir/storage/logs
 chown -R apache $dir/storage/ $dir/bootstrap/cache/
+
+# make .env files readable for apache
+chgrp -R apache /etc/nmsprime/env
+chmod -R o-rwx /etc/nmsprime/env
+chmod -R g-w /etc/nmsprime/env
