490d87fd019b46eeb78abbb3c4dbea4f885ccbb5
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-11-07 19:00:46 +0100

    Cleaned modules/ProvVoip/Http/Controllers/PhonebookEntryController.php.

--- modules/ProvVoip/Http/Controllers/PhonebookEntryController.php
+++ modules/ProvVoip/Http/Controllers/PhonebookEntryController.php
@@ -219,7 +219,7 @@ class PhonebookEntryController extends \BaseController {
 	}
 
 	/**
-	 * Get all management jobs for Envia
+	 * Get all management jobs for envia TEL
 	 *
 	 * @author Patrick Reichel
 	 * @param $phonebookentry current phonebookentry object
@@ -229,7 +229,7 @@ class PhonebookEntryController extends \BaseController {
 
 		$provvoipenvia = new \Modules\ProvVoipEnvia\Entities\ProvVoipEnvia();
 
-		// check if user has the right to perform actions against Envia API
+		// check if user has the right to perform actions against envia TEL API
 		\App\Http\Controllers\BaseAuthController::auth_check('view', 'Modules\ProvVoipEnvia\Entities\ProvVoipEnvia');
 
 		return $provvoipenvia->get_jobs_for_view($phonebookentry, 'phonebookentry');
