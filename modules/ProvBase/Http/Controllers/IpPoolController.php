2b70ce2692f0c565c9183c9d445ca1e80cf78284
Torsten Schmidt <schmto@roetzer-engineering.com>
2017-11-11 18:58:12 +0100

    IPPool: create - add auto calc

--- modules/ProvBase/Http/Controllers/IpPoolController.php
+++ modules/ProvBase/Http/Controllers/IpPoolController.php
@@ -14,13 +14,71 @@ class IpPoolController extends \BaseController {
      */
 	public function view_form_fields($model = null)
 	{
+		$init_values = array();
+
 		if (!$model)
+		{
 			$model = new IpPool;
+		}
+
+		// create context: calc next free ip pool
+		if (!$model->exists)
+		{
+			$init_values = [];
+
+			// parse which ip type we want to create
+			$type = (isset($_GET['type']) ? $_GET['type'] : 'CM');
+
+			// get last ip pools ordered desc by net, which means get last added ip pool
+			$ippools=IpPool::where('type','=',$type)->orderBy('net', 'DESC')->get();
+
+			// check if we still have pools in DB
+			if ($ippools->count()>0)
+			{
+				// create the next free ip net: this means:
+				// next_net = last_broadcast + 1
+				$last_ippool = $ippools[0];
+				$next_net = long2ip(ip2long($last_ippool->broadcast_ip)+1);
+
+				// next dns is most likly last dns
+				$init_values['dns1_ip'] = $last_ippool->dns1_ip;
+				$init_values['dns2_ip'] = $last_ippool->dns2_ip;
+				$init_values['dns3_ip'] = $last_ippool->dns3_ip;
+			}
+			else switch ($type) { // if not: add default net, depending on type
+				case 'CM':      $next_net = env('IP_CM_DEFAULT_NET', '10.0.0.0');	break;
+				case 'CPEPriv': $next_net = env('IP_CPE_PRIV_DEFAULT_NET', '100.64.0.0');  $init_values['dns1_ip'] = '8.8.8.8'; break;
+				case 'CPEPub':  $next_net = env('IP_CPE_PUB_DEFAULT_NET', '192.168.100.0'); $init_values['dns1_ip'] = '8.8.8.8'; break;
+				case 'MTA':     $next_net = env('IP_MTA_DEFAULT_NET', '172.17.0.0'); $init_values['dns1_ip'] = \ProvBase::first()->provisioning_server;	break;
+				default: $next_net = '192.168.200.0'; break;
+			}
+
+			// Get default IP net size, like 255.255.255.0 = /24
+			switch ($type) {
+				case 'CM':      $size = env('IP_CM_DEFAULT_SIZE', 19);	break; // /19
+				case 'CPEPriv': $size = env('IP_CPE_PRIV_DEFAULT_SIZE', 22);	break; // /22
+				case 'CPEPub':  $size = env('IP_CPE_PUB_DEFAULT_SIZE', 27);	break; // /27
+				case 'MTA':     $size = env('IP_MTA_DEFAULT_SIZE', 24);	break; // /24
+				default: $size = 24; break;
+			}
+
+			// calc the ip next net
+			$sub = new \IPv4\SubnetCalculator( $next_net, $size );
+
+			$init_values += array(
+				'net' => $sub->getNetworkPortion(),
+				'netmask' => $sub->getSubnetMask(),
+				'ip_pool_start' => long2ip(ip2long($sub->getIPAddressRange()[0])+1), // first ip + 1
+				'ip_pool_end' => long2ip(ip2long($sub->getIPAddressRange()[1])-2), // last ip -2
+				'router_ip' => long2ip(ip2long($sub->getIPAddressRange()[1])-1), // last ip -1
+				'broadcast_ip' => $sub->getBroadcastAddress()
+			);
+		}
 
 		// label has to be the same like column in sql table
-		return array(
+		$ret_tmp = array(
 			array('form_type' => 'select', 'name' => 'cmts_id', 'description' => 'CMTS Hostname', 'value' => $model->html_list($model->cmts_hostnames(), 'hostname'), 'hidden' => 1),
-			array('form_type' => 'select', 'name' => 'type', 'description' => 'Type', 'value' => array( 'CM' => 'Cable Modem', 'CPEPriv' => 'CPE Private', 'CPEPub' => 'CPE Public', 'MTA' => 'MTA')),
+			array('form_type' => 'select', 'name' => 'type', 'description' => 'Type', 'value' => array( 'CM' => 'Cable Modem', 'CPEPriv' => 'CPE Private', 'CPEPub' => 'CPE Public', 'MTA' => 'MTA'), 'hidden'=>'C'),
 			array('form_type' => 'text', 'name' => 'net', 'description' => 'Net'),
 			array('form_type' => 'text', 'name' => 'netmask', 'description' => 'Netmask'),
 			array('form_type' => 'text', 'name' => 'ip_pool_start', 'description' => 'First IP'),
@@ -33,7 +91,20 @@ class IpPoolController extends \BaseController {
 			array('form_type' => 'textarea', 'name' => 'optional', 'description' => 'Additional Options'),
 			array('form_type' => 'textarea', 'name' => 'description', 'description' => 'Description')
 		);
+
+		// add init values if set
+		$ret = array();
+		foreach ($ret_tmp as $elem) {
+
+			if (array_key_exists($elem['name'], $init_values)) {
+				$elem['init_value'] = $init_values[$elem['name']];
+			}
+			array_push($ret, $elem);
+		}
+
+		return $ret;
 	}
+
 
     /**
      * Replaces the placeholders (named like the array key inside the data array/sql columns)
