fbc4e526d12af6f09ada020970c5302fd752769b
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-11-15 15:44:57 +0100

    Added flag for child delete and checking for n:m relations on child delete.
    Also, added some nice output: NMSPrime now tells us what has been
    deleted.

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -22,6 +22,9 @@ class BaseModel extends Eloquent
 	// use to enable force delete for inherit models
 	protected $force_delete = 0;
 
+	// flag showing if children also shall be deleted on Model::delete()
+	protected $delete_children = True;
+
 	public $voip_enabled;
 	public $billing_enabled;
 	protected $fillable = array();
@@ -735,9 +738,25 @@ class BaseModel extends Eloquent
 	 */
 	public function get_all_children()
 	{
-		$relations = [];
-		// exceptions
-		$exceptions = ['configfile_id', 'salesman_id', 'costcenter_id', 'company_id', 'sepaaccount_id', 'product_id'/*, 'mibfile_id', 'oid_id'*/];
+		$relations = [
+			'1:n' => [],
+			'n:m' => []
+		];
+		// exceptions – the children (=their database ID fields) that never should be deleted
+		$exceptions = [
+			'company_id',
+			'configfile_id',
+			'costcenter_id',
+			'country_id',	// not used yet
+			//'mibfile_id',
+			//'oid_id',
+			'product_id',
+			'qos_id',
+			'salesman_id',
+			'sepaaccount_id',
+			'voip_id',
+		];
+
 
 		// this is the variable that holds table names in $table returned by DB::select('SHOW TABLES')
 		// named dynamically containing the database name
@@ -755,18 +774,44 @@ class BaseModel extends Eloquent
 					if (in_array($column, $exceptions))
 						continue;
 					// get all objects with $column
-					foreach (DB::select('SELECT id FROM '.$table->$tables_var_name.' WHERE '.$column.'='.$this->id) as $child)
+					$query = 'SELECT * FROM '.$table->$tables_var_name.' WHERE '.$column.'='.$this->id;
+					echo $query.'<br>';
+					foreach (DB::select($query) as $child)
 					{
 						$class_child_name = $this->_guess_model_name ($table->$tables_var_name);
-						$class = new $class_child_name;
-
-						array_push($relations, $class->find($child->id));
+						// check if we got a model name
+						if ($class_child_name) {
+							// yes! 1:n relation
+							$class = new $class_child_name;
+							$rel = $class->find($child->id);
+							if (!is_null($rel)) {
+								array_push($relations['1:n'], $rel);
+							}
+						}
+						else {
+							// seems to be a n:m relation
+							$parts = explode('_', $table->$tables_var_name);
+							foreach ($parts as $part) {
+								$class_child_name = $this->_guess_model_name($part);
+								if ($class_child_name == get_class($this)) {
+									continue;
+								}
+								else {
+									$class = new $class_child_name;
+									$id_col = $part.'_id';
+									$rel = $class->find($child->{$id_col});
+									if (!is_null($rel)) {
+										array_push($relations['n:m'], $rel);
+									}
+								}
+							}
+						}
 					}
 				}
 			}
 		}
 
-		return array_filter ($relations);
+		return $relations;
 	}
 
 
@@ -784,22 +829,82 @@ class BaseModel extends Eloquent
 
 
 	/**
-	 *	Recursive delete of all children objects
+	 * Recursive delete of all children objects
 	 *
-	 *	@author Torsten Schmidt
+	 * @author Torsten Schmidt
 	 *
-	 *	@return bool
+	 * @return bool
 	 *
-	 *  @todo return state on success, should also take care of deleted children
+	 * @todo return state on success, should also take care of deleted children
 	 */
 	public function delete()
 	{
-		// dd( $this->get_all_children() );
-		foreach ($this->get_all_children() as $child)
-			$child->delete();
+		// check from where the deletion request has been triggered and set the correct var to show information
+		$prev = explode('?', \URL::previous())[0];
+		$prev = \Str::lower($prev);
+
+		if ($this->delete_children) {
+			$children = $this->get_all_children();
+			// find and delete all children
+
+			// deletion of 1:n related children is straight forward
+			foreach ($children['1:n'] as $child) {
+
+				// if one direct or indirect child cannot be deleted:
+				// do not delete anything
+				if (!$child->delete()) {
+					$msg = "Cannot delete ".$this->get_model_name()." $this->id because ".$child->get_model_name()." $child->id cannot be deleted";
+					if (\Str::endsWith($prev, 'edit')) {
+						\Session::push('tmp_error_above_relations', $msg);
+					}
+					else {
+						\Session::push('tmp_error_above_index_list', $msg);
+					}
+					return false;
+				}
+			}
+
+			// in n:m relations we have to detach instead of deleting if
+			// child is related to others, too
+			// this should be handled in class methods because BaseModel cannot know the possible problems
+			foreach ($children['n:m'] as $child) {
+				$delete_method = 'deleteNtoM'.$child->get_model_name();
+				if (!$this->{$delete_method}($child)) {
+					$msg = "Cannot delete ".$this->get_model_name()." $this->id because of existing n:m relation with ".$child->get_model_name()." $child->id.";
+					if (\Str::endsWith($prev, 'edit')) {
+						\Session::push('tmp_error_above_relations', $msg);
+					}
+					else {
+						\Session::push('tmp_error_above_index_list', $msg);
+					}
+					return false;
+				}
+			}
+
+		}
 
 		// always return this value (also in your derived classes!)
-		return $this->_delete();
+		$deleted = $this->_delete();
+		if (!$deleted) {
+			$msg = "Could not delete ".$this->get_model_name()." $this->id";
+			if (\Str::endsWith($prev, 'edit')) {
+				\Session::push('tmp_error_above_relations', $msg);
+			}
+			else {
+				\Session::push('tmp_error_above_index_list', $msg);
+			}
+		}
+		else {
+			$msg = "Deleted ".$this->get_model_name()." $this->id";
+			if (\Str::endsWith($prev, 'edit')) {
+				\Session::push('tmp_success_above_relations', $msg);
+			}
+			else {
+				\Session::push('tmp_success_above_index_list', $msg);
+			}
+		}
+
+		return $deleted;
 	}
 
 
