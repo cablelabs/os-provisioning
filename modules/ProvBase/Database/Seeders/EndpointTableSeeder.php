4bf3ce404f2a38952737d446e2302a3cd6ea7bb2
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2018-01-16 15:28:10 +0100

    Added lifecycle test for Endpoint.

--- modules/ProvBase/Database/Seeders/EndpointTableSeeder.php
+++ modules/ProvBase/Database/Seeders/EndpointTableSeeder.php
@@ -2,24 +2,36 @@
 
 namespace Modules\ProvBase\Database\Seeders;
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
 use Modules\ProvBase\Entities\Endpoint;
 
 class EndpointTableSeeder extends \BaseSeeder {
 
 	public function run()
 	{
-		$faker = Faker::create();
-
 		foreach(range(1, self::$max_seed_l2) as $index)
 		{
-			Endpoint::create([
-				'mac' => (rand(0,1) == 1 ? $faker->macAddress() : ''),
-				'description' => $faker->realText(200),
-				'type' => (rand(0,1) == 1 ? 'cpe' : 'mta')
-			]);
+			Endpoint::create(static::get_fake_data('seed'));
 		}
 	}
 
+
+	/**
+	 * Returns an array with faked endpoint data; used e.g. in seeding and testing
+	 *
+	 * @param $topic Context the method is used in (seed|test)
+	 *
+	 * @author Patrick Reichel
+	 */
+	public static function get_fake_data($topic, $contract=null) {
+
+		$faker =& \NmsFaker::getInstance();
+
+		$ret = [
+			'mac' => $faker->macAddress(),
+			'description' => $faker->realText(200),
+			'hostname' => "$faker->domainWord.$faker->domainName",
+		];
+
+		return $ret;
+	}
 }
