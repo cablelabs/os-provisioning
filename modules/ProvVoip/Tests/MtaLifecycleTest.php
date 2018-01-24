7b0968675e1077064abd194646d79a568f5f9c53
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2018-01-24 18:03:46 +0100

    Minor changes in lifecycle tests of Mta and Phonenumber.

--- modules/ProvVoip/Tests/MtaLifecycleTest.php
+++ modules/ProvVoip/Tests/MtaLifecycleTest.php
@@ -10,4 +10,10 @@ class MtaLifecycleTest extends \BaseLifecycleTest {
 	// modem can only be created from Modem.edit
 	protected $create_from_model_context = '\Modules\ProvBase\Entities\Modem';
 
+
+	// fields to be used in update test
+	protected $update_fields = [
+		'mac',
+		'configfile',
+	];
 }
--- modules/ProvVoip/Tests/PhonenumberLifecycleTest.php
+++ modules/ProvVoip/Tests/PhonenumberLifecycleTest.php
@@ -10,4 +10,11 @@ class PhonenumberLifecycleTest extends \BaseLifecycleTest {
 	// modem can only be created from Modem.edit
 	protected $create_from_model_context = '\Modules\ProvVoip\Entities\Mta';
 
+
+	// fields to be used in update test
+	protected $update_fields = [
+		'username',
+		'password',
+	];
+
 }
