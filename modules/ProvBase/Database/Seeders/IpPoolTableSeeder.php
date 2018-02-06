d71a2ab0f9f88d91d0356be2a696b71182d5f6f3
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2018-02-06 18:13:57 +0100

    IpPool lifecycle test running (with caveats!).
    Problem is a complex validation of used IP pools which is hard to
    implement in the seeder – so I disabled testing of create using fake
    data.
    On the other hand it is possible to empty create (without fake data) as
    the controller autofills the create form. Because of this update and
    delete tests are running, too.

--- modules/ProvBase/Database/Seeders/IpPoolTableSeeder.php
+++ modules/ProvBase/Database/Seeders/IpPoolTableSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Modules\ProvBase\Database\Seeders;
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
 use Modules\ProvBase\Entities\IpPool;
 use Modules\ProvBase\Entities\Cmts;
 
@@ -11,24 +9,57 @@ class IpPoolTableSeeder extends \BaseSeeder {
 
 	public function run()
 	{
-		$m = 0;
-		$n = 0;
-
 		foreach(range(1, self::$max_seed) as $index)
 		{
-			IpPool::create([
-				'cmts_id' => CMTS::all()->random(1)->id,
-				'type' => rand(0,3),
-				'net' => '10.'.$m.'.'.$n.'.0',
-				'netmask' => '255.255.255.0',
-				'ip_pool_start' => '10.'.$m.'.'.$n.'.2',
-				'ip_pool_end' => '10.'.$m.'.'.$n.'.253',
-				'router_ip' => '10.'.$m.'.'.$n.'.1',		// = cmts ip
-				'broadcast_ip' => '10.'.$m.'.'.$n.'.255',
-				'dns1_ip' => '10.'.$m.'.'.$n.'.1',
-			]);
-			$m += 10;
+			IpPool::create(static::get_fake_data('seed'));
 		}
 	}
 
+
+	/**
+	 * Returns an array with faked IP pool data; used e.g. in seeding and testing
+	 *
+	 * @param $topic Context the method is used in (seed|test)
+	 * @param $cmts CMTS to create the IP pool at; used in testing
+	 *
+	 * @author Patrick Reichel
+	 */
+	public static function get_fake_data($topic, $cmts=null) {
+
+		$faker =& \NmsFaker::getInstance();
+
+		// in seeding mode: choose random CMTS to create modem at
+		if ($topic == 'seed') {
+			$cmts = CMTS::all()->random(1);
+			$cmts_id = $cmts->id;
+		}
+		else {
+			if (!is_null($cmts)) {
+				$cmts_id = $cmts->id;
+			}
+			else {
+				$cmts_id = null;
+			}
+		}
+
+		$m = $faker->numberBetween(0, 255);
+		$n = $faker->numberBetween(0, 255);
+
+		$ret = [
+			'cmts_id' => CMTS::all()->random(1)->id,
+			'type' => rand(0,3),
+			'net' => '10.'.$m.'.'.$n.'.0',
+			'netmask' => '255.255.255.0',
+			'ip_pool_start' => '10.'.$m.'.'.$n.'.2',
+			'ip_pool_end' => '10.'.$m.'.'.$n.'.253',
+			'router_ip' => '10.'.$m.'.'.$n.'.1',		// = cmts ip
+			'broadcast_ip' => '10.'.$m.'.'.$n.'.255',
+			'dns1_ip' => '10.'.$m.'.'.$n.'.1',
+			'dns2_ip' => $faker->localIpv4(),
+			'dns3_ip' => $faker->localIpv4(),
+			'description' => $faker->sentence(),
+		];
+
+		return $ret;
+	}
 }
