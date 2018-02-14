b2642518498d97ffd24d61791d0a3a9677662522
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-02-14 09:34:58 +0100

    set new php defaults for memory and upload filesize after installation

--- Install/after_install.sh
+++ Install/after_install.sh
@@ -47,6 +47,7 @@ systemctl enable mariadb
 # populate timezone info and set php timezone based on the local one
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
 sed -i "s|^;date.timezone =$|date.timezone = $(timedatectl | grep 'Time zone' | cut -d':' -f2 | xargs | cut -d' ' -f1)|" /etc/php.ini
+sed -i 's/^memory_limit =.*/memory_limit = 1024M/;s/^upload_max_filesize =.*/upload_max_filesize = 50M/' /etc/php.ini
 
 # create mysql db
 mysql -u root -e "CREATE DATABASE nmsprime;"
