ebc67664a21999b33250d64a0262b50c72f1ac34
Christian Schramm <christian.schramm@roetzer-engineering.com>
2018-02-22 17:20:25 +0100

    LAR-100: FIX Dashboard View and add JS to Edit Blade only

--- resources/views/Generic/edit.blade.php
+++ resources/views/Generic/edit.blade.php
@@ -155,6 +155,92 @@
 		});
 	$( $.fn.dataTable.tables(true) ).DataTable().responsive.recalc();
 	});
-	</script>
+
+$(document).ready(function() {
+	var handlePanel = function () {
+	var target = $('.panel').parent('[class*=col]');
+	var targetHandle = '.panel-heading';
+	var connectedTarget = '.row > [class*=col]';
+
+	$(target).sortable({
+		handle: targetHandle,
+		connectWith: connectedTarget,
+		stop: function (event, ui) {
+			ui.item.find('.panel-title').append('<i class="fa fa-refresh fa-spin m-l-5" data-id="title-spinner"></i>');
+			handlePanelPosition(ui.item);
+		}
+	});
+	};
+
+	var handlePanelPosition = function(element) {
+		if ($('.ui-sortable').length !== 0) {
+		var newValue = [];
+		var index = 0;
+		$.when($('.ui-sortable').each(function() {
+			var panelSortableElement = $(this).find('[data-sortable-id]');
+			if (panelSortableElement.length !== 0) {
+				var columnValue = [];
+				$(panelSortableElement).each(function() {
+					var targetSortId = $(this).attr('data-sortable-id');
+					columnValue.push({id: targetSortId});
+				});
+				newValue.push(columnValue);
+			} else {
+				newValue.push([]);
+			}
+			index++;
+		})).done(function() {
+			var targetPage = window.location.href;
+				targetPage = targetPage.split('?');
+				targetPage = targetPage[0].split('/');
+				targetPage = targetPage[targetPage.length - 3];
+			localStorage.setItem(targetPage, JSON.stringify(newValue));
+			$(element).find('[data-id="title-spinner"]').delay(500).fadeOut(500, function() {
+				$(this).remove();
+			});
+		});
+	}
+	};
+
+	var handlePanelStorage = function() {
+		if (typeof(Storage) !== 'undefined' && typeof(localStorage) !== 'undefined') {
+			var targetPage = window.location.href;
+				targetPage = targetPage.split('?');
+				targetPage = targetPage[0].split('/');
+				targetPage = targetPage[targetPage.length - 3];
+		var panelPositionData = localStorage.getItem(targetPage);
+
+		if (panelPositionData) {
+			panelPositionData = JSON.parse(panelPositionData);
+			var i = 0;
+			$.when($('.panel').parent('[class*="col-"]').each(function() {
+				var storageData = panelPositionData[i];
+				var targetColumn = $(this);
+				if (storageData) {
+					$.each(storageData, function(index, data) {
+						var targetId = $('[data-sortable-id="'+ data.id +'"]').not('[data-init="true"]');
+						if ($(targetId).length !== 0) {
+							var targetHtml = $(targetId).clone();
+							$(targetId).remove();
+							$(targetColumn).append(targetHtml);
+							$('[data-sortable-id="'+ data.id +'"]').attr('data-init','true');
+						}
+					});
+				}
+				i++;
+			})).done(function() {
+				window.dispatchEvent(new CustomEvent('localstorage-position-loaded'));
+			});
+		}
+	} else {
+		alert('Your browser is not supported with the local storage');
+	}
+	};
+
+	handlePanel();
+	handlePanelPosition();
+	handlePanelStorage();
+});
+</script>
 @endif
 @stop
