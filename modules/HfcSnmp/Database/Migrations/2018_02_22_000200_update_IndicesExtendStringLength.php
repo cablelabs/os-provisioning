57cc8a026485b15b05e45fd34c481216ce23dd26
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2018-02-22 15:43:33 +0100

    HFC: Extend DB string length for indices

---
+++ modules/HfcSnmp/Database/Migrations/2018_02_22_000200_update_IndicesExtendStringLength.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+
+/**
+ * This is used for Specifying Indices for a Parameter of a NetElement
+ */
+class UpdateIndicesExtendStringLength extends BaseMigration {
+
+	// name of the table to create
+	protected $tablename = "indices";
+
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table($this->tablename, function(Blueprint $table)
+		{
+			$table->string('indices', 1024)->change();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Dont revert as we need this already
+	}
+
+}
