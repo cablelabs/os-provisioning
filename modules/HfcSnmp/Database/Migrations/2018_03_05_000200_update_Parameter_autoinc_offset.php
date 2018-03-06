c4e2f19de8da9b5ad62a1d2b92065f8885c1afe5
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-03-06 13:10:36 +0100

    leave some space for netelementtypes defined by upstream

---
+++ modules/HfcSnmp/Database/Migrations/2018_03_05_000200_update_Parameter_autoinc_offset.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Modules\HfcReq\Entities\NetElementType;
+
+class UpdateParameterAutoincOffset extends Migration {
+
+    // name of the table to create
+    protected $tablename = "parameter";
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // let netelementtype id start with 1000, so that we can add enough upstream netelementtypes <1000,
+        // which won't collide with user defined ones
+        DB::statement("UPDATE $this->tablename SET netelementtype_id = netelementtype_id + 1000 where netelementtype_id > 6;");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+}
