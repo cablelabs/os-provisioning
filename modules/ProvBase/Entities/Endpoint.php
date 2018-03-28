3946081680e33e6df7a367c6c90df54fbd5dea2d
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-03-28 09:56:53 +0200

    add and delete A, CNAME and PTR for endpoints, delete them for expired/released leases as well

--- modules/ProvBase/Entities/Endpoint.php
+++ modules/ProvBase/Entities/Endpoint.php
@@ -4,6 +4,7 @@ namespace Modules\ProvBase\Entities;
 
 use File;
 use Modules\ProvBase\Entities\Cmts;
+use Modules\ProvBase\Entities\ProvBase;
 
 class Endpoint extends \BaseModel {
 
@@ -69,6 +70,41 @@ class Endpoint extends \BaseModel {
 	}
 
 
+	public function nsupdate($del = false)
+	{
+		$cmd = '';
+		$zone = ProvBase::first()->domain_name;
+
+		if($del) {
+			if ($this->getOriginal()['fixed_ip'] && $this->getOriginal()['ip']) {
+				$rev = implode('.', array_reverse(explode('.', $this->getOriginal()['ip'])));
+				$cmd .= "update delete {$this->getOriginal()['hostname']}.cpe.$zone.\nsend\n";
+				$cmd .= "update delete $rev.in-addr.arpa.\nsend\n";
+			} else {
+				$mangle = exec("echo \"{$this->getOriginal()['mac']}:{$this->modem->mac}\" | tr -cd '[:xdigit:]' | xxd -r -p | openssl dgst -sha256 -macopt hexkey:$(cat /etc/named-ddns-cpe.key) -binary | python -c 'import base64; import sys; print(base64.b32encode(sys.stdin.read())[:6].lower())'");
+				$cmd .= "update delete {$this->getOriginal()['hostname']}.cpe.$zone.\nsend\n";
+				$cmd .= "update delete $mangle.cpe.$zone.\nsend\n";
+			}
+		} else {
+			if ($this->fixed_ip && $this->ip) {
+				// endpoints with a fixed-address will get an A and PTR record (ip <-> hostname)
+				$rev = implode('.', array_reverse(explode('.', $this->ip)));
+				$cmd .= "update add $this->hostname.cpe.$zone. 3600 A $this->ip\nsend\n";
+				$cmd .= "update add $rev.in-addr.arpa. 3600 PTR $this->hostname.cpe.$zone.\nsend\n";
+			} else {
+				// other endpoints will get a CNAME and PTR record (mangle <-> hostname)
+				// mangle name is based on cm and cpe mac
+				$mangle = exec("echo \"$this->mac:{$this->modem->mac}\" | tr -cd '[:xdigit:]' | xxd -r -p | openssl dgst -sha256 -macopt hexkey:$(cat /etc/named-ddns-cpe.key) -binary | python -c 'import base64; import sys; print(base64.b32encode(sys.stdin.read())[:6].lower())'");
+				$cmd .= "update add $this->hostname.cpe.$zone. 3600 CNAME $mangle.cpe.$zone.\nsend\n";
+				$cmd .= "update add $mangle.cpe.$zone. 3600 PTR $this->hostname.cpe.$zone.\nsend\n";
+			}
+		}
+
+		$pw = env("DNS_PASSWORD");
+		$handle = popen("/usr/bin/nsupdate -v -l -y dhcpupdate:$pw", 'w');
+		fwrite($handle, $cmd);
+		pclose($handle);
+	}
 
 	/**
 	 * BOOT:
@@ -118,17 +154,25 @@ class EndpointObserver {
 	{
 		$endpoint->make_dhcp();
 		Cmts::make_dhcp_conf_all();
+		$endpoint->nsupdate();
+	}
+
+	public function updating($endpoint)
+	{
+		$endpoint->nsupdate(true);
 	}
 
 	public function updated($endpoint)
 	{
 		$endpoint->make_dhcp();
 		Cmts::make_dhcp_conf_all();
+		$endpoint->nsupdate();
 	}
 
 	public function deleted($endpoint)
 	{
 		$endpoint->make_dhcp();
 		Cmts::make_dhcp_conf_all();
+		$endpoint->nsupdate(true);
 	}
 }
