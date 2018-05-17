226d137f94e49976dcae14a07b2ee9cc0b65caff
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-05-17 14:40:44 +0200

    we don't need to replace php54 with php56, since we are now using php71 from software collections

--- INSTALL-REPO.sh
+++ INSTALL-REPO.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 # add epel
-# NOTE: IUS requires epel
 yum install -y epel-release
-
-# add IUS repo
-# Note: Whitelist – only use php56u and yum-replace-plugin from IUS
-rpm -ivh https://centos7.iuscommunity.org/ius-release.rpm
-sed -i 's/\[ius\]/\[ius\]\nincludepkgs=php56u* yum-plugin-replace/' /etc/yum.repos.d/ius.repo
 
 # add Icinga repo
 rpm -ivh https://packages.icinga.com/epel/icinga-rpm-release-7-latest.noarch.rpm
 
 # add nmsprime repo
 curl https://raw.githubusercontent.com/schmto/nmsprime/master/nmsprime.repo -Lo /etc/yum.repos.d/nmsprime.repo
-
-# install and replace php56u
-yum install -y php yum-plugin-replace
-yum replace -y php --replace-with php56u
 
 # enable software collections, since icingaweb2 depends on php7.1 from there
 yum install -y centos-release-scl
