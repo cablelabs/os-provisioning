e72b965b7fa23fc3506ee6bc6d091b8c7b1c8aac
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2018-07-03 14:22:42 +0200

    Bugfix: Fix wrong translation array keys

--- modules/ProvBase/Entities/Modem.php
+++ modules/ProvBase/Entities/Modem.php
@@ -593,7 +593,7 @@ class Modem extends \BaseModel {
 			else throw new Exception("CMTS company not set");
 
 			// success message
-			\Session::push('tmp_info_above_form', trans('messages.modem_restart_cmts'));
+			\Session::push('tmp_info_above_form', trans('messages.modem_restart_success_cmts'));
 		}
 		catch (Exception $e)
 		{
@@ -604,7 +604,7 @@ class Modem extends \BaseModel {
 				snmpset($fqdn, $config->rw_community, '1.3.6.1.2.1.69.1.1.3.0', 'i', '1', 300000, 1);
 
 				// success message - make it a warning as sth is wrong when it's not already restarted by CMTS??
-				\Session::push('tmp_info_above_form', trans('messages.modem_restart_direct'));
+				\Session::push('tmp_info_above_form', trans('messages.modem_restart_success_direct'));
 			} catch (Exception $e) {
 				\Log::error("Could not restart $this->hostname directly ('".$e->getMessage()."')");
 
