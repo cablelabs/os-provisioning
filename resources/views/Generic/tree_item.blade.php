234c6b0d9fc6b3a57c659624758de6ef906ee025
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-07-17 10:21:50 +0200

    add indentation, no functional change

--- resources/views/Generic/tree_item.blade.php
+++ resources/views/Generic/tree_item.blade.php
@@ -5,21 +5,21 @@
 
 <ul>
 @foreach($items as $item)
-	<?php
-		$type = method_exists($item, 'get_icon_type') ? $item->get_icon_type() : $color_classes[$color % 4];
-	?>
-	<li id="ids[{{$item->id}}]"
-		class="f-s-14 p-t-5 {{in_array($item->id, $undeletables) ? 'nocheck' : ''}}
-			{{ in_array($item->id, $undeletables) && $item->parent_id ? 'p-l-25' : ''}}"
-		data-jstree='{"type":"{!! $type !!}" }'>
+		<?php
+			$type = method_exists($item, 'get_icon_type') ? $item->get_icon_type() : $color_classes[$color % 4];
+		?>
+		<li id="ids[{{$item->id}}]"
+			class="f-s-14 p-t-5 {{in_array($item->id, $undeletables) ? 'nocheck' : ''}}
+				{{ in_array($item->id, $undeletables) && $item->parent_id ? 'p-l-25' : ''}}"
+			data-jstree='{"type":"{!! $type !!}" }'>
 
-		{!! HTML::linkRoute("$route_name.edit", $item->view_index_label(), $item->id) !!}
+			{!! HTML::linkRoute("$route_name.edit", $item->view_index_label(), $item->id) !!}
 
-		@if($item->children()->count() > 0)
-			@include('Generic.tree_item', array('items' => $item->children, 'color' => $color++))
-		@else
-			<?php $color++; ?>
-		@endif
-	</li>
+			@if($item->children()->count() > 0)
+				@include('Generic.tree_item', array('items' => $item->children, 'color' => $color++))
+			@else
+				<?php $color++; ?>
+			@endif
+		</li>
 @endforeach
 </ul>
