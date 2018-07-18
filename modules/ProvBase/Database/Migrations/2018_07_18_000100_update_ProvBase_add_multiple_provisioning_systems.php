c1f4b935519307d809ce9379e249ec33b9729749
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2018-07-18 16:23:30 +0200

    Added database field for multiple provisioning systems.

---
+++ modules/ProvBase/Database/Migrations/2018_07_18_000100_update_ProvBase_add_multiple_provisioning_systems.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProvBaseAddMultipleProvisioningSystems extends BaseMigration {
+
+	// name of the table to create
+	protected $tablename = 'provbase';
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table($this->tablename, function(Blueprint $table)
+		{
+			$table->boolean('multiple_provisioning_systems');
+		});
+
+		DB::update("UPDATE $this->tablename SET multiple_provisioning_systems = 0;");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table($this->tablename, function(Blueprint $table)
+		{
+			$table->dropColumn('multiple_provisioning_systems');
+		});
+	}
+
+}
