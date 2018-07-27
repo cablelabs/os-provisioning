84a44e1cf6d02788c97efaea3e373f353ad7d6b8
Christian Schramm <christian.schramm@roetzer-engineering.com>
2018-07-27 12:35:56 +0200

    Refresh Session Key if a new Cluster is set.

--- modules/HfcReq/Entities/NetElement.php
+++ modules/HfcReq/Entities/NetElement.php
@@ -226,13 +226,13 @@ class NetElement extends \BaseModel {
 	 */
 	public function get_all_cluster_to_net ()
 	{
-		if (Session::has('Net-' . $this->name))
-			return Session::get('Net-' . $this->name);
+		if (Session::has('Net-' . $this->id))
+			return Session::get('Net-' . $this->id);
 
 		$cluster_id = array_search('Cluster', NetElementType::$undeletables);
 		$return = NetElement::where('netelementtype_id', '=', $cluster_id)->where('net', '=', $this->id)->orderBy('name')->get();
 
-		Session::put('Net-' . $this->name, $return);
+		Session::put('Net-' . $this->id, $return);
 
 		return $return;
 	}
@@ -287,7 +287,7 @@ class NetElement extends \BaseModel {
 
 	public function get_native_net ()
 	{
-		return $this->_get_native_helper('Net');
+		return $this->_get_native_helper();
 	}
 
 	public function get_native_cmts ()
@@ -420,6 +420,8 @@ class NetElementObserver
 		if (!$netelement->observer_enabled)
 			return;
 
+		$this->handleSidebarClusters($netelement);
+
 		// if ($netelement->is_type_cluster())
 		// in created because otherwise netelement does not have an ID yet
 		$netelement->net 	 = $netelement->get_native_net();
@@ -432,6 +434,9 @@ class NetElementObserver
 	{
 		if (!$netelement->observer_enabled)
 			return;
+
+		if($netelement['original']['parent_id'] != $netelement['attributes']['parent_id'])
+			$this->handleSidebarClusters($netelement, 1);
 
 		$netelement->net 	 = $netelement->get_native_net();
 		$netelement->cluster = $netelement->get_native_cluster();
@@ -451,10 +456,30 @@ class NetElementObserver
 				else {
 					// Show Alert that not all indices could be assigned to the new parameter -> user has to create new indices and delete the old ones
 					// We also could delete them directly, so that user has to add them again
-					\Session::put('info', trans('messages.indices_unassigned'));
+					Session::put('info', trans('messages.indices_unassigned'));
 				}
 			}
 		}
 	}
 
+	public function deleted($netelement)
+	{
+		$this->handleSidebarClusters($netelement);
+	}
+
+	protected function handleSidebarClusters($netelement, $isUpdating = 0)
+	{
+		if ($netelement->is_type_cluster()) {
+			$netId = $netelement->get_native_net();
+
+			if ($isUpdating) {
+				$oldNetId = NetElement::find($netelement['original']['parent_id'])->get_native_net();
+				$netId = NetElement::find($netelement['attributes']['parent_id'])->get_native_net();
+
+				$oldNetId ? Session::forget('Net-' . $oldNetId) : '';
+			}
+
+			$netId ? Session::forget('Net-' . $netId) : Session::forget('Net-' . $netelement->id);
+		}
+	}
 }
