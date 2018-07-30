904ced93504a3d97be0f8d14f4932f8ccc465d51
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-07-30 10:05:49 +0200

    nms:auth is auth:nms now, change invocations accordingly

--- Install/module_after_install.sh
+++ Install/module_after_install.sh
@@ -6,6 +6,6 @@ dir="/var/www/nmsprime"
 cd "$dir"
 /opt/rh/rh-php71/root/usr/bin/php artisan module:publish
 /opt/rh/rh-php71/root/usr/bin/php artisan module:migrate
-/opt/rh/rh-php71/root/usr/bin/php artisan nms:auth
+/opt/rh/rh-php71/root/usr/bin/php artisan auth:nms
 
 chown -R apache $dir/storage $dir/bootstrap/cache
