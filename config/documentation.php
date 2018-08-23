5d8e148396090ce14f4e6ff25443d36e57261425
Torsten Schmidt <schmto@roetzer-engineering.com>
2018-08-23 12:51:09 +0200

    Help-Integration: move docu button in seperate file

--- config/documentation.php
+++ config/documentation.php
@@ -3,4 +3,8 @@ return[
 	'cmts' => [	'doc' => 'https://devel.roetzer-engineering.com/confluence/display/NMS/CMTS+setup',
 				'youtube' => 'https://www.youtube.com/watch?v=g-OChuE3mf4',
 				'url' => null],
+
+	'configfile' => [	'doc' => null, // todo
+				'youtube' => 'https://www.youtube.com/watch?v=aYjuWXhaV3s&t=500s', // jump to 500s
+				'url' => null],
 	];
