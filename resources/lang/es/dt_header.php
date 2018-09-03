2b9b109651a531471765ba51ede1df0a0fecbfdb
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2018-09-03 11:02:40 +0200

    Translation (#238)
    * New translations passwords.php (German)
    
    * New translations view.php (Bengali)
    
    * New translations view.php (Bengali, India)
    
    * New translations view.php (Bengali)
    
    * New translations view.php (Bengali, India)
    
    * update style of translations
    
    * New translations view.php (Portuguese, Brazilian)
    
    * New translations dt_header.php (Chinese Simplified)
    
    * New translations messages.php (Chinese Simplified)
    
    * New translations view.php (Chinese Simplified)
    
    * New translations messages.php (Bengali)
    
    * New translations dt_header.php (Bengali, India)
    
    * New translations view.php (Bengali)
    
    * New translations dt_header.php (Bengali)
    
    * New translations view.php (Bengali, India)
    
    * New translations view.php (Chinese Traditional)
    
    * New translations view.php (Portuguese)
    
    * New translations view.php (Portuguese, Brazilian)
    
    * New translations view.php (Chinese Simplified)
    
    * New translations dt_header.php (Chinese Traditional)
    
    * New translations messages.php (Bengali, India)
    
    * New translations dt_header.php (Bengali, India)
    
    * New translations view.php (Bengali)
    
    * New translations dt_header.php (Bengali)
    
    * New translations view.php (Chinese Traditional)
    
    * New translations view.php (Bengali, India)
    
    * New translations messages.php (Chinese Traditional)
    
    * New translations view.php (Portuguese, Brazilian)
    
    * New translations dt_header.php (Spanish)
    
    * New translations messages.php (Spanish)
    
    * New translations messages.php (Spanish)

--- resources/lang/es/dt_header.php
+++ resources/lang/es/dt_header.php
@@ -70,7 +70,7 @@ return [
     // NetElement
     'netelement.id' => 'ID',
     'netelement.name' => 'Elemento de red',
-    'netelement.ip' => 'IP Adress',
+    'netelement.ip' => 'Direccion IP',
     'netelement.state' => 'Estado',
     'netelement.pos' => 'Posición',
     // NetElementType
