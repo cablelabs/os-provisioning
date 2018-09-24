5b08cd14797ce4bef2a50b4dcc88b7d1cb5fd2f5
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-09-24 09:15:20 +0200

    adapt style-ci

--- routes/web.php
+++ routes/web.php
@@ -43,31 +43,30 @@ BaseRoute::group([], function () {
     BaseRoute::resource('Sla', 'SlaController');
 
     // As we dont want authorization middleware (only authentication) we can not set the routes with our general function and have to set them manually
-    BaseRoute::get("SupportRequest", [
-        'uses' => "SupportRequestController@index",
-        'as'   => "SupportRequest.index",
+    BaseRoute::get('SupportRequest', [
+        'uses' => 'SupportRequestController@index',
+        'as'   => 'SupportRequest.index',
         ]);
-    BaseRoute::post("SupportRequest", [
-        'uses' => "SupportRequestController@store",
-        'as'   => "SupportRequest.store",
+    BaseRoute::post('SupportRequest', [
+        'uses' => 'SupportRequestController@store',
+        'as'   => 'SupportRequest.store',
         ]);
-    BaseRoute::get("SupportRequest/create", [
-        'uses' => "SupportRequestController@create",
-        'as'   => "SupportRequest.create",
+    BaseRoute::get('SupportRequest/create', [
+        'uses' => 'SupportRequestController@create',
+        'as'   => 'SupportRequest.create',
         ]);
-    BaseRoute::get("SupportRequest/{id}", [
-        'uses' => "SupportRequestController@edit",
-        'as'   => "SupportRequest.edit",
+    BaseRoute::get('SupportRequest/{id}', [
+        'uses' => 'SupportRequestController@edit',
+        'as'   => 'SupportRequest.edit',
         ]);
-    BaseRoute::put("SupportRequest/{id}", [
-        'uses' => "SupportRequestController@update",
-        'as'   => "SupportRequest.update",
+    BaseRoute::put('SupportRequest/{id}', [
+        'uses' => 'SupportRequestController@update',
+        'as'   => 'SupportRequest.update',
         ]);
-    BaseRoute::get("SupportRequest/{id}/log", [
+    BaseRoute::get('SupportRequest/{id}/log', [
         'uses' => '\App\Http\Controllers\GuiLogController@filter',
-        'as' => "SupportRequest.guilog",
+        'as' => 'SupportRequest.guilog',
     ]);
-
 
     BaseRoute::get('base/fulltextSearch', [
         'as' => 'Base.fulltextSearch',
@@ -82,36 +81,35 @@ BaseRoute::group([], function () {
     BaseRoute::get('profile/{id}', [
         'as' => 'User.profile',
         'uses' => 'Auth\UserController@edit',
-        'middleware' => ["owns:view,App\User"],
+        'middleware' => ['owns:view,App\User'],
     ]);
 
     BaseRoute::post('user/detach/{id}/{func}', [
         'as' => 'user.detach',
         'uses' => 'Auth\UserController@detach',
-        'middleware' => ["can:delete,App\User"],
+        'middleware' => ['can:delete,App\User'],
     ]);
 
     BaseRoute::post('Role/customAbility', [
         'as' => 'customAbility.update',
         'uses' => 'Auth\AbilityController@updateCustomAbility',
-        'middleware' => ["can:update,App\Role"],
+        'middleware' => ['can:update,App\Role'],
     ]);
 
     BaseRoute::post('Role/modelAbility', [
         'as' => 'modelAbility.update',
         'uses' => 'Auth\AbilityController@updateModelAbility',
-        'middleware' => ["can:update,App\Role"],
+        'middleware' => ['can:update,App\Role'],
     ]);
 
     BaseRoute::get('Guilog/restore/{id}', [
         'as' => 'Guilog.restore',
         'uses' => 'GuiLogController@restoreModel',
-        'middleware' => ["can:delete,App\Role"],
+        'middleware' => ['can:delete,App\Role'],
     ]);
 
     BaseRoute::post('Sla/clicked', [
         'as' => 'Sla.clicked_sla',
         'uses' => 'SlaController@clicked_sla',
     ]);
-
 });
