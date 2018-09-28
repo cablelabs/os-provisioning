f6ac1cf804482f5fcf839aa9dc9826f0848b7b0b
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-09-28 16:17:36 +0200

    add install migration for deprecated SelfHandling interface

---
+++ database/migrations/2018_09_28_000100_install_remove_self_handling.php
@@ -0,0 +1,53 @@
+<?php
+
+
+class InstallRemoveSelfHandling extends BaseMigration
+{
+    protected $tablename = '';
+
+    protected $files = [
+        '/modules/BillingBase/Console/accountingCommand.php',
+        '/modules/HfcCustomer/Console/MpsCommand.php',
+        '/modules/ProvBase/Console/configfileCommand.php',
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->files as $file) {
+            $file = base_path().$file;
+            $str = file_get_contents($file);
+            $str = preg_replace(
+                "/use Illuminate\\\\Console\\\\Command;\nuse Illuminate\\\\Contracts\\\\Bus\\\\SelfHandling;/",
+                'use Illuminate\\Console\\Command;',
+                $str
+            );
+            $str = preg_replace('/implements SelfHandling, ShouldQueue/', 'implements ShouldQueue', $str);
+            file_put_contents($file, $str);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->files as $file) {
+            $file = base_path().$file;
+            $str = file_get_contents($file);
+            $str = preg_replace(
+                '/use Illuminate\\\\Console\\\\Command;/',
+                "use Illuminate\\Console\\Command;\nuse Illuminate\\Contracts\\Bus\\SelfHandling;",
+                $str
+            );
+            $str = preg_replace('/implements ShouldQueue/', 'implements SelfHandling, ShouldQueue', $str);
+            file_put_contents($file, $str);
+        }
+    }
+}
