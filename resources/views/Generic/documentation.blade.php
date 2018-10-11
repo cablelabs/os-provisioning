3ac0a8c9274c607d576b1e6cb9ff24932abe44cc
Roy Schneider <roy.schneider@roetzer-engineering.com>
2018-10-11 08:27:13 +0200

    title of the support button now displays the correct text

--- resources/views/Generic/documentation.blade.php
+++ resources/views/Generic/documentation.blade.php
@@ -1,7 +1,7 @@
         {{--Help Section--}}
     <div class="align-self-end m-r-20 dropdown btn-group">
         <button type="button" class="btn btn-outline m-b-10 float-right dropdown-toggle" style="simple" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-toggle="tooltip" data-delay='{"show":"250"}' data-placement="top"
-            title="{{ \App\Http\Controllers\BaseViewController::translate_view('Delete', 'Button' ) }}" form="IndexForm" name="_delete">
+            title="{{ trans('messages.support') }}" form="IndexForm" name="support">
             <i class="fa fa-question fa-2x" aria-hidden="true"></i>
         </button>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
