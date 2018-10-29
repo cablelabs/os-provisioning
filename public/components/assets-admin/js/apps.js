30a290c4f4259d21167222f0756bd18bb04d7f14
Gitrosa <brausetinte34@googlemail.com>
2018-10-29 10:30:11 +0100

    Fixes for buttons css and comment in apps.js in terms of update

--- public/components/assets-admin/js/apps.js
+++ public/components/assets-admin/js/apps.js
@@ -1,4 +1,6 @@
 /*
+For updating this file also see commit 2042e79 
+
 Template Name: Color Admin - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.7 & Bootstrap 4.0.0-Alpha 6
 Version: 3.0.0
 Author: Sean Ngu
