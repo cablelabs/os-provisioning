1eec59b1025b9949493282ac50083a20431eb952
Roy Schneider <roy.schneider@roetzer-engineering.com>
2019-01-16 12:17:00 +0100

    add option to set name and address of noreply sender in Global Config Page

---
+++ database/migrations/2019_01_15_151246_addTicketConfigToGlobalConfig.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Modules\Ticketsystem\Entities\Ticket;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTicketConfigToGlobalConfig extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('global_config', function (Blueprint $table) {
+            $table->string('noReplyMail');
+        });
+
+        Schema::table('global_config', function (Blueprint $table) {
+            $table->string('noReplyName');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('global_config', function (Blueprint $table) {
+            $table->dropColumn('noReplyMail');
+        });
+
+        Schema::table('global_config', function (Blueprint $table) {
+            $table->dropColumn('noReplyName');
+        });
+    }
+}
