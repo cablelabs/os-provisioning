2e3449d2eeaa9d75c6bf9ef43cb51f27078f15f0
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-02-07 17:18:24 +0100

    Bugfix: Contracts of yesterday have to be considered\!

--- modules/ProvBase/Console/contractCommand.php
+++ modules/ProvBase/Console/contractCommand.php
@@ -44,7 +44,7 @@ class contractCommand extends Command
             return false;
         }
 
-        $cs = Contract::where(whereLaterOrEqualThanDate('contract_end', date('Y-m-d')))->get();
+        $cs = Contract::where(whereLaterOrEqualThanDate('contract_end', date('Y-m-d', strtotime('-2 day'))))->get();
 
         $i = 1;
         $num = count($cs);
