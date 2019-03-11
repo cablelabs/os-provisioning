7312ea66540987d6a8a511bc3efdb8c963cf4b71
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-03-11 11:14:05 +0100

    Add option to let a panel be collapsed on page load for view_has_many version 2

--- resources/views/bootstrap/panel.blade.php
+++ resources/views/bootstrap/panel.blade.php
@@ -4,13 +4,15 @@
 		$style = ($height == 'auto') ? '' : "height:$height%";
 
 	$overflow_y = isset($overflow) ? $overflow : 'auto';
+
+	$display = isset($options['display']) ? 'display: '.$options['display'] : '';
 ?>
 
 {{-- begin col-dyn --}}
 <div class="col-md-{{$md}} ui-sortable">
 	<div class="panel panel-inverse card-2 d-flex flex-column" data-sortable-id="table-index-{{ isset($i) ? $i : '1'}}">
 		@include ('bootstrap.panel-header', ['view_header' => $view_header])
-		<div class="panel-body fader d-flex flex-column" style="overflow-y:{{ $overflow_y }}; {{ $style }}">
+		<div class="panel-body fader d-flex flex-column" style="overflow-y:{{ $overflow_y }}; {{ $style }}; {{ $display }}">
 			@yield($content)
 		</div>
 	</div>
