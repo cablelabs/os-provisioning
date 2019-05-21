710e2ea192ec379c22247dd07d2b041d12877201
Ole Ernst <ole.ernst@roetzer-engineering.com>
2019-05-21 08:57:04 +0200

    skip private ip addresses, halving the execution time

--- modules/ProvBase/Console/cpeHostnameCommand.php
+++ modules/ProvBase/Console/cpeHostnameCommand.php
@@ -33,7 +33,7 @@ class cpeHostnameCommand extends Command
     }
 
     /**
-     * Execute the console command - Create global Config & all Entries for Modems, Endpoints & Mtas to get an IP from Server
+     * Execute the console command
      *
      * @return mixed
      */
@@ -55,6 +55,11 @@ class cpeHostnameCommand extends Command
         // get the required parameters and run named-ddns.sh for every cpe
         foreach ($leases[0] as $lease) {
             if (preg_match('/;\s*binding state active;.*set ip = "([^"]*).*set hw_mac = "([^"]*)/s', $lease, $match)) {
+                $octets = explode('.', $match[1]);
+                if ((! filter_var($match[1], FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE)) ||
+                    ($octets[0] == 100 && $octets[0] >= 64 && $octets[0] <= 127)) {
+                    continue;
+                }
                 exec("/etc/named-ddns.sh $match[2] $match[1] 0\n");
             }
         }
