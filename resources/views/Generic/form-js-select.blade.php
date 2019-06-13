c9dc82984fae368d907b2c6bb39f7f3c0fc4ea1f
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-06-13 17:08:29 +0200

    Intendation commit

--- resources/views/Generic/form-js-select.blade.php
+++ resources/views/Generic/form-js-select.blade.php
@@ -10,21 +10,21 @@ NOTE: will be used from form-js blade and must be called inside java context
 --}}
 
 <?php
-	// generate hide all variable, to hide all classes
-	$hide_all = '';
-	foreach ($field['select'] as $select)
-		$hide_all .= ' $(".'.$select.'").hide();';
+    // generate hide all variable, to hide all classes
+    $hide_all = '';
+    foreach ($field['select'] as $select)
+        $hide_all .= ' $(".'.$select.'").hide();';
 ?>
 
 {!! $hide_all !!}
 
 {{-- handle select fields --}}
 @if ($field['form_type'] == 'select')
-	@foreach($field['select'] as $val => $hide)
-		<?php $val = is_string($val) ? $val = '"'.$val.'"' : $val; ?>
-		if ($('#{!! $field['name'] !!}').val() == {!! $val !!})
-		{
-			$(".{{$hide}}").show();
-		}
-	@endforeach
+    @foreach($field['select'] as $val => $hide)
+        <?php $val = is_string($val) ? $val = '"'.$val.'"' : $val; ?>
+        if ($('#{!! $field['name'] !!}').val() == {!! $val !!})
+        {
+            $(".{{$hide}}").show();
+        }
+    @endforeach
 @endif
--- resources/views/Layout/split-nopanel.blade.php
+++ resources/views/Layout/split-nopanel.blade.php
@@ -2,39 +2,39 @@
 
 @section ('content')
 
-	<div class="card card-inverse col-12 p-b-5 p-t-10">
-		@if(isset($tabs))
-		<div class="card-header m-b-15">
-			<ul class="nav nav-tabs card-header-tabs d-flex">
-				@foreach ($tabs as $tab)
-					@if ($tab['route'] == Route::getCurrentRoute()->getName())
-						<?php
-							$class = 'active';
-							$blade = \Input::get('blade');
-							if (is_null($blade))
-							{
-								$class = !isset($tab['link'][1]) || $tab['link'][1] == 'blade=0' ? 'active' : '';
-							}
-							else if (isset($tab['link'][1]))
-							{
-								$class = 'blade='.$blade == $tab['link'][1] ? 'active' : '';
-							}
-						?>
-						<li class="nav-item {{$class}}" role="tab"> {{ HTML::linkRoute($tab['route'], $tab['name'], $tab['link']) }}</li>
-					@elseif ($tab['name'] == "Logging")
-						<li class="nav-item order-12 ml-auto" role="tab" style="float: right"><a id="loggingtab" class="" href="#logging" data-toggle="tab"> Logging</a></li>
-					@else
-						<li role="tab"> {{ HTML::linkRoute($tab['route'], $tab['name'], $tab['link']) }}</li>
-					@endif
-				@endforeach
-			</ul>
-		</div>
-		@endif
-		<div class="row">
-			<div class="card card-inverse col-md-{{(!isset($relations) || empty($relations)) ? '12' : $edit_left_md_size}}">
-				@yield('content_left')
-			</div>
-			@yield('content_right')
-		</div>
-	</div>
+    <div class="card card-inverse col-12 p-b-5 p-t-10">
+        @if(isset($tabs))
+        <div class="card-header m-b-15">
+            <ul class="nav nav-tabs card-header-tabs d-flex">
+                @foreach ($tabs as $tab)
+                    @if ($tab['route'] == Route::getCurrentRoute()->getName())
+                        <?php
+                            $class = 'active';
+                            $blade = \Input::get('blade');
+                            if (is_null($blade))
+                            {
+                                $class = !isset($tab['link'][1]) || $tab['link'][1] == 'blade=0' ? 'active' : '';
+                            }
+                            else if (isset($tab['link'][1]))
+                            {
+                                $class = 'blade='.$blade == $tab['link'][1] ? 'active' : '';
+                            }
+                        ?>
+                        <li class="nav-item {{$class}}" role="tab"> {{ HTML::linkRoute($tab['route'], $tab['name'], $tab['link']) }}</li>
+                    @elseif ($tab['name'] == "Logging")
+                        <li class="nav-item order-12 ml-auto" role="tab" style="float: right"><a id="loggingtab" class="" href="#logging" data-toggle="tab"> Logging</a></li>
+                    @else
+                        <li role="tab"> {{ HTML::linkRoute($tab['route'], $tab['name'], $tab['link']) }}</li>
+                    @endif
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <div class="row">
+            <div class="card card-inverse col-md-{{(!isset($relations) || empty($relations)) ? '12' : $edit_left_md_size}}">
+                @yield('content_left')
+            </div>
+            @yield('content_right')
+        </div>
+    </div>
 @stop
