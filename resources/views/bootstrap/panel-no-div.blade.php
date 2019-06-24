fd209f5b4ff625faa4a3d403db8eece25ce431a4
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-06-24 17:28:31 +0200

    Clean up: Remove redundant 'panel-no-div' blade

--- resources/views/bootstrap/panel-no-div.blade.php
+++
@@ -1,7 +0,0 @@
-		<div class="panel panel-inverse card-3" data-sort-id="{{ $tab['name'] . '-' . $view }}">
-			@include ('bootstrap.panel-header', ['view_header' => $view_header])
-
-			<div class="panel-body fader">
-				@yield($content)
-			</div>
-		</div>
