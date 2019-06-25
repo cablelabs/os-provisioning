94a22f038fdd967be16fe85c7011d8d40b67c06c
Ole Ernst <ole.ernst@roetzer-engineering.com>
2019-06-25 14:34:07 +0200

    rotate application key on every new nmsprime release

--- Install/after_upgrade.sh
+++ Install/after_upgrade.sh
@@ -2,8 +2,12 @@
 source scl_source enable rh-php71
 
 dir="/var/www/nmsprime"
-
 cd "$dir"
+
+# see https://laravel.com/docs/5.5/upgrade#upgrade-5.5.42
+key=$(/opt/rh/rh-php71/root/usr/bin/php artisan key:generate | grep -o '\[.*\]' | tr -d '[]')
+sed -i "s|APP_KEY=.*|APP_KEY=$key|" /etc/nmsprime/env/global.env
+
 /opt/rh/rh-php71/root/usr/bin/php artisan clear-compiled
 /opt/rh/rh-php71/root/usr/bin/php artisan optimize
 /opt/rh/rh-php71/root/usr/bin/php artisan migrate
