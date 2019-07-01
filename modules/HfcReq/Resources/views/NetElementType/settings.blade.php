526d8a05c7f7794384458a9cec466f5d8a912a46
Christian Schramm <christian.schramm@roetzer-engineering.com>
2019-07-01 15:09:52 +0200

    Optimize NetElementType Settings blade

--- modules/HfcReq/Resources/views/NetElementType/settings.blade.php
+++ modules/HfcReq/Resources/views/NetElementType/settings.blade.php
@@ -1,26 +1,22 @@
 {!! Form::open(['route' => ['NetElementType.settings', $view_var->id], 'method' => 'post']) !!}
 
-    @php
-        $list = Modules\HfcReq\Entities\NetElementType::param_list($view_var->id);
-    @endphp
-
     <div class="col-md-12">
     <div class="form-group row">
-    {!! Form::label('param_id', 'Choose Parameter') !!}
-    {!! Form::select('param_id[]', $list, null , ['multiple' => 'multiple']) !!}
+        {!! Form::label('param_id', 'Choose Parameter') !!}
+        {!! Form::select('param_id[]', $list, null , ['multiple' => 'multiple']) !!}
     <br><br><br>
     </div></div>
 
     <div class="col-md-12">
     <div class="form-group row">
-    {!! Form::label('html_frame', 'HTML Frame ID') !!}
-    {!! Form::text('html_frame') !!}
+        {!! Form::label('html_frame', 'HTML Frame ID') !!}
+        {!! Form::text('html_frame') !!}
     </div></div>
 
     <div class="col-md-12">
     <div class="form-group row">
-    {!! Form::label('html_id', 'Order ID') !!}
-    {!! Form::text('html_id') !!}
+        {!! Form::label('html_id', 'Order ID') !!}
+        {!! Form::text('html_id') !!}
     </div></div>
 
     {!! Form::submit('Set Value(s)') !!}
