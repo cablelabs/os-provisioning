0921ab7b935bef8e6282459bf89a6f41ff9437f3
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2019-07-03 18:00:43 +0200

    Implement DocumentTemplate.render.
    This includes collecting data and testing templates.

---
+++ modules/ProvBase/Entities/Document.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\ProvBase\Entities;
+
+class Document extends \BaseModel
+{
+    // The associated SQL table for this Model
+    public $table = 'document';
+
+    // Name of View
+    public static function view_headline()
+    {
+        return 'Documents';
+    }
+
+    // View Icon
+    public static function view_icon()
+    {
+        return '<i class="fa fa-tag"></i>';
+    }
+
+    // There are no validation rules
+    public static function rules($id = null)
+    {
+        return [
+        ];
+    }
+
+    public function get_bsclass()
+    {
+        $bsclass = 'success';
+
+        return $bsclass;
+    }
+}
+
