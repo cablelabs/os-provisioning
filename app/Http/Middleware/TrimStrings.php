f53a4c8b2535ed2e64140748f4d9fefba8c2bc9e
Christian Schramm <christian.schramm@roetzer-engineering.com>
2019-07-18 12:03:43 +0200

    Trim all input-strings (even passwords) as discussed with @ninory

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -12,7 +12,7 @@ class TrimStrings extends Middleware
      * @var array
      */
     protected $except = [
-        'password',
-        'password_confirmation',
+        //'password',
+        //'password_confirmation',
     ];
 }
