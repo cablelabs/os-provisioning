458898c90b2a45daa69191b2a3341d7f94c20617
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-07-24 10:51:56 +0200

    Remove superfluous icons

--- modules/Dunning/Resources/views/Debt/result.blade.php
+++ modules/Dunning/Resources/views/Debt/result.blade.php
@@ -10,11 +10,8 @@
 <table class="table">
   <tr>
     <td class="{{ $bsclass }}" align="center">
-      <i class="fa fa-file-invoice-dollar-o"></i>
       <i class="fa fa-dollar"></i>
-      <i class="fa fa-id-card-o"></i>
-      <i class="far fa-money-bill-alt"></i>
-      <i class="fas fa-donate"></i> {{ $debt }}
+      {{ $debt }}
     </td>
   </tr>
 </table>
