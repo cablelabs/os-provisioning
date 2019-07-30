e30928e682f8ea4dfa1073603d5f74c4f979de8d
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-07-30 15:48:03 +0200

    Intendation commit

--- modules/ProvVoip/Resources/views/index.blade.php
+++ modules/ProvVoip/Resources/views/index.blade.php
@@ -2,27 +2,27 @@
 
 @section('content')
 
-	<div class="col-md-12">
+    <div class="col-md-12">
 
-		<h1 class="page-header">{{ $title }}</h1>
+        <h1 class="page-header">{{ $title }}</h1>
 
-		{{--Quickstart--}}
+        {{--Quickstart--}}
 
-		<div class="row">
-			<div class="col-md-12">
-				@include('provvoip::widgets.quickstart')
-			</div>
-		</div>
-	</div>
-	<div class="col-md-12">
-		<div class="row">
-			<div class="col-md-7">
+        <div class="row">
+            <div class="col-md-12">
+                @include('provvoip::widgets.quickstart')
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-7">
 
-			</div>
-			<div class="col-md-5">
-				@include('provvoip::widgets.documentation')
-			</div>
-		</div>
-	</div>
-	
+            </div>
+            <div class="col-md-5">
+                @include('provvoip::widgets.documentation')
+            </div>
+        </div>
+    </div>
+
 @stop
