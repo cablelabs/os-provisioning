42b0a8e1f5952ea5654b4dbfdd59c91d76538ba5
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-08-02 11:49:57 +0200

    Improve Javascript syntax by calling hide function on multiple selector

--- resources/views/Generic/form-js-select.blade.php
+++ resources/views/Generic/form-js-select.blade.php
@@ -11,9 +11,13 @@ NOTE: will be used from form-js blade and must be called inside java context
 
 <?php
     // generate hide all variable, to hide all classes
-    $hide_all = '';
-    foreach ($field['select'] as $select)
-        $hide_all .= ' $(".'.$select.'").hide();';
+    $classes = [];
+    foreach ($field['select'] as $class) {
+        $classes[] = ".$class";
+    }
+
+    $classes = implode(', ', $classes);
+    $hide_all = "$('$classes').hide();";
 ?>
 
 {!! $hide_all !!}
