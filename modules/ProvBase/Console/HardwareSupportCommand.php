65e44e4cf2696bf93aa2c3772174afa6347753cc
Burim Arifi <arifi.burim@gmail.com>
2019-08-18 20:44:49 +0000

    Apply fixes from StyleCI

--- modules/ProvBase/Console/HardwareSupportCommand.php
+++ modules/ProvBase/Console/HardwareSupportCommand.php
@@ -3,12 +3,9 @@
 namespace Modules\provbase\Console;
 
 use Carbon\Carbon;
-use http\Client;
 use Illuminate\Console\Command;
-use Modules\ProvVoip\Entities\Mta;
 use Modules\ProvBase\Entities\Cmts;
 use Modules\ProvBase\Entities\Modem;
-use Modules\ProvBase\Entities\Endpoint;
 use Modules\ProvBase\Entities\ProvBase;
 
 class HardwareSupportCommand extends Command
@@ -47,7 +44,6 @@ class HardwareSupportCommand extends Command
      */
     public function fire()
     {
-
         $this->snmp_def_mode();
         $modems = Modem::all();
         $cmtses = Cmts::all();
@@ -56,7 +52,7 @@ class HardwareSupportCommand extends Command
         foreach ($modems as $modem) {
             $hostname = $modem->hostname.'.'.$this->domain_name;
             $support_state = 'not-supported';
-            if($modem->serial_num !== ''){
+            if ($modem->serial_num !== '') {
                 //TODO: check the response on a live system, not tested on a fully integrated system
                 $modem->serial_num = snmpget($hostname, $ro_community, '1.3.6.1.2.1.69.1.1.4.0');
             }
@@ -67,7 +63,7 @@ class HardwareSupportCommand extends Command
                 $result = json_decode($contents, true);
                 if (isset($result[$modem_serial_no_md5]) && $result[$modem_serial_no_md5] === 'valid') {
                     $support_state = 'full-support';
-                } else if ($modem->created_at->diffInWeeks(Carbon::now()) < 6) {
+                } elseif ($modem->created_at->diffInWeeks(Carbon::now()) < 6) {
                     $support_state = 'verifying';
                 }
             }
@@ -83,8 +79,7 @@ class HardwareSupportCommand extends Command
             //TODO: Test snmpwalk response a live system, not tested on a fully integrated system
             $cmts_serials = snmpwalk($hostname, $ro_community, '1.3.6.1.2.1.47.1.1.1.1.11');
             $count_found = 0;
-            foreach ($cmts_serials as $cmts_serial){
-
+            foreach ($cmts_serials as $cmts_serial) {
                 $cmts_serial_md5 = md5($cmts_serial);
                 $contents = file_get_contents('https://support.nmsprime.com/hwsn/api.php?q='.$cmts_serial_md5);
 
@@ -96,9 +91,9 @@ class HardwareSupportCommand extends Command
                 }
             }
 
-            if($count_found){
-                switch ($percentage = $count_found/count($cmts_serials)*100){
-                    case $percentage > 80 && $percentage <=95:
+            if ($count_found) {
+                switch ($percentage = $count_found / count($cmts_serials) * 100) {
+                    case $percentage > 80 && $percentage <= 95:
                         $support_state = 'restricted';
                         break;
                     case $percentage > 95:
@@ -111,7 +106,6 @@ class HardwareSupportCommand extends Command
         }
         $cmts->support_state = $support_state;
         $cmts->save();
-
     }
 
     /**
@@ -119,7 +113,6 @@ class HardwareSupportCommand extends Command
      * Note: Must be only called once per Object Init
      *
      * Note: copied from SnmpController
-     *
      */
     private function snmp_def_mode()
     {
