e6aa7548992f7796e56a1fd56131fec82a889b82
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-10-24 15:23:14 +0200

    Base: Fix form url concatenation in relation panel - class is correct - view can be any string

--- resources/views/Generic/relation.blade.php
+++ resources/views/Generic/relation.blade.php
@@ -34,7 +34,7 @@ Relation Blade is used inside a Panel Element to display relational class object
         {{-- Create Button: (With hidden add fields if required) --}}
         @if (!isset($options['hide_create_button']))
 
-            {!! Form::open(['url' => route($view.'.create', [$key => $view_var->id]), 'method' => 'POST', 'name' => 'createForm']) !!}
+            {!! Form::open(['url' => route($class.'.create', [$key => $view_var->id]), 'method' => 'POST', 'name' => 'createForm']) !!}
             {!! Form::hidden($key, $view_var->id) !!}
 
             {{-- Add hidden input fields if create tag is set in $form_fields - This sets global POST Variable --}}
@@ -45,9 +45,9 @@ Relation Blade is used inside a Panel Element to display relational class object
             @endforeach
 
             <div class="col align-self-start">
-                <a href="{{ route($view.'.create', [$key => $view_var->id]) }}">
+                <a href="{{ route($class.'.create', [$key => $view_var->id]) }}">
                     <button class="btn btn-outline-primary float-right m-b-10" style="simple" data-toggle="tooltip" data-delay='{"show":"250"}' data-placement="top"
-                        title="{{ isset($options['create_button_text']) ? trans($options['create_button_text']) : \App\Http\Controllers\BaseViewController::translate_view('Create '.$view, 'Button') }}">
+                        title="{{ isset($options['create_button_text']) ? trans($options['create_button_text']) : \App\Http\Controllers\BaseViewController::translate_view('Create '.$class, 'Button') }}">
                         <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
                     </button>
                 </a>
@@ -80,7 +80,7 @@ Relation Blade is used inside a Panel Element to display relational class object
     @if (isset($options['many_to_many']))
         {!! Form::open(array('route' => array($route_name.'.detach', $view_var->id, $options['many_to_many']), 'method' => 'post', 'id' => $class)) !!}
     @else
-        {!! Form::open(array('route' => array($view.'.destroy', 0), 'method' => 'delete', 'id' => $tab['name'].$class)) !!}
+        {!! Form::open(array('route' => array($class.'.destroy', 0), 'method' => 'delete', 'id' => $tab['name'].$class)) !!}
     @endif
 
     <table class="table">
@@ -88,7 +88,7 @@ Relation Blade is used inside a Panel Element to display relational class object
             <?php $labelData = $rel_elem->view_index_label(); ?>
             <tr class="{{isset ($labelData['bsclass']) ? $labelData['bsclass'] : ''}}">
                 <td width="20"> {!! Form::checkbox('ids['.$rel_elem->id.']', 1, null, null, ['style' => 'simple']) !!} </td>
-                <td> {!! $rel_elem->view_icon() !!} {!! HTML::linkRoute($view.'.'.$method, is_array($labelData) ? $labelData['header'] : $labelData, $rel_elem->id) !!} </td>
+                <td> {!! $rel_elem->view_icon() !!} {!! HTML::linkRoute($class.'.'.$method, is_array($labelData) ? $labelData['header'] : $labelData, $rel_elem->id) !!} </td>
             </tr>
         @endforeach
     </table>
