e7d758f67f0073b6116876bf75f957b30948c2b9
Ole Ernst <ole.ernst@roetzer-engineering.com>
2019-12-02 17:12:54 +0100

    require PPPoE username for tr069 devices

--- modules/ProvBase/Http/Controllers/ModemController.php
+++ modules/ProvBase/Http/Controllers/ModemController.php
@@ -422,6 +422,7 @@ class ModemController extends \BaseController
         if (\Modules\ProvBase\Entities\Configfile::find($data['configfile_id'])->device == 'tr069') {
             $id = \Route::current()->hasParameter('Modem') ? \Route::current()->parameters()['Modem'] : 0;
             $rules['serial_num'] = "required|unique:modem,serial_num,$id,id,deleted_at,NULL";
+            $rules['ppp_username'] = "required|unique:modem,ppp_username,$id,id,deleted_at,NULL";
         }
 
         return parent::prepare_rules($rules, $data);
