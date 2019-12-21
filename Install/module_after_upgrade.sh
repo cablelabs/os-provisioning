52ec3da237c58410a2ec29aa48e082aac371c7da
Ole Ernst <ole.ernst@roetzer-engineering.com>
2019-12-21 18:19:56 +0100

    explicitly remove excel.php, otherwise artisan fails
    this is because the migration of the package is run prior to yum
    cleaning up the old files

--- Install/module_after_upgrade.sh
+++ Install/module_after_upgrade.sh
@@ -7,6 +7,7 @@ cd '/var/www/nmsprime'
 # TODO: if a new nmsprime- package is installed as a dependency
 #       the migration may not run for the last, but next to last package
 if [ $(rpm -qa nmsprime-* --queryformat '%{VERSION}-%{RELEASE}\n' | sort | uniq -c | awk '{print $1}' | sort -u | wc -l) -eq 1 ]; then
+  rm -f /var/www/nmsprime/config/excel.php
   /opt/rh/rh-php71/root/usr/bin/php artisan config:cache
   /opt/rh/rh-php71/root/usr/bin/php artisan clear-compiled
   /opt/rh/rh-php71/root/usr/bin/php artisan optimize
