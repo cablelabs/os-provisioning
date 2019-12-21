092c5bdfbf372a34fbe039bf6203fb8f57d20099
Ole Ernst <ole.ernst@roetzer-engineering.com>
2019-12-21 18:19:02 +0100

    the command won't work without the dash in the rpm scriptlet

--- Install/module_after_upgrade.sh
+++ Install/module_after_upgrade.sh
@@ -4,7 +4,9 @@ source scl_source enable rh-php71
 cd '/var/www/nmsprime'
 
 # run artisan commands only after the last module has been upgraded
-if [ $(rpm -qa nmsprime* --queryformat '%{VERSION}-%{RELEASE}\n' | sort | uniq -c | head -1 | xargs | cut -d' ' -f1) -eq 1 ]; then
+# TODO: if a new nmsprime- package is installed as a dependency
+#       the migration may not run for the last, but next to last package
+if [ $(rpm -qa nmsprime-* --queryformat '%{VERSION}-%{RELEASE}\n' | sort | uniq -c | awk '{print $1}' | sort -u | wc -l) -eq 1 ]; then
   /opt/rh/rh-php71/root/usr/bin/php artisan config:cache
   /opt/rh/rh-php71/root/usr/bin/php artisan clear-compiled
   /opt/rh/rh-php71/root/usr/bin/php artisan optimize
