4c460d1ea619b0690065df12d1a6f36cf1a91a73
Christian Schramm <christian.schramm@roetzer-engineering.com>
2020-01-07 10:22:24 +0100

    Improve Configfile Tree Query

--- modules/ProvBase/Entities/Configfile.php
+++ modules/ProvBase/Entities/Configfile.php
@@ -431,10 +431,10 @@ class Configfile extends \BaseModel
     public static function undeletables()
     {
         $used_ids = [];
+
         // only public configfiles can be assigned to a modem or mta
-        foreach (self::where('public', '=', 'yes')->get() as $cf) {
-            if ((($cf->device == 'cm') && $cf->modem()->count()) ||
-                (($cf->device == 'mta') && $cf->mtas()->count())) {
+        foreach (self::where('public', 'yes')->withCount('modem', 'mtas')->with('parent')->get() as $cf) {
+            if ((($cf->device == 'cm') && $cf->modem_count) || (($cf->device == 'mta') && $cf->mtas_count)) {
                 array_push($used_ids, $cf->id);
                 self::_add_parent($used_ids, $cf);
             }
