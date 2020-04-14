16f08147decb9068490a6d887858715ef751c378
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2020-04-14 15:39:27 +0200

    Product: Add deprecated column to index table

--- resources/lang/de/dt_header.php
+++ resources/lang/de/dt_header.php
@@ -6,6 +6,7 @@ return [
     'city' => 'Stadt',
     'connected' => 'Angeschlossen',
     'connection_type' => 'Anschlusstyp',
+    'deprecated' => 'Veraltet',
     'email' => 'Email',
     'expansion_degree' => 'Ausbaugrad',
     'floor' => 'Etage',
