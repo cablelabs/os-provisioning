3f9219853b4ab935a53534e42d32469f7e7c53aa
Ole Ernst <ole.ernst@nmsprime.com>
2020-04-23 10:45:06 +0200

    add config suggestion for ZyXEL OLTs

--- modules/ProvBase/Config/netgw.php
+++ modules/ProvBase/Config/netgw.php
@@ -1,10 +1,12 @@
 <?php
 
-    return [
-        'Cisco'  => ['ubr7225', 'ubr7246', 'ubr10k'],
-        'Casa'   => ['C1G', 'C2200', 'C3200', 'C100G'],
-        'Arris'  => ['C1000', 'C3', 'C4', 'E6000'],
-        'Motorola' => ['BSR64000', 'BSR2000'],
-        'ZTE' => ['C320'],
-        'NSN' => ['hiX5608'],
-    ];
+return [
+    'Arris'     => ['C1000', 'C3', 'C4', 'E6000'],
+    'Casa'      => ['C1G', 'C2200', 'C3200', 'C100G'],
+    'Cisco'     => ['ubr7225', 'ubr7246', 'ubr10k', 'ASR1000'],
+    'Motorola'  => ['BSR2000', 'BSR64000'],
+    'NSN'       => ['hiX5608'],
+    'Ubiquiti'  => ['EdgeRouter'],
+    'ZTE'       => ['C320'],
+    'ZyXEL'     => ['IES4204', 'OLT1404A', 'OLT1408A'],
+];
