42510494730d6a126ca97867d7d6a65387226d68
Ole Ernst <ole.ernst@nmsprime.com>
2020-06-02 12:11:07 +0200

    increase php upload limit to 100M
    * since there are now firmware images bigger than 50M

---
+++ database/migrations/2020_06_20_000100_install_upload_limit.php
@@ -0,0 +1,28 @@
+<?php
+
+class InstallUploadLimit extends BaseMigration
+{
+    protected $tablename = '';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        exec("sed -e 's/^upload_max_filesize =.*/upload_max_filesize = 100M/' -e 's/^post_max_size =.*/post_max_size = 100M/' -i /etc/{,opt/rh/rh-php71/}php.ini");
+        exec('systemctl restart rh-php71-php-fpm.service');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        exec("sed -e 's/^upload_max_filesize =.*/upload_max_filesize = 50M/' -e 's/^post_max_size =.*/post_max_size = 50M/' -i /etc/{,opt/rh/rh-php71/}php.ini");
+        exec('systemctl restart rh-php71-php-fpm.service');
+    }
+}
