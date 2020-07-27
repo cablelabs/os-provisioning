2ee357a653556b1605b60a866469df8655f0aea8
Burim <arifi.burim@gmail.com>
2020-07-27 10:40:13 +0200

    Correcting Resolved Conflicts.

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -242,6 +242,26 @@ class BaseController extends Controller
     }
 
     /**
+     * Normalizes numeric values to minimize problems for e.g. German users (using a comma instead a dot in float).
+     *
+     * @param $value the numeric string to normalize
+     *
+     * @author Patrick Reichel
+     */
+    protected function normalizeNumericString($value)
+    {
+        // take care of nullable fields
+        if (is_null($value)) {
+            return;
+        }
+
+        // Germans use comma as decimal separator – replace by dot
+        $value = str_replace(',', '.', $value);
+
+        return $value;
+    }
+
+    /**
      * Returns a default input data array, that shall be overwritten
      * from the appropriate model controller if needed.
      *
@@ -828,6 +848,7 @@ class BaseController extends Controller
 
         // Prepare and Validate Input
         $data = $controller->prepare_input(Request::all());
+        $data['id'] = $id;
         $rules = $controller->prepare_rules($obj::rules($id), $data);
         $validator = Validator::make($data, $rules);
 
