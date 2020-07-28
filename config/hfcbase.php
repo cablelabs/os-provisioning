3f286469d4e769968dc6490281cb61ac3e04f433
Burim <arifi.burim@gmail.com>
2020-07-28 03:15:32 +0200

    Route change

--- config/hfcbase.php
+++ config/hfcbase.php
@@ -1,15 +1,7 @@
 <?php
 
-use Modules\HfcReq\Entities\NetElement;
-
-return [
-    'name' => 'HfcBase',
-    'link' => 'HfcBase.index',
-    'MenuItems' => [
-        'VicinityGraph' => [
-            'link' => 'VicinityGraph.showGraph',
-            'icon' => 'fa-sitemap',
-            'class' => NetElement::class,
-        ],
-    ],
-];
+    return 		[
+        'name' => 'HfcBase',
+        'link' => 'HfcBase.index',
+        'MenuItems' => [],
+    ];
