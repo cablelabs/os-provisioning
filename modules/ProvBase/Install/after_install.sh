9c84e83f43dec9f72625014b1ab2a929130d43ab
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2020-08-17 11:44:45 +0200

    IPv6: Install and activate ISC kea DHCP for basic IPv6 allocation with mysql

--- modules/ProvBase/Install/after_install.sh
+++ modules/ProvBase/Install/after_install.sh
@@ -1,6 +1,8 @@
 dir='/var/www/nmsprime'
 env='/etc/nmsprime/env'
 source "$env/root.env"
+source "$env/global.env"
+
 # unfortunately dhcpd does not support hmacs other than hmac-md5
 # see: https://bugs.centos.org/view.php?id=12107
 pw=$(ddns-confgen -a hmac-md5 -r /dev/urandom | grep secret)
@@ -8,15 +10,27 @@ mysql_radius_psw=$(pwgen 12 1)
 
 # create folders
 install -dm750 /etc/dhcp-nmsprime/cmts_gws
+install -dm750 /etc/kea/gateways
+# create wrong directory due to bug https://gitlab.isc.org/isc-projects/kea/-/issues/1144 - Remove on CentOS 8!
+install -dm750 /var/lib/lib/kea/
 
 # change owner
 chown -R apache:dhcpd /etc/dhcp-nmsprime
 chown -R apache /tftpboot
 chown -R named:named /var/named/dynamic
+chown -R apache /etc/kea/
+
+# Create kea DB and grant all permissions to user nmsprime
+mysql -u "$ROOT_DB_USERNAME" --password="$ROOT_DB_PASSWORD" << EOF
+CREATE DATABASE kea;
+GRANT ALL ON kea.* TO 'nmsprime'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+EOF
+/usr/sbin/kea-admin db-init mysql -u "$ROOT_DB_USERNAME" -p "$ROOT_DB_PASSWORD" -n kea
 
 sed -i "s|^.*secret \"<DNS-PASSWORD>\";|$pw|" /etc/dhcp-nmsprime/dhcpd.conf
 sed -i "s|^.*secret \"<DNS-PASSWORD>\";|$pw|" /etc/named-nmsprime.conf
 sed -i "s/<hostname>/$(hostname | cut -d '.' -f1)/" /var/named/dynamic/{nmsprime.test,in-addr.arpa}.zone
+sed -i "s/kea-dhcp6.conf/dhcp6-nmsprime.conf/" /usr/lib/systemd/system/kea-dhcp6.service
 
 echo $'\ninclude /etc/chrony.d/*.conf' >> /etc/chrony.conf
 
@@ -24,6 +38,7 @@ systemctl daemon-reload
 
 systemctl enable chronyd
 systemctl enable dhcpd
+systemctl enable kea-dhcp6.service
 systemctl enable named
 systemctl enable nmsprimed
 systemctl enable xinetd
