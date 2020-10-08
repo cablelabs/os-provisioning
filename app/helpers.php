43eac4e8a8014da90ee35ef2e8e125ab5a699feb
Christian Schramm <christian.schramm@roetzer-engineering.com>
2020-10-08 11:52:14 +0200

    L56: Move to ignition error page for d() helper

--- app/helpers.php
+++ app/helpers.php
@@ -3,39 +3,13 @@
 use Illuminate\Support\Facades\Log;
 
 /**
- * An improved version of laravel's dd() function
- * This will first print some meta information about the caller of dd
- * and then passes all given arguments to the original dd() function
- *
- * Background: I tend to have multiple dd() calls on debugging. Sometimes
- * it is hard to find all of those again ;-)
- *
- * To enable functions within this file run composer dump-auto (if it is autoloaded by composer.json)
- *
- * @author Patrick Reichel
+ * Due to the new Iginition Error Page, the ddd function exists, which does the
+ * same and more than the legacy d() method. This is kept for convinience and
+ * to quickly access this function.
  */
 function d()
 {
-    $args = func_get_args();
-
-    // write meta information about the caller
-    $td = '<td style="font-size: 11px; font-family:monospace; color:#444">';
-    $bt = debug_backtrace();
-    echo '<table>';
-    echo '<tr>';
-    echo $td.'File: </td>';
-    echo $td.array_get($bt[0], 'file', 'n/a').', line '.array_get($bt[0], 'line', 'n/a').'</td>';
-    echo '</tr>';
-    echo '<tr>';
-    echo $td.'Method: </td>';
-    echo $td.array_get($bt[1], 'class', 'n/a').'::'.array_get($bt[1], 'function', 'n/a').'()</td>';
-    echo '</tr>';
-    echo '</table>';
-
-    echo '<hr size="1" noshade>';
-
-    // call laravel's dd function and pass all given params
-    call_user_func_array('dd', $args);
+    ddd(...func_get_args());
 }
 
 /**
