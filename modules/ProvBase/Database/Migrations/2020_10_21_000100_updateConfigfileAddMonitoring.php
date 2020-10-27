6481c42cdc3ec55bf4db870cdb4cea4fdf03ea38
gitrosa <robin.sachse@nmsprime.com>
2020-10-27 12:45:02 +0100

    Using  in migration updateConfigfileAddMonitoring

--- modules/ProvBase/Database/Migrations/2020_10_21_000100_updateConfigfileAddMonitoring.php
+++ modules/ProvBase/Database/Migrations/2020_10_21_000100_updateConfigfileAddMonitoring.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Schema\Blueprint;
 
 class UpdateConfigfileAddMonitoring extends BaseMigration
 {
+    protected $tablename = 'configfile';
+
     /**
      * Run the migrations
      *
@@ -11,7 +13,7 @@ class UpdateConfigfileAddMonitoring extends BaseMigration
      */
     public function up()
     {
-        Schema::table('configfile', function (Blueprint $table) {
+        Schema::table($this->tablename, function (Blueprint $table) {
             $table->text('monitoring')->nullable();
         });
     }
@@ -23,7 +25,7 @@ class UpdateConfigfileAddMonitoring extends BaseMigration
      */
     public function down()
     {
-        Schema::table('configfile', function (Blueprint $table) {
+        Schema::table($this->tablename, function (Blueprint $table) {
             $table->dropColumn('monitoring');
         });
     }
