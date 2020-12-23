4a3e56234560320fae404c38ad3a7e45ddf7920e
Christian Schramm <christian.schramm@nmsprime.com>
2020-12-23 11:43:32 +0100

    Notifications: Enable Laravel Notification System

---
+++ config/notifications.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'navbarLength' => 6,
+];
---
+++ database/migrations/2020_12_16_025647_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateNotificationsTable extends BaseMigration
+{
+    protected $called_up_table_generic = true; //no soft deletes for notifications
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
