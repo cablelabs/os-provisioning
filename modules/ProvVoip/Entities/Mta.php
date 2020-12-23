33fa3fd3921a5b87095a2473dce3d5cc89e13f93
Patrick Reichel <patrick.reichel@nmsprime.com>
2020-12-23 16:37:04 +0100

    Move ProvVoip MtaObserver to own file.

--- modules/ProvVoip/Entities/Mta.php
+++ modules/ProvVoip/Entities/Mta.php
@@ -203,7 +203,7 @@ class Mta extends \BaseModel
     {
         parent::boot();
 
-        self::observe(new MtaObserver);
+        self::observe(new \Modules\ProvVoip\Observers\MtaObserver);
         self::observe(new \App\Observers\SystemdObserver);
     }
 
@@ -337,58 +337,5 @@ class Mta extends \BaseModel
 
             return -1;
         }
-    }
-}
-
-/**
- * MTA Observer Class
- * Handles changes on MTAs
- *
- * can handle   'creating', 'created', 'updating', 'updated',
- *              'deleting', 'deleted', 'saving', 'saved',
- *              'restoring', 'restored',
- *
- * @author Patrick Reichel
- */
-class MtaObserver
-{
-    public function created($mta)
-    {
-        $mta->hostname = 'mta-'.$mta->id;
-        $mta->save(); 			// forces to call updated method
-        $mta->modem->make_dhcp_cm(false, true);
-        $mta->modem->restart_modem();
-    }
-
-    public function updated($mta)
-    {
-        $modifications = $mta->getDirty();
-        if (isset($modifications['updated_at'])) {
-            unset($modifications['updated_at']);
-        }
-
-        // only make configuration files when relevant data was changed
-        if ($modifications) {
-            if (array_key_exists('mac', $modifications)) {
-                $mta->make_dhcp_mta();
-                $mta->modem->make_configfile();
-
-                // in case mta mac begun with or is changed to 'ff:' the modem dhcp entry has to be changed as well
-                $mta->modem->make_dhcp_cm(false, true);
-            }
-
-            $mta->make_configfile();
-        }
-
-        $mta->restart();
-    }
-
-    public function deleted($mta)
-    {
-        $mta->make_dhcp_mta(true);
-        $mta->modem->make_dhcp_cm(false, true);
-        $mta->delete_configfile();
-        $mta->modem->make_configfile();
-        $mta->modem->restart_modem();
     }
 }
