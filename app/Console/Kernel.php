35f5297c55c5ce85dfbf8b5e49764df601e11148
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2020-12-30 14:19:58 +0100

    Remove superfluous argument

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -154,7 +154,7 @@ class Kernel extends ConsoleKernel
             $schedule->call('\Modules\ProvBase\Entities\Modem@update_model_firmware')->daily();
 
             // Hardware support check for modems and CMTS
-            $schedule->command('nms:hardware-support daily')->twiceDaily(10, 14);
+            $schedule->command('nms:hardware-support')->twiceDaily(10, 14);
         }
 
         // Automatic Power Control based on measured SNR
