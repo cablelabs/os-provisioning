d671ddf12cb5f164a21271d596b25c0607e486ec
Roy Schneider <roy.schneider@roetzer-engineering.com>
2021-01-22 17:35:43 +0100

    adapt StyleCI changes

--- storage/app/config/ExternalApps.php
+++ storage/app/config/ExternalApps.php
@@ -2,23 +2,23 @@
 
 return [
     'Cacti' => [
-    	// source https://publicdomainvectors.org/en/free-clipart/Outlined-cactus/69391.html
-        'icon'	=> 'cacti.svg',
+        // source https://publicdomainvectors.org/en/free-clipart/Outlined-cactus/69391.html
+        'icon'  => 'cacti.svg',
         // source https://cacti.net
         'description' => 'Cacti is a complete network graphing solution.',
         'link' => 'https://cacti.net',
         'rpmName' => 'cacti',
     ],
     'Icinga' => [
-    	// source https://www.svgrepo.com/svg/13675/network
-        'icon'	=> 'icinga.svg',
+        // source https://www.svgrepo.com/svg/13675/network
+        'icon'  => 'icinga.svg',
         // source https://icinga.com/docs/icinga-2/latest
         'description' => 'Icinga is a monitoring system which checks the availability of your network resources, notifies users of outages, and generates performance data for reporting.',
         'link' => 'https://icinga.com/docs/icinga-2/latest',
         'rpmName' => 'icinga2',
     ],
     'GenieACS' => [
-        'icon'	=> 'genieacs.svg',
+        'icon'  => 'genieacs.svg',
         // source https://genieacs.com
         'description' => 'GenieACS is an open source TR-069 remote management solution with advanced device provisioning capabilities.',
         'link' => 'https://genieacs.com',
