e266d71516b0f73779b72f2dc22347677a6f440a
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2021-02-19 14:30:38 +0100

    Intendation commit

--- Install/module_after_upgrade.sh
+++ Install/module_after_upgrade.sh
@@ -7,20 +7,20 @@ cd '/var/www/nmsprime'
 # TODO: if a new nmsprime- package is installed as a dependency
 #       the migration may not run for the last, but next to last package
 if [ $(rpm -qa nmsprime-* --queryformat '%{VERSION}-%{RELEASE}\n' | sort | uniq -c | awk '{print $1}' | sort -u | wc -l) -eq 1 ]; then
-  rm -f /var/www/nmsprime/config/excel.php
-  rm -rf /var/www/nmsprime/bootstrap/cache/*
-  /opt/rh/rh-php73/root/usr/bin/php artisan config:cache
-  /opt/rh/rh-php73/root/usr/bin/php artisan clear-compiled
-  /opt/rh/rh-php73/root/usr/bin/php artisan optimize
-  /opt/rh/rh-php73/root/usr/bin/php artisan migrate
-  /opt/rh/rh-php73/root/usr/bin/php artisan module:migrate
-  /opt/rh/rh-php73/root/usr/bin/php artisan module:publish
-  #/opt/rh/rh-php73/root/usr/bin/php artisan queue:restart
-  pkill -f "artisan queue:work"
-  /opt/rh/rh-php73/root/usr/bin/php artisan bouncer:clean
-  /opt/rh/rh-php73/root/usr/bin/php artisan auth:nms
-  /opt/rh/rh-php73/root/usr/bin/php artisan route:cache
-  /opt/rh/rh-php73/root/usr/bin/php artisan view:clear
+    rm -f /var/www/nmsprime/config/excel.php
+    rm -rf /var/www/nmsprime/bootstrap/cache/*
+    /opt/rh/rh-php73/root/usr/bin/php artisan config:cache
+    /opt/rh/rh-php73/root/usr/bin/php artisan clear-compiled
+    /opt/rh/rh-php73/root/usr/bin/php artisan optimize
+    /opt/rh/rh-php73/root/usr/bin/php artisan migrate
+    /opt/rh/rh-php73/root/usr/bin/php artisan module:migrate
+    /opt/rh/rh-php73/root/usr/bin/php artisan module:publish
+    #/opt/rh/rh-php73/root/usr/bin/php artisan queue:restart
+    pkill -f "artisan queue:work"
+    /opt/rh/rh-php73/root/usr/bin/php artisan bouncer:clean
+    /opt/rh/rh-php73/root/usr/bin/php artisan auth:nms
+    /opt/rh/rh-php73/root/usr/bin/php artisan route:cache
+    /opt/rh/rh-php73/root/usr/bin/php artisan view:clear
 fi
 
 systemctl reload httpd
