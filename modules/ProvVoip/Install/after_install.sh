f059e41915ee7c4fbce57554cf15b271e675cd86
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2021-03-04 08:48:17 +0100

    Fill database after ProvVoip was installed to be able to create PhonenumberManagements/avoid exception

--- modules/ProvVoip/Install/after_install.sh
+++ modules/ProvVoip/Install/after_install.sh
@@ -3,3 +3,7 @@ chown -R apache:dhcpd /etc/dhcp-nmsprime
 sed -i '/^#.*mta.conf";/s/^#//' /etc/dhcp-nmsprime/dhcpd.conf
 
 firewall-cmd --reload
+
+php artisan provvoip:update_carrier_code_database
+php artisan provvoip:update_ekp_code_database
+php artisan provvoip:update_trc_class_database
