c8b87f3e088ac6953c0d3985438195fba7b98fae
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2021-03-05 15:29:24 +0100

    Use full path of php73 binary as it otherwise can not be guaranteed that nmsd is working

--- app/extensions/systemd/nmsd.php
+++ app/extensions/systemd/nmsd.php
@@ -19,8 +19,6 @@ while (1) {
         $service = basename($service);
 
         if (! file_exists($dir_scripts.$service.'.php')) {
-            file_put_contents($logfile, '['.date('Y-m-d H:i:s')."] local.Error: Restart script for service $service not found! (nmsd.php)\n", FILE_APPEND);
-
             continue;
         }
 
@@ -28,7 +26,7 @@ while (1) {
         if (! exec("ps -aux | grep $service.php | grep -v grep")) {	// when nothing is returned the script isnt running
             unlink($dir.$service);
 
-            exec('php -f '.$dir_scripts.$service.'.php &>/dev/null &');
+            exec('/opt/rh/rh-php73/root/usr/bin/php -f '.$dir_scripts.$service.'.php &>/dev/null &');
         }
     }
 
