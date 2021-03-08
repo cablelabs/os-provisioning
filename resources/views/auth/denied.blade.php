aa0afce41aea37bc7490ffbe8ca06555dd500204
Christian Schramm <christian.schramm@nmsprime.com>
2021-03-08 14:31:43 +0100

    Fix: Convert Indentation to Whitespaces

--- resources/views/auth/denied.blade.php
+++ resources/views/auth/denied.blade.php
@@ -1,27 +1,27 @@
 <!doctype html>
 <html>
 
-	<head>
-		<meta charset="utf-8">
-		<title>NMS</title>
-		@include ('bootstrap.header')
-	</head>
+    <head>
+        <meta charset="utf-8">
+        <title>NMS</title>
+        @include ('bootstrap.header')
+    </head>
 
-	@include ('bootstrap.header')
+    @include ('bootstrap.header')
 
-	<body class="pace-top">
+    <body class="pace-top">
 
         <div class="error">
             <div class="error-code m-b-10">Access Denied <i class="fa fa-warning"></i></div>
             <div class="error-content">
                 <div class="error-message">
-					@if (isset($status))
-						{{ $status }}
-					@endif
+                    @if (isset($status))
+                        {{ $status }}
+                    @endif
 
-	                @if (session('status'))
-					    {{ session('status') }}
-					@endif
+                    @if (session('status'))
+                        {{ session('status') }}
+                    @endif
 
                     @if ( !empty($message))
                         {{ $message }} <a href="{{ route('Auth.logout') }}" class="btn btn-success">Logout</a>
@@ -37,7 +37,7 @@
 
     </body>
 
-	@include ('bootstrap.footer')
+    @include ('bootstrap.footer')
 </html>
 
 
--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -1,43 +1,42 @@
 <!doctype html>
 <html>
 
-	<head>
-		<meta charset="utf-8">
-		<title>NMS</title>
-		@include ('bootstrap.header')
-	</head>
+    <head>
+        <meta charset="utf-8">
+        <title>NMS</title>
+        @include ('bootstrap.header')
+    </head>
 
 <?php
-	if (!isset($error))
-		$error = 404;
+    if (!isset($error))
+        $error = 404;
 
-	if (!isset($message))
-		$message = 'Page not found';
+    if (!isset($message))
+        $message = 'Page not found';
 
-	if (!isset($link))
-		$link = \BaseRoute::getBaseUrl();
+    if (!isset($link))
+        $link = \BaseRoute::getBaseUrl();
 
 ?>
 
-	<body class="pace-top">
+    <body class="pace-top">
 
         <div class="error">
             <div class="error-code m-b-10">{{$error}}<i class="fa fa-warning m-l-10"></i></div>
             <div class="error-content">
                 <div class="error-message mb-4">{!! $message !!}</div>
                 @if ($error == 403)
-                	<div class="error-desc m-b-20">
-						<b>Permission denied!</b> <br />
-    	            </div>
-				@elseif ($error == 404)
-                	<div class="error-desc m-b-20">
-                    	The page you're looking for doesn't exist. <br />
-	                    <!-- Perhaps, there pages will help find what you're looking for. -->
-    	            </div>
-    	        @endif
+                    <div class="error-desc m-b-20">
+                        <b>Permission denied!</b> <br />
+                    </div>
+                @elseif ($error == 404)
+                    <div class="error-desc m-b-20">
+                        The page you're looking for doesn't exist. <br />
+                    </div>
+                @endif
                 <div>
-					<a href="{{$link}}" class="btn btn-success">Go Back to Home Page</a><br><br>
-					<a href="{{ URL::previous() }}" class="btn btn-success">Go Back to previous page.</a>
+                    <a href="{{$link}}" class="btn btn-success">Go Back to Home Page</a><br><br>
+                    <a href="{{ URL::previous() }}" class="btn btn-success">Go Back to previous page.</a>
                 </div>
             </div>
         </div>
@@ -45,5 +44,5 @@
 
     </body>
 
-	@include ('bootstrap.footer')
+    @include ('bootstrap.footer')
 </html>
