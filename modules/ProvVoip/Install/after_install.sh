2e540a3eb0adc815b5e73b5789b6507703a1eb0d
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2021-03-10 10:35:58 +0100

    Install-ProvVoip: use php73 and full artisan path to not fail

--- modules/ProvVoip/Install/after_install.sh
+++ modules/ProvVoip/Install/after_install.sh
@@ -1,9 +1,11 @@
+source scl_source enable rh-php73
+
 # change owner
 chown -R apache:dhcpd /etc/dhcp-nmsprime
 sed -i '/^#.*mta.conf";/s/^#//' /etc/dhcp-nmsprime/dhcpd.conf
 
 firewall-cmd --reload
 
-php artisan provvoip:update_carrier_code_database
-php artisan provvoip:update_ekp_code_database
-php artisan provvoip:update_trc_class_database
+php /var/www/nmsprime/artisan provvoip:update_carrier_code_database
+php /var/www/nmsprime/artisan provvoip:update_ekp_code_database
+php /var/www/nmsprime/artisan provvoip:update_trc_class_database
