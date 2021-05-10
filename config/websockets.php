fee84e5c0262629c2b557ee5e32c8cfcbdd6a5ff
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2021-05-10 11:49:20 +0200

    Broadcasting: Use encryption, but remove path of test certificate

--- config/websockets.php
+++ config/websockets.php
@@ -116,13 +116,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', '/etc/httpd/ssl/demo.nmsprime.com.crt'),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', '/etc/httpd/ssl/httpd.key'),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', '/etc/httpd/ssl/demo.nmsprime.com.key'),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', '/etc/httpd/ssl/httpd.pem'),
 
         /*
          * Passphrase for your local_cert file.
