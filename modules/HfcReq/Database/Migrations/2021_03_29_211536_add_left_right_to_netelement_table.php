ca9fff73947c17bca126dfc3ca5ec7597a5e5425
Christian Schramm <christian.schramm@nmsprime.com>
2021-05-19 12:31:58 +0200

    Vicinity Graph: Fix Migration dependency

--- modules/HfcReq/Database/Migrations/2021_03_29_211536_add_left_right_to_netelement_table.php
+++ modules/HfcReq/Database/Migrations/2021_03_29_211536_add_left_right_to_netelement_table.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 
-class AddLeftRightToNetelementTable extends Migration
+class AddLeftRightToNetelementTable extends BaseMigration
 {
+    public $migrationScope = 'database';
+
     /**
      * Run the migrations.
      *
