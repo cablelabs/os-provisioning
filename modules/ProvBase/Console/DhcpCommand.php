0c43a745bc78673279d9fe61d01dab8d8bac143a
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2021-05-19 12:45:19 +0200

    DhcpCommand: Command shall always run the code directly, Job is handled by worker

--- modules/ProvBase/Console/DhcpCommand.php
+++ modules/ProvBase/Console/DhcpCommand.php
@@ -3,6 +3,10 @@
 namespace Modules\ProvBase\Console;
 
 use Illuminate\Console\Command;
+use Modules\ProvBase\Entities\Modem;
+use Modules\ProvBase\Entities\NetGw;
+use Modules\ProvBase\Entities\Endpoint;
+use Modules\ProvBase\Entities\ProvBase;
 
 class DhcpCommand extends Command
 {
@@ -37,6 +41,43 @@ class DhcpCommand extends Command
      */
     public function handle()
     {
-        \Queue::push(new \Modules\ProvBase\Jobs\DhcpJob());
+        // Global Config part
+        $prov = ProvBase::first();
+        $prov->make_dhcp_glob_conf();
+        $prov->make_dhcp_default_network_conf();
+
+        echo "Build modem related files ...\n";
+        Modem::make_dhcp_cm_all();
+        echo "Build CPE related files ...\n";
+        Modem::create_ignore_cpe_dhcp_file();
+        Modem::createDhcpBlockedCpesFile();
+
+        Endpoint::makeDhcp4All();
+        Endpoint::makeDhcp6All();
+
+        if (\Module::collections()->has('ProvVoip') && \Schema::hasTable('mta')) {
+            echo "Build MTA related files ...\n";
+            \Modules\ProvVoip\Entities\Mta::make_dhcp_mta_all();
+        }
+
+        // don't run this command during a new installation
+        // this is needed, due to cmts to netgw renaming
+        $table = (new \ReflectionClass(NetGw::class))->getDefaultProperties()['table'];
+        if (\Schema::hasTable($table)) {
+            echo "Build NetGw related files ...\n";
+            foreach (NetGw::where('type', 'cmts')->get() as $cmts) {
+                $cmts->makeDhcpConf();
+            }
+        }
+
+        // Restart dhcp server
+        $dir = storage_path('systemd/');
+        if (! is_dir($dir)) {
+            mkdir($dir, 0700, true);
+            chown($dir, 'apache');
+        }
+        touch($dir.'dhcpd');
+
+        system('/usr/bin/chown -R apache /etc/dhcp-nmsprime/ /etc/kea/');
     }
 }
