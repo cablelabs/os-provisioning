470b63247dd3472874c5fc98dc333a9df86bb7fb
Christian Schramm <christian.schramm@nmsprime.com>
2021-06-10 13:22:51 +0200

    Refactor Relation table for easier generic usage

--- modules/HfcReq/Resources/views/NetElementType/parameters.blade.php
+++ modules/HfcReq/Resources/views/NetElementType/parameters.blade.php
@@ -25,38 +25,24 @@
 
 
 {{-- attach button --}}
-<div class='col-md-6'>
-	{!! Form::open(['route' => [$model.'.assign', $view_var->id], 'method' => 'get']) !!}
-		{!! Form::submit(trans('view.Assign', ['model' => $description[$model]]), ['style' => 'simple']) !!}
-	{{-- .\App\Http\Controllers\BaseViewController::translate($view) --}}
-	{!! Form::close() !!}
-</div>
-
-{{-- detach all button --}}
-<div class='col-md-6'>
-	{!! Form::open(['route' => [$model.'.detach_all', $view_var->id], 'method' => 'delete']) !!}
-		{!! Form::submit(trans('view.Detach all', ['model' => $description[$model]]), ['!class' => 'btn btn-danger', 'style' => 'simple']) !!}
-	{!! Form::close() !!}
-</div>
-
-<br><br>
-
-{{-- list and detach button --}}
-{!! Form::open(array('route' => array('Parameter.destroy', 0), 'method' => 'delete')) !!}
-
-	<br>
-	<table class="table">
-		@foreach ($list as $param)
-			<tr class="{{isset ($param->view_index_label()['bsclass']) ? $param->view_index_label()['bsclass'] : ''}}">
-				<td> {!! Form::checkbox('ids['.$param->id.']', 1, null, null, ['style' => 'simple']) !!} </td>
-				<td> {!! HTML::linkRoute('Parameter.edit', is_array($param->view_index_label()) ? $param->view_index_label()['header'] : $param->view_index_label(), $param->id) !!} </td>
-			</tr>
-		@endforeach
-	</table>
-
-	{{-- Delete button --}}
-	<div class='col-md-12'>
-		{!! Form::submit('Delete', ['!class' => 'btn btn-danger', 'style' => 'simple']) !!}
+<div class="row justify-content-between">
+	<div>
+		{!! Form::open(['route' => [$model.'.assign', $view_var->id], 'method' => 'get']) !!}
+			{!! Form::submit(trans('view.Assign', ['model' => $description[$model]]), ['style' => 'simple']) !!}
+		{{-- .\App\Http\Controllers\BaseViewController::translate($view) --}}
+		{!! Form::close() !!}
 	</div>
 
-{!! Form::close() !!}
+	{{-- detach all button --}}
+	<div>
+		{!! Form::open(['route' => [$model.'.detach_all', $view_var->id], 'method' => 'delete']) !!}
+			{!! Form::submit(trans('view.Detach all', ['model' => $description[$model]]), ['!class' => 'btn btn-danger', 'style' => 'simple']) !!}
+		{!! Form::close() !!}
+	</div>
+</div>
+<br><br>
+
+@include('Generic.relationTable', [
+	'relation' => $list,
+	'method' => 'edit',
+])
