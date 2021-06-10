cb1d06d92db7d76930b46d6a61bf690fbce6c112
Christian Schramm <christian.schramm@nmsprime.com>
2021-06-10 13:36:59 +0200

    Refactor IndicesController - no logic change

--- modules/HfcSnmp/Http/Controllers/IndicesController.php
+++ modules/HfcSnmp/Http/Controllers/IndicesController.php
@@ -28,24 +28,36 @@ class IndicesController extends \BaseController
      */
     public function view_form_fields($model = null)
     {
-        $netelement_id = $model->netelement_id ?: \Request::input('netelement_id');
-
-        $netelem = NetElement::find($netelement_id);
+        $netelement = NetElement::findOrFail($model->netelement_id ?: request('netelement_id'));
 
         // get params from parent netgw for cluster
-        if ($netelem->netelementtype_id == 2) {
-            $netelem = $netelem->get_parent_netgw();
+        if ($netelement->netelementtype_id == 2) {
+            $netelement = $netelement->get_parent_netgw();
         }
 
-        $params = $netelem ? NetElementType::param_list($netelem->netelementtype_id) : [];
-
         // label has to be the same like column in sql table
-        $a = [
-            ['form_type' => 'text', 'name' => 'netelement_id', 'description' => 'NetElement', 'hidden' => 1],
-            ['form_type' => 'select', 'name' => 'parameter_id', 'description' => 'Parameter', 'value' => $params],
-            ['form_type' => 'text', 'name' => 'indices', 'description' => 'Indices', 'options' => ['placeholder' => '2,3,8...']],
-        ];
+        return [
+            [
+                'form_type' => 'text',
+                'name' => 'netelement_id',
+                'description' => 'NetElement',
+                'hidden' => 1,
+            ],
+            [
+                'form_type' => 'select',
+                'name' => 'parameter_id',
+                'description' => 'Parameter',
+                'value' => $netelem ? NetElementType::param_list($netelem->netelementtype_id) : [],
 
-        return $a;
+            ],
+            [
+                'form_type' => 'text',
+                'name' => 'indices',
+                'description' => 'Indices',
+                'options' => [
+                    'placeholder' => '2,3,8...',
+                ],
+            ],
+        ];
     }
 }
