08df54085fc1b8a2f8caea47c5500584eb555c56
Ole Ernst <ole.ernst@nmsprime.com>
2021-06-17 15:31:40 +0200

    specify thresholds + HF-column for impaired view
    * don't load netelement specific KMLs, since this slows down the view
      significantly
    * TODO: specify thresholds and column via WebGUI

--- modules/ProvBase/Resources/views/index.blade.php
+++ modules/ProvBase/Resources/views/index.blade.php
@@ -91,7 +91,7 @@
                             '<span class="btn btn-dark p-10 m-5 m-r-10 text-center">
                                 <i style="font-size: 25px;" class="img-center fa fa-hdd-o text-danger p-10"></i><br>
                                 <span class="username text-ellipsis text-center">'.trans('view.dashboard.impairedModem').'</span>
-                            </span>'))
+                            </span>', ['row' => 'us_pwr', 'lower' => 50]))
                         !!}
                     @endif
 
