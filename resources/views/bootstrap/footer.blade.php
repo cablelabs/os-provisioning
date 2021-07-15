09728b7a7d6fc939022e3890fd45571fb9188c82
Christian Schramm <christian.schramm@nmsprime.com>
2021-07-15 10:24:57 +0200

    NE Sidebar: remove unused Code

--- resources/views/bootstrap/footer.blade.php
+++ resources/views/bootstrap/footer.blade.php
@@ -82,21 +82,6 @@ $(document).ready(function() {
   {{-- init modals --}}
   $("#alertModal").modal();
 });
-Vue.directive('hover-class', {
-  bind(el, binding, vnode) {
-    const { value="" } = binding;
-    el.addEventListener('mouseenter',()=> {
-        el.classList.add(value)
-    });
-    el.addEventListener('mouseleave',()=> {
-        el.classList.remove(value)
-    });
-  },
-  unbind(el, binding, vnode) {
-    el.removeEventListener('mouseenter');
-    el.removeEventListener('mouseleave')
-  }
-})
 
 new Vue({
   el: '#sidebar',
