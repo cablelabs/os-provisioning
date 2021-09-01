9f34138ec1987a585dd6c777530df78e0a973d68
Roy Schneider <roy.schneider@roetzer-engineering.com>
2021-09-01 10:42:19 +0200

    Configfile: add required validation for dashboard

--- modules/ProvBase/Entities/Configfile.php
+++ modules/ProvBase/Entities/Configfile.php
@@ -37,6 +37,7 @@ class Configfile extends \BaseModel
         return [
             'name' => 'required_without:import|unique:configfile,name,'.$id.',id,deleted_at,NULL',
             'text' => 'docsis',
+            'dashboard' => 'required',
         ];
     }
 
