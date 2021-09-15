f1e278d53121117549d37c1cd546934db40ef974
Patrick Reichel <patrick.reichel@nmsprime.com>
2021-09-15 10:47:04 +0200

    Update and restart modified supervisord jobs.

--- Install/after_upgrade.sh
+++ Install/after_upgrade.sh
@@ -7,5 +7,11 @@ systemctl reload httpd
 
 rm -f storage/framework/sessions/*
 chown -R apache storage bootstrap/cache
-systemctl restart nmsprimed supervisord
+systemctl restart nmsprimed
 systemd-tmpfiles --create
+
+# reread and deploy supervisor config
+/usr/bin/supervisorctl update
+
+# restart all jobs to make sure all workers use current code
+/usr/bin/supervisorctl restart all
