1e59a0498dd73c257415ca8f72035e0e756eb19c
Ole Ernst <olebowle@gmx.com>
2021-10-14 12:37:40 +0200

    New translations passwords.php (Spanish)

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => 'We can\'t find a user with that e-mail address.',
+    'password' => 'Las contraseñas deben coincidir y contener al menos 6 caracteres',
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
+    'token' => 'El token de recuperación de contraseña es inválido.',
+    'user' => 'No podemos encontrar ningún usuario con ese correo electrónico.',
 
 ];
