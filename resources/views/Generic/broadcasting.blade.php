634668831b889a4f6832bd8e8ff15043e142160f
Roy Schneider <roy.schneider@roetzer-engineering.com>
2021-10-29 09:54:46 +0200

    Modem analysis: create blade for broadcasting *including laravel echo config and ajax call

---
+++ resources/views/Generic/broadcasting.blade.php
@@ -0,0 +1,43 @@
+<script type="module">
+    import Echo from "{{ asset('vendor/echo.js') }}"
+
+    var config = "{{ json_encode(config('broadcasting.connections.pusher')) }}"
+    config = JSON.parse(config.replace(/&quot;/ig,'"'))
+
+    window.echo = new Echo({
+        broadcaster: 'pusher',
+        key: config.key,
+        wsHost: window.location.hostname,
+        wsPort: config.options.port,
+        wssPort: config.options.port,
+        forceTLS: config.options.encrypted,
+        disableStats: true,
+        enabledTransports: ['ws', 'wss'],
+    })
+
+
+    /**
+     * Unsubscribe from channel
+     *
+     */
+    window.unsubscribe = function unsubscribe()
+    {
+        echo.leave(channel)
+        console.log('Leave channel ' + channel)
+    }
+
+    // Vue mixin that performs ajax if route is defined in component data
+    window.broadcastingAxiosCall = {
+        data: function () {
+            return {
+                route: null
+            }
+        },
+        mounted () {
+            axios.post(this.route)
+                .finally(() => {
+                    console.log('Vue mounted')
+                })
+        }
+    }
+</script>
