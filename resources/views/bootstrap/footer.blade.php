406568381f632f27b7f85a1e423dfc391ea9e249
Christian Schramm <christian.schramm@nmsprime.com>
2021-12-06 04:10:46 +0100

    OL2Leaflet:Improve dynamic filtering

--- resources/views/bootstrap/footer.blade.php
+++ resources/views/bootstrap/footer.blade.php
@@ -72,6 +72,43 @@ $(document).ready(function() {
   $("#alertModal").modal();
 });
 
+Vue.component("select2", {
+    props: ["options", "value"],
+    template: "#select2-template",
+    mounted: function() {
+        var vm = this
+
+        $(this.$el)
+        // init select2
+        .select2({ data: this.options })
+        .val(this.value)
+        .trigger("change")
+        // emit event on change.
+        .on("change", function() {
+          vm.$emit("input", this.value)
+        })
+    },
+    watch: {
+        value: function(value) {
+            // update value
+            $(this.$el)
+            .val(value)
+            .trigger("change")
+        },
+        options: function(options) {
+            // update options
+            $(this.$el)
+            .empty()
+            .select2({ data: options })
+        }
+    },
+    destroyed: function() {
+        $(this.$el)
+        .off()
+        .select2("destroy")
+    }
+})
+
 new Vue({
   el: '#sidebar',
   mounted() {
