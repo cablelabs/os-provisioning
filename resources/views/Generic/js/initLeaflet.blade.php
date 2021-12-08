98b09aea1a5a3ab409e7f7361747201d6418d31a
Christian Schramm <christian.schramm@nmsprime.com>
2021-12-08 17:56:17 +0100

    Refactor: Load API Keys inside Map

--- resources/views/Generic/js/initLeaflet.blade.php
+++ resources/views/Generic/js/initLeaflet.blade.php
@@ -7,14 +7,14 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
 
-@isset ($hereMapApiKey)
-    const here = {apiKey: '{{ $hereMapApiKey }}'}
+@if (config('app.hereMapApiKey'))
+    const here = {apiKey: '{{ config('app.hereMapApiKey') }}'}
     const hereHybrid = L.tileLayer(`https://{m}.aerial.maps.ls.hereapi.com/maptile/2.1/maptile/newest/hybrid.day/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&pois=true&ppi=320`, {m: Math.floor(Math.random() * 3 + 1)})
     const hereStreet = L.tileLayer(`https://{m}.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day.transit/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&pois=true&ppi=320`, {m: Math.floor(Math.random() * 3 + 1)})
     const hereTerrain = L.tileLayer(`https://{m}.aerial.maps.ls.hereapi.com/maptile/2.1/maptile/newest/terrain.day/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&pois=true&ppi=320`, {m: Math.floor(Math.random() * 3 + 1)})
 @endif
 
-@isset ($googleApiKey)
+@if (config('app.googleApiKey'))
     const gHybrid = L.gridLayer.googleMutant({type: 'hybrid'})
     const gRoadmap = L.gridLayer.googleMutant({type: 'roadmap'})
     const gTerrain = L.gridLayer.googleMutant({type: 'terrain'})
@@ -22,12 +22,12 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let baseLayers = {
     "Open Street Maps": osm,
-    @isset ($hereMapApiKey)
+    @if (config('app.hereMapApiKey'))
         "Here Maps Hybrid": hereHybrid,
         "Here Maps Street Map": hereStreet,
         "Here Maps Terrain": hereTerrain,
     @endif
-    @isset ($googleApiKey)
+    @if (config('app.googleApiKey'))
         "Google Hybrid": gHybrid,
         "Google Roadmap": gRoadmap,
         "Google Terrain": gTerrain,
