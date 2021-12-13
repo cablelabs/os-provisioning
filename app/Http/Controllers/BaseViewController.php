a9c1e30f152bee3910d30a4979e4fe22ebd04015
Christian Schramm <christian.schramm@nmsprime.com>
2021-12-13 12:39:51 +0100

    Bugfix: fix lat/lng update for Modem

--- app/Http/Controllers/BaseViewController.php
+++ app/Http/Controllers/BaseViewController.php
@@ -566,10 +566,10 @@ class BaseViewController extends Controller
     public static function geoPosFields($model)
     {
         return "<div class=col-md-3>
-                <input class=form-control name=x type=text value='".$model['lng']."' id=x style='background-color:inherit'>
+                <input class=form-control name='lng' type=text value='".$model['lng']."' id='lng' style='background-color:inherit'>
             </div>
             <div class=col-md-4>
-                <input class=form-control name=y type=text value='".$model['lat']."' id=y style='background-color:inherit'>
+                <input class=form-control name='lat' type=text value='".$model['lat']."' id='lat' style='background-color:inherit'>
             </div>";
     }
 
