966dd01514e191fc37d926ca0d39881b5f1e5bb1
Christian Schramm <christian.schramm@nmsprime.com>
2021-12-15 11:39:41 +0100

    Bugfix: Fix editing drawn shapes

--- public/components/nmsprime.js
+++ public/components/nmsprime.js
@@ -257,6 +257,19 @@ function rezizeTextareas() {
   });
 }
 
+function debounce(func, wait, immediate) {
+  let timeout
+  return function() {
+      let context = this, args = arguments
+      clearTimeout(timeout)
+      timeout = setTimeout(function() {
+          timeout = null
+          if (!immediate) func.apply(context, args);
+      }, wait)
+      if (immediate && !timeout) func.apply(context, args);
+  }
+}
+
 function redirect(i18nAll)
 {
     let select = document.getElementById('show-value')
