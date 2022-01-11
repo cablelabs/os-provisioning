aef94f1067c1a1ea13596cdee3c1b42856d9dd38
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2022-01-11 11:41:16 +0100

    fix: Use full path of PHP8 during installation

--- modules/ProvVoip/Install/after_install.sh
+++ modules/ProvVoip/Install/after_install.sh
@@ -7,8 +7,8 @@ sed -i '/^#.*mta.conf";/s/^#//' /etc/dhcp-nmsprime/dhcpd.conf
 
 firewall-cmd --reload
 
-php /var/www/nmsprime/artisan module:migrate
-php /var/www/nmsprime/artisan provvoip:update_carrier_code_database
-php /var/www/nmsprime/artisan provvoip:update_ekp_code_database
+/opt/remi/php80/root/usr/bin/php /var/www/nmsprime/artisan module:migrate
+/opt/remi/php80/root/usr/bin/php /var/www/nmsprime/artisan provvoip:update_carrier_code_database
+/opt/remi/php80/root/usr/bin/php /var/www/nmsprime/artisan provvoip:update_ekp_code_database
 # Currently only implemented for use with ProvVoipEnvia module - this would fail as it would run before envia module is installed
 # php /var/www/nmsprime/artisan provvoip:update_trc_class_database
