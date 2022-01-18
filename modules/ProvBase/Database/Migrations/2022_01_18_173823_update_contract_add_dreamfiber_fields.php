f35f92d69e77691e20a588474239405760f280a5
Patrick Reichel <patrick.reichel@nmsprime.com>
2022-01-18 19:46:06 +0100

    Dreamfiber: Add OTO fields to contract table.

---
+++ modules/ProvBase/Database/Migrations/2022_01_18_173823_update_contract_add_dreamfiber_fields.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Modules\ProvBase\Entities\Contract;
+use Illuminate\Database\Schema\Blueprint;
+
+class UpdateContractAddDreamfiberFields extends BaseMigration
+{
+    public $migrationScope = 'database';
+
+    protected $tableName = 'contract';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->string('sep_id', 32)->nullable();
+            $table->string('oto_id', 64)->nullable();
+            $table->smallInteger('oto_port')->unsigned()->nullable();
+            $table->string('oto_socket_usage', 64)->nullable();
+            $table->string('oto_status', 32)->nullable();
+            $table->string('flat_id', 32)->nullable();
+            $table->string('alex_status', 32)->nullable();
+            $table->string('omdf_id', 128)->nullable();
+            $table->string('boc_label', 128)->nullable();
+            $table->string('bof_label', 32)->nullable();
+            $table->string('type', 32)->nullable()->default('nmsprime');
+        });
+
+        $contract = Contract::create([
+            'company' => 'n/a',
+            'department' => 'n/a',
+            'firstname' => 'n/a',
+            'lastname' => 'n/a',
+            'street' => 'n/a',
+            'house_number' => 'n/a',
+            'zip' => 'n/a',
+            'city' => 'n/a',
+            'country_code' => 'n/a',
+            'description' => '“Storage” holding free ONT. Change parameters to your needs.',
+            'type' => 'DF_OTO_STORAGE',
+        ]);
+        $contract->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->dropColumn([
+                'sep_id',
+                'oto_id',
+                'oto_port',
+                'oto_socket_usage',
+                'oto_status',
+                'flat_id',
+                'alex_status',
+                'omdf_id',
+                'boc_label',
+                'bof_label',
+                'type',
+            ]);
+        });
+    }
+}
