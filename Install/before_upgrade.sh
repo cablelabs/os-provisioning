1006bfc9c6680b54308d78340460ea59753fdd7f
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2022-05-05 16:24:15 +0200

    Fix-Pgsql: change tinyint to smallint as it would be converted to boolean otherwise

--- Install/before_upgrade.sh
+++ Install/before_upgrade.sh
@@ -24,6 +24,10 @@ mysql -u "${auths[2]}" --password="${auths[1]}" "${auths[0]}" --exec="
     UPDATE oid set type = 'u' where type = '' or type is null;
     UPDATE phonetariff set voip_protocol = 'SIP' where voip_protocol = '' or voip_protocol is null;
     UPDATE sepamandate set valid_to = NULL where valid_to = '0000-00-00';
+    ALTER TABLE billingbase modify rcd smallint(6) null;
+    ALTER TABLE billingbase modify cdr_offset smallint(6) null;
+    ALTER TABLE debt modify indicator smallint(6) null;
+    ALTER TABLE item modify payed_month smallint(6) null;
     ALTER TABLE settlementrun modify month smallint(6) null;
     ALTER TABLE invoice modify month smallint(6) null;
     ALTER TABLE costcenter modify billing_month smallint(6) null;
