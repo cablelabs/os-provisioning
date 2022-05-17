d1855329f50860f90e51792fd853ffc774821151
Christian Schramm <christian.schramm@nmsprime.com>
2022-05-17 11:43:32 +0200

    Move sidebar filter component to sidebar layout folder

--- resources/views/Layout/sidebar/_filter-select.blade.php
+++ resources/views/Layout/sidebar/_filter-select.blade.php
@@ -5,20 +5,22 @@
             $$name = Modules\HfcReq\Entities\NetElement::where('netelementtype_id', $options['netTypeId'])->get();
         @endphp
         <div class="text-gray-900 flex items-center">
-            <select2 class="select2-ajax"
-                name="{{ $name }}" id="{{ $name }}"
-                data-allow-clear="true"
+            <select2 id="{{ $name }}"
+                class="select2-ajax"
+                name="{{ $name }}"
+                v-model="{{ $options['var'] }}"
                 data-placeholder="Choose a {{ $name }}"
-                ajax-route="{{ route('Sidebar.select2', ['nettype' => $options['netTypeId']] ) }}"
+                data-allow-clear="true"
+                :ajax-route="ajaxRoute('{{ route('Sidebar.select2', ['nettype' => $options['netTypeId']] ) }}')"
                 @if (isset($ids) && $net = $ids->where('netelementtype.base_type_id', $options['netTypeId']))
-                    initial-value="{{ $net->first()?->id }}"
+                    :initial="{{ $net->first()?->id }}"
                 @endif
             >
                 @if (isset($ids) && $net = $ids->where('netelementtype.base_type_id', $options['netTypeId']))
                     <option value="{{ $net->first()?->id }}" selected="selected">{{ $net->first()?->name }}</option>
                 @endif
             </select2>
-            <a href="{{ route($options['route'], ['netelement' => Modules\HfcReq\Entities\NetElement::where('netelementtype_id', $options['netTypeId'])->first()?->id ?? 643398])}}">
+            <a :href="route({{ $options['var'] }}, '{{ route($options['route'], ['netelement' => 'NETELEMENT_ID'])}}')">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-50 ml-1 hover:cursor-pointer hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
                 </svg>
