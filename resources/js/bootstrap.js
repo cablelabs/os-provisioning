66e9a3afd3a25d135b82cbcc7db4c1409f9e5c70
Christian Schramm <christian.schramm@nmsprime.com>
2022-08-10 15:57:48 +0200

    Fix ProvMon Analysis Page

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -46,13 +46,4 @@ if (token) {
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
-
-import Echo from 'laravel-echo'
 window.Pusher = require('pusher-js')
-
-//  window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// })
