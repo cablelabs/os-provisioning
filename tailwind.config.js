f6c844627e604cc7a39fc66c7c15ffe3a687c248
Christian Schramm <christian.schramm@nmsprime.com>
2022-08-12 23:24:30 +0200

    Stylefix: Apply styles to tailwind config with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./modules/**/*.blade.php",
-      "./modules/**/*.vue",
-    ],
-    theme: {
-      extend: {
-        colors: {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    './modules/**/*.blade.php',
+    './modules/**/*.vue'
+  ],
+  theme: {
+    extend: {
+      colors: {
         'sidebar-dark': '#1a2229',
         'sidebar-light': '#2d353c',
-        'lime-nmsprime': '#98d145',
-        }
-      },
-    },
-    plugins: [],
-  }
+        'lime-nmsprime': '#98d145'
+      }
+    }
+  },
+  plugins: []
+}
