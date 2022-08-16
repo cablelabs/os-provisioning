e590ba01b068c9d52e1e9b4d7721062d4809e9de
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2022-08-16 11:53:58 +0200

    Add new linux user 'nmsprime' to use as group for files where multiple users/processes need access to - assign apache and telegraf user to the group

---
+++ database/migrations/2022_08_16_000136_install_add_linux_user_nmsprime.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class InstallAddLinuxUserNmsprime extends BaseMigration
+{
+    public $migrationScope = 'system';
+
+    /**
+     * Run the migrations.
+     *
+     * Add new linux user 'nmsprime' to use as group for files where multiple users/processes
+     * need access to - assign apache and telegraf user to the group
+     *
+     * @return void
+     */
+    public function up()
+    {
+        system('useradd nmsprime');
+        system('chgrp nmsprime /etc/nmsprime/env/ /etc/nmsprime/env/global.env');
+        system('usermod -a -G nmsprime apache');
+        system('usermod -a -G nmsprime telegraf');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        system('userdel nmsprime');
+        system('groupdel nmsprime');
+        system('chgrp apache /etc/nmsprime/env/ /etc/nmsprime/env/global.env');
+    }
+}
