133bab43b0103ad3cf50465b1e5da3c170ffecd5
Roy Schneider <roy.schneider@nmsprime.com>
2022-08-24 13:35:01 +0200

    Arp: feature * add model and migration

---
+++ database/migrations/2022_08_24_115739_create_arp_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArpTable extends BaseMigration
+{
+    public $migrationScope = 'database';
+
+    protected $tableName = 'arp';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $this->up_table_generic($table);
+            $table->string('display_key')->nullable();
+            $table->string('address')->nullable();
+            $table->string('type')->nullable();
+            $table->string('interface')->nullable();
+            $table->string('mac')->nullable();
+            $table->integer('netelement_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
