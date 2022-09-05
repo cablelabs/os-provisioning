38726ed8c9cf5a1de4c1e44cab2104f41fc0d446
Farshid Ghiasimanesh <farshid.ghiasimanesh@nmsprime.com>
2022-09-05 13:11:27 +0200

    update composer.json and add some new features related to laravel 7

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
