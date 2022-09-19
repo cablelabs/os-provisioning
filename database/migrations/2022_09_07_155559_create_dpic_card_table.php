5810b035dc77b780e2e8ed7bc890e4dd0a0a7fe5
Roy Schneider <roy.schneider@nmsprime.com>
2022-09-19 10:15:22 +0200

    DPIC cards, fiber nodes: feature * add model and migration

---
+++ database/migrations/2022_09_07_155559_create_dpic_card_table.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateDpicCardTable extends BaseMigration
+{
+    public $migrationScope = 'database';
+
+    protected $tableName = 'dpic_card';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $this->up_table_generic($table);
+            $table->string('sys_name')->nullable();
+            $table->string('description')->nullable();
+            // last status change can be updated_at
+            $table->string('uptime')->nullable();
+            $table->string('bandwidth')->nullable();
+            $table->string('utilization')->nullable();
+            $table->integer('netelement_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
