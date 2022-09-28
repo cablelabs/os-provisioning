4bdf62522a5dde1a99acbce0b6ac265f439ba689
Farshid Ghiasimanesh <farshid.ghiasimanesh@nmsprime.com>
2022-09-28 14:54:14 +0200

    rollback webpack extraction

--- webpack.mix.js
+++ webpack.mix.js
@@ -34,5 +34,4 @@ mix.js('resources/js/app.js', 'public/js')
     require('tailwindcss'),
   ])
   .postCss('modules/Ccc/Resources/assets/css/ccc.css', 'css')
-  .extract()
-  .extract(['@panzoom/panzoom'], 'js/panzoom-vendor.js')
+  .extract(['@panzoom/panzoom'])
