747a31a4e99d7659ee679286bbbe6d4c1daa9b73
kaianuar <anuar.jamlus@gmail.com>
2022-10-04 11:41:43 +0200

    Refactor code

--- modules/ProvBase/Http/Controllers/ModemController.php
+++ modules/ProvBase/Http/Controllers/ModemController.php
@@ -183,12 +183,11 @@ class ModemController extends \BaseController
             $c[12] = array_merge($c[12], ['space' => 1]);
         }
 
-        if (Module::collections()->has('Altiplano')) {
-            $altiplanoApi = new \Modules\Altiplano\Helpers\AltiplanoApi();
-            if ($altiplanoApi->token) {
-                $availableFiberNames = \Modules\Altiplano\Services\AltiplanoService::resolveAvailableFiberNames();
-                $b[] = ['form_type' => 'select', 'name' => 'fiber_name', 'description' => 'Fiber Name', 'value' => $availableFiberNames, 'space' => '1'];
-            }
+        if (Module::collections()->has('Altiplano')
+            && (new \Modules\Altiplano\Helpers\AltiplanoApi())->token
+        ) {
+            $availableFiberNames = \Modules\Altiplano\Services\AltiplanoService::resolveAvailableFiberNames();
+            $b[] = ['form_type' => 'select', 'name' => 'fiber_name', 'description' => 'Fiber Name', 'value' => $availableFiberNames, 'space' => '1'];
         }
 
         $d = [
