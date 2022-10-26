1fd35949c32a73a3f31142478583de8a48009b28
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2022-10-26 11:21:23 +0200

    CCAP: Get fiber nodes via cli, parse via textfsm and insert n:m relations in pivot table

---
+++ database/migrations/2022_10_21_000100_create_fiber_node_rpd_pivot_table.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateFiberNodeRpdPivotTable extends BaseMigration
+{
+    public $migrationScope = 'database';
+
+    protected $tableName = 'fiber_node_rpd';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->timestampTz('created_at')->nullable();
+            $table->integer('fiber_node_id');
+            $table->integer('rpd_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
---
+++ database/migrations/2022_10_21_000300_add_mdd_status_to_fiber_node.php
@@ -0,0 +1,36 @@
+<?php
+
+use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddMddStatusToFiberNode extends BaseMigration
+{
+    public $migrationScope = 'database';
+
+    protected $tableName = 'fiber_node';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->string('mdd_status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->dropColumn('mdd_status');
+        });
+    }
+}
