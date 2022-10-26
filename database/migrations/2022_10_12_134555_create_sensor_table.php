86d002dd2fe9919cfb0c7515e2453ca6dc4ebac2
Christian Schramm <christian.schramm@nmsprime.com>
2022-10-26 08:01:42 +0200

    Create Sensor Table

---
+++ database/migrations/2022_10_12_134555_create_sensor_table.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends BaseMigration
+{
+    public $migrationScope = 'database';
+    public $tableName = 'sensor';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 50);
+            $table->string('status', 20);
+            $table->unsignedDecimal('value', 4, 2);
+            $table->string('unit', 20);
+
+            $table->unsignedInteger('temperature_id');
+            $table->morphs('core_element');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+};
