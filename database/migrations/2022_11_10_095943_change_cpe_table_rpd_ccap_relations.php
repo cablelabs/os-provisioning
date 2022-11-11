ca02328d86a615c4baca238d97e7822902859eb7
Christian Schramm <christian.schramm@nmsprime.com>
2022-11-11 11:37:14 +0100

    Change CPE table to have managing rpd and ccap relation

---
+++ database/migrations/2022_11_10_095943_change_cpe_table_rpd_ccap_relations.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends BaseMigration
+{
+    public $migrationScope = 'database';
+    public $tableName = 'cpe';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->renameColumn('netelement_id', 'ccap_id');
+            $table->dropColumn('ccap_core');
+            $table->integer('rpd_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->renameColumn('ccap_id', 'netelement_id');
+            $table->string('ccap_core')->nullable();
+            $table->dropIfExists('rpd_id');
+        });
+    }
+};
