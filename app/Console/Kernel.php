fbd60e52a2ad6ec57566a08f054fdc6be4d8231f
Farshid Ghiasimanesh <farshid.ghiasimanesh@nmsprime.com>
2022-11-15 11:38:21 +0100

    add second job for serial and model

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -116,6 +116,10 @@ class Kernel extends ConsoleKernel
             $schedule->call(function () {
                 \Queue::pushOn('low', new \Modules\CoreMon\Jobs\ImportKafkaCcapInfoDataJob());
             })->cron('5,20,35,50 * * * *');
+
+            $schedule->call(function () {
+                \Queue::pushOn('low', new \Modules\CoreMon\Jobs\ImportKafkaCcapInfo2DataJob());
+            })->cron('5,20,35,50 * * * *');
         }
         // Parse News from repo server and save to local JSON file
         if (\Module::collections()->has('Dashboard')) {
