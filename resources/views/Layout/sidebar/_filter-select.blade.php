d1b03011710ca88d32ef8a1959069438ac15c627
Christian Schramm <christian.schramm@nmsprime.com>
2022-11-16 09:42:58 +0100

    CPE: Feature
    * split logic between netelement and CPE for sidebar
    * @cschra adjustments

--- resources/views/Layout/sidebar/_filter-select.blade.php
+++ resources/views/Layout/sidebar/_filter-select.blade.php
@@ -26,7 +26,11 @@
                     <option value="{{ $net->id }}" selected="selected">{{ $net->name }}</option>
                 @endif
             </sidebar-select2>
-            <a :href="route({{ $options['var'] }}, '{{ route($options['route'], ['netelement' => 'NETELEMENT_ID'])}}')" class="w-[8%]">
+            @if ($name !== 'CPE')
+                <a :href="route({{ $options['var'] }}, '{{ route($options['route'], ['netelement' => 'NETELEMENT_ID'])}}')" class="w-[8%]">
+            @else
+                <a :href="route({{ $options['var'] }}, '{{ route($options['route'], ['cpe' => 'NETELEMENT_ID'])}}')" class="w-[8%]">
+            @endif
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 ml-1 text-gray-50 hover:cursor-pointer hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
                 </svg>
