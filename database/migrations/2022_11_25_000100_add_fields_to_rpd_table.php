26aa2b62ec7ff21f4b5a5622d427d36cf8388a00
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2022-12-12 13:45:34 +0100

    GetRpds: Use laravel's internal upsert method instead of Batch package - set necessary unique constraints in RPD table and get rid of duplicate entries

---
+++ database/migrations/2022_11_25_000100_add_fields_to_rpd_table.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends BaseMigration
+{
+    public $migrationScope = 'database';
+    protected $tableName = 'rpd';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->bigInteger('netelement_ccap_id')->nullable();
+            $table->string('cable_if')->nullable();
+            $table->string('state_changed_at')->nullable();         // stateChangeTimestamp in Smartphy response
+            $table->unique(['mac', 'netelement_id']);
+        });
+
+        system('sudo -u postgres /usr/bin/psql nmsprime -c "grant select on ALL tables in schema nmsprime to grafana"');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->dropColumns(['netelement_ccap_id', 'cable_if', 'state_changed_at']);
+            $table->dropUnique(['mac', 'netelement_id']);
+        });
+    }
+};
