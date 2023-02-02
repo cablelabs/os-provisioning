190dfacfedb229ca6144cba4af744fe9f69f4857
Christian Schramm <christian.schramm@nmsprime.com>
2023-02-02 14:59:27 +0100

    Fix prepare script
    * fix error caused by && -> made 2 seperate lines

--- phpunit_prepare.sh
+++ phpunit_prepare.sh
@@ -18,7 +18,8 @@ declare -a CMDS=(
 	"php artisan optimize:clear"
 	"php artisan optimize"
 	"php artisan nms:coremon-publish"
-	"npm install && npm run dev"
+	"npm install"
+        "npm run dev"
 	"chown -R apache /var/www/nmsprime/storage/framework"
 	"systemctl restart supervisord httpd"
 	)
