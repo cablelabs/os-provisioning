fbe1880f30e4023bc0ab66203af40fcca7b30018
Christian Schramm <christian.schramm@nmsprime.com>
2023-02-15 11:12:02 +0100

    Prepare Script: use spaces as indentation

--- phpunit_prepare.sh
+++ phpunit_prepare.sh
@@ -8,28 +8,28 @@ export COMPOSER_MEMORY_LIMIT=-1
 export COMPOSER_ALLOW_SUPERUSER=1
 
 declare -a CMDS=(
-	"mkdir -p /var/www/nmsprime/storage/framework/cache"
-	"mkdir -p /var/www/nmsprime/storage/framework/sessions"
-	"mkdir -p /var/www/nmsprime/storage/framework/views"
-	"composer update"
-	"php artisan migrate"
-	"php artisan module:migrate"
-	"php artisan module:publish"
-	"php artisan optimize:clear"
-	"php artisan optimize"
-	"php artisan nms:coremon-publish"
-	"npm install"
-        "npm run dev"
-	"chown -R apache /var/www/nmsprime/storage/framework"
-	"systemctl restart supervisord httpd"
-	)
+    "mkdir -p /var/www/nmsprime/storage/framework/cache"
+    "mkdir -p /var/www/nmsprime/storage/framework/sessions"
+    "mkdir -p /var/www/nmsprime/storage/framework/views"
+    "composer update"
+    "php artisan migrate"
+    "php artisan module:migrate"
+    "php artisan module:publish"
+    "php artisan optimize:clear"
+    "php artisan optimize"
+    "php artisan nms:coremon-publish"
+    "npm install"
+    "npm run dev"
+    "chown -R apache /var/www/nmsprime/storage/framework"
+    "systemctl restart supervisord httpd"
+    )
 
 clear
 
 for CMD in "${CMDS[@]}"; do
-	echo
-	echo "Running $CMD…"
-	$CMD
+    echo
+    echo "Running $CMD…"
+    $CMD
 done
 echo
 echo
