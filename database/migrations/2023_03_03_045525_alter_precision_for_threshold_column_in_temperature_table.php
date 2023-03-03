444bc8f522b8b6927766e48fb73c7e005c4a1391
kaianuar <anuar.jamlus@gmail.com>
2023-03-03 05:25:51 +0100

    Extract temperature for NCS via CLI

---
+++ database/migrations/2023_03_03_045525_alter_precision_for_threshold_column_in_temperature_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends BaseMigration
+{
+    public $migrationScope = 'database';
+
+    protected $tableName = 'temperature';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->unsignedDecimal('threshold', 5, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->unsignedDecimal('threshold', 4, 2)->change();
+        });
+    }
+};
