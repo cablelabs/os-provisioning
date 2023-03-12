a6eba06904766ca5f844a8bf5ab515ad24d20eb6
Wali Razzaq <wali.razzaq@nmsprime.com>
2023-03-12 20:14:03 +0100

    DocumentManagement: move migrations to root
    Moves Document related migrations to `/database/migrations` from
    ProvBase module

--- database/migrations/2023_03_12_181546_create_document_template_table.php
+++ database/migrations/2023_03_12_181546_create_document_template_table.php
@@ -1,10 +1,29 @@
 <?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 
-class CreateDocumentTemplateTable extends BaseMigration
+return new class extends BaseMigration
 {
+    public $migrationScope = 'database';
     protected $tablename = 'documenttemplate';
 
     // have to correlate with order in document type migration
@@ -61,6 +80,4 @@ class CreateDocumentTemplateTable extends BaseMigration
     {
         Schema::drop($this->tablename);
     }
-}
-
-
+};
--- database/migrations/2023_03_12_181933_create_document_table.php
+++ database/migrations/2023_03_12_181933_create_document_table.php
@@ -1,10 +1,28 @@
 <?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 use Database\Migrations\BaseMigration;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateDocumentTable extends BaseMigration
+return new class extends BaseMigration
 {
+    public $migrationScope = 'database';
     protected $tablename = 'document';
 
     /**
@@ -35,4 +53,4 @@ class CreateDocumentTable extends BaseMigration
     {
         Schema::drop($this->tablename);
     }
-}
+};
