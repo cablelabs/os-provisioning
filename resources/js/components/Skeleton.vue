660981dd8fd8d226d632752ab022a8aa5d190b6f
Roy Schneider <roy.schneider@nmsprime.com>
2023-03-27 13:58:37 +0200

    Modem Analysis: Bugfix * fix length of Skeleton to fit with various screen sizes

--- resources/js/components/Skeleton.vue
+++ resources/js/components/Skeleton.vue
@@ -18,7 +18,7 @@
 <template>
   <div id="skeleton">
     <div class="flex justify-between">
-      <div class="w-1/12">
+      <div class="w-1/2">
         <div
           v-for="n in 6"
           :key="n"
@@ -28,12 +28,10 @@
       <div class="skeleton rounded my-2 mx-3 w-1/3 h-48"></div>
     </div>
 
-    <div>
-      <div class="skeleton m-3 w-1/12 rounded-sm"></div>
+    <div class="pb-2">
       <div v-for="n in 6" :key="n" class="skeleton m-3 w-2/3 rounded-sm"></div>
     </div>
-    <div>
-      <div class="skeleton m-3 w-1/12 rounded-sm"></div>
+    <div class="pb-2">
       <div v-for="n in 6" :key="n" class="skeleton m-3 w-2/3 rounded-sm"></div>
     </div>
   </div>
