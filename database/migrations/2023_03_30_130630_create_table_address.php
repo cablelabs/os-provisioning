e4e48268741b98eaa70945500d87ee53ae44224e
Farshid Ghiasimanesh <farshid.ghiasimanesh@nmsprime.com>
2023-04-05 11:48:35 +0200

    [CS-61]: Add Address table and model

---
+++ database/migrations/2023_03_30_130630_create_table_address.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Database\Migrations\BaseMigration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends BaseMigration
+{
+    public $migrationScope = 'database';
+
+    protected $tableName = 'address';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $this->up_table_generic($table);
+            $table->decimal('lat', 9, 6);
+            $table->decimal('lng', 9, 6);
+            $table->string('source')->nullable();
+            $table->string('district')->nullable();
+            $table->string('zip')->nullable();
+            $table->string('city')->nullable();
+            $table->string('street')->nullable();
+            $table->string('house_number')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+};
---
+++ modules/ProvBase/Entities/Address.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\ProvBase\Entities;
+
+use App\BaseModel;
+
+class Address extends BaseModel
+{
+    // The associated SQL table for this Model
+    public $table = 'address';
+}
