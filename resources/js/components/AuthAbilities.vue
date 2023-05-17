219b1d9bf2515e5fd04ca82e02f000ffdcc2e362
Farshid Ghiasimanesh <farshid.ghiasimanesh@nmsprime.com>
2023-05-17 13:23:03 +0200

    return header licenses to files which were removed

--- resources/js/components/AuthAbilities.vue
+++ resources/js/components/AuthAbilities.vue
@@ -1,4 +1,26 @@
+/**
+ * Copyright (c) NMS PRIME GmbH ("NMS PRIME Community Version")
+ * and others – powered by CableLabs. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 <script>
+/**
+ * This is the Frontend logic for the App\Http\Controllers\Auth\AbilityController::class.
+ * The template for this module is resources/views/auth/abilities.blade.php.
+ */
+
 import { ref, reactive, onMounted } from 'vue'
 
 export default {
