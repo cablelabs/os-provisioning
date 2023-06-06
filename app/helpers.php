913093610262fa968c8b2d77c10e1d1ccb4f4cbf
Wali Razzaq <wali.razzaq@nmsprime.com>
2023-06-06 17:17:12 +0200

    DocumentManagement: HTML-PDF livewire adaptations

--- app/helpers.php
+++ app/helpers.php
@@ -707,3 +707,58 @@ function parseLocation($location)
 
     return $result;
 }
+
+/**
+ * Sanitizes HTML content.
+ * Removes specified HTML tags and attributes along with their content
+ *
+ * @author Wali Razzaq
+ */
+function sanitizeHtml(string $content,array $tags = ['script'],array $attributes=['x-data','x-init'])
+{
+    return new class($content,$tags,$attributes) implements Stringable
+    {
+        public function __construct(
+            private string $content,
+            private array $tags,
+            private array $attributes
+        ) {
+        }
+
+        public function tags(...$tags):self
+        {
+            $this->tags = $tags;
+            return $this;
+        }
+
+        public function attributes(...$attributes):self
+        {
+            $this->attributes = $attributes;
+            return $this;
+        }
+
+        protected function cleanup():self
+        {
+            foreach ($this->tags as $tag) {
+                $this->content = preg_replace('/<'.preg_quote($tag).'\b[^<]*(?:(?!<\/'.preg_quote($tag).'>)<[^<]*)*<\/'.preg_quote($tag).'>/', '', $this->content);
+            }
+
+            foreach ($this->attributes as $attribute) {
+                $this->content = preg_replace('/'.preg_quote($attribute).'\s*=\s*([\x22\x27])([\s\S]*?)\1/','',$this->content);
+            }
+
+            return $this;
+        }
+        public function __toString(): string
+        {
+            $this->cleanup();
+            return $this->content;
+        }
+
+        public function __destruct()
+        {
+            return $this->__toString();
+        }
+
+    };
+}
