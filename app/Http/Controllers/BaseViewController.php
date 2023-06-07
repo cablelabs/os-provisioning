3dc8034b4ba7ac3bc882f898c0c97cca92d9d1ee
Farshid Ghiasimanesh <farshid.ghiasimanesh@nmsprime.com>
2023-06-07 12:19:49 +0200

    [CS-83]: values have not to be int. conversion to integer changes the main values like zipcodes

--- app/Http/Controllers/BaseViewController.php
+++ app/Http/Controllers/BaseViewController.php
@@ -440,7 +440,7 @@ class BaseViewController extends Controller
                         $escaped_field_name = Str::substr($field['name'], 0, Str::length($field['name']) - 2);
                         $field['field_value'] = Request::old($escaped_field_name, array_keys($field['selected']));
                         // values MUST be int, because of strict type checking in Form module
-                        $field['field_value'] = array_map('intval', $field['field_value']);
+                        // $field['field_value'] = array_map('intval', $field['field_value']);
                     }
 
                     $s .= Form::select($field['name'], $value, $field['field_value'], $options, $field['optionsAttributes'] ?? []);
