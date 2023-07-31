6b63ddf67d6e98c876135573ddbe3cc748c6cac7
Christian Schramm <christian.schramm@nmsprime.com>
2023-07-31 03:20:21 +0200

    [CS-70] Fix mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -75,9 +75,9 @@ if (mix.inProduction()) {
   mix.postCss('resources/css/vendor.css', 'public/css')
 }
 
-  // extra assets
-  mix.js('resources/js/assets/nmsprime-canvas.js', 'public/js')
+// extra assets
   mix.js('resources/js/leaflet/pixi-overlay-tools.js', 'public/js/leaflet')
+  mix.js('resources/js/assets/nmsprime-canvas.js', 'public/js')
 
   // copy from node_modules do public
 mix.copy('node_modules/jszip/dist/jszip.min.js', 'public/js/jszip.min.js')
