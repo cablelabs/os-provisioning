9309eab96a8f645a07f98c521fc15e38c2a69a68
Christian Schramm <christian.schramm@nmsprime.com>
2023-11-01 16:57:05 +0100

    [CS-142] Revert accidental style changes

--- modules/ProvBase/Resources/js/components/ModemAnalysis.vue
+++ modules/ProvBase/Resources/js/components/ModemAnalysis.vue
@@ -1,22 +1,22 @@
 <script>
-import { ref } from "vue";
-import axios from "axios";
+import { ref } from 'vue'
+import axios from 'axios'
 
 export default {
   setup() {
-    const propData = document.querySelector("#OpenSourceModemAnalysis").dataset;
+    const propData = document.querySelector('#OpenSourceModemAnalysis').dataset
 
     // refs
-    const loading = ref(true);
-    const pingStarted = ref(false);
-    const selectedPing = ref(1);
-    const floodPingResult = ref(null);
+    const loading = ref(true)
+    const pingStarted = ref(false)
+    const selectedPing = ref(1)
+    const floodPingResult = ref(null)
     const pingOptions = [
       { id: 1, text: propData.modemAnalysisFloodpingLowLoad },
       { id: 2, text: propData.modemAnalysisFloodpingAverageLoad },
       { id: 3, text: propData.modemAnalysisFloodpingBigLoad },
       { id: 4, text: propData.modemAnalysisFloodpingHugeLoad },
-    ];
+    ]
 
     // methods
     function floodPing() {
@@ -25,31 +25,31 @@ export default {
         2: 10000,
         3: 30000,
         4: 30000,
-      };
+      }
 
       snotify.success(propData.messagesAnalysisPingInProgress, null, {
         timeout: timeout[selectedPing.value],
-      });
-      pingStarted.value = true;
-      floodPingResult.value = "";
+      })
+      pingStarted.value = true
+      floodPingResult.value = ''
 
       axios({
-        method: "post",
+        method: 'post',
         url: propData.routeModemFloodPing,
-        headers: { "X-CSRF-TOKEN": propData.csrfToken },
-        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        headers: { 'X-CSRF-TOKEN': propData.csrfToken },
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
         data: {
           task: selectedPing.value,
         },
       })
         .then((response) => {
-          floodPingResult.value = response.data;
-          pingStarted.value = false;
+          floodPingResult.value = response.data
+          pingStarted.value = false
         })
         .catch((error) => {
-          console.error(error);
-          pingStarted.value = false;
-        });
+          console.error(error)
+          pingStarted.value = false
+        })
     }
 
     return {
@@ -59,7 +59,7 @@ export default {
       floodPingResult,
       pingOptions,
       floodPing,
-    };
+    }
   },
-};
+}
 </script>
